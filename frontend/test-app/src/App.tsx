5d5876820da7b855dd6c5e9793a8909f0f99278d
Eduardo Robles <edu@sequentech.io>
2023-08-06 04:54:59 +0000

    caching builds on devcontainer

--- frontend/test-app/src/App.tsx
+++ frontend/test-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Yay.
+          Edit <code>src/App.tsx</code> and save to reload. Let's change it.
         </p>
         <a
           className="App-link"
