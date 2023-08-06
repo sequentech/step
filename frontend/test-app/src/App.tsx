bdaa19c6026741ad261005825423a610ad7b2577
Eduardo Robles <edu@sequentech.io>
2023-08-06 09:12:19 +0000

    initial working version

--- frontend/test-app/src/App.tsx
+++ frontend/test-app/src/App.tsx
@@ -1,29 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Keycloak from 'keycloak-js';
-
-const KeycloakConfig = {
-  url: 'http://127.0.0.1:8090/auth',
-  realm: 'electoral-process',
-  clientId: 'frontend'
-};
+import { AuthContext } from './context/AuthContextProvider';
 
 let App = () => {
-  const keycloak = new Keycloak(KeycloakConfig);
-
-  try {
-    //const authenticated = await keycloak.init();
-    //console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
-  } catch (error) {
-    console.error('Failed to initialize adapter:', error);
-  }
+  const authContext = useContext(AuthContext);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {
+        !authContext.isAuthenticated
+          ? <p>Not Authenticated</p>
+          : <p>User Authenticated</p>
+        }
         <p>
           Edit <code>src/App.tsx</code> and save to reload. Let's change it.
         </p>
