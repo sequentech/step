62768f3cb10b40975a3a02934833eae8c685b572
Félix Robles <felix@sequentech.io>
2023-08-11 22:08:20 +0000

    wip

--- frontend/voting-portal/src/services/ApolloService.ts
+++ frontend/voting-portal/src/services/ApolloService.ts
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache, createHttpLink} from "@apollo/client"
 import {setContext} from "@apollo/client/link/context"
 
 const httpLink = createHttpLink({
-    uri: "http://localhost:8082/v1/graphql",
+    uri: "http://localhost:8080/v1/graphql",
 })
 
 const authLink = setContext((_, {headers}) => {
