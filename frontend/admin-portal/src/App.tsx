db501e5348104d49740eda692cf5d9cfc715a46d
Félix Robles <felix@sequentech.io>
2023-08-13 20:26:52 +0000

    wip

--- frontend/admin-portal/src/App.tsx
+++ frontend/admin-portal/src/App.tsx
@@ -22,11 +22,16 @@ import {
     Create,
     ReferenceInput,
     SelectInput,
+    ArrayInput,
+    SimpleFormIterator,
 } from "react-admin"
 import buildHasuraProvider from "ra-data-hasura"
 import {apolloClient} from "./services/ApolloService"
 import {Chip} from "@mui/material"
-import {Sequent_Backend_Election} from "./gql/graphql"
+import {CreateElectionEventMutation, Sequent_Backend_Election} from "./gql/graphql"
+import {v4} from "uuid"
+import { useMutation } from "@apollo/client"
+import { INSERT_ELECTION_EVENT } from "./queries/InsertElectionEvent"
 
 const ListActions: React.FC = () => (
     <TopToolbar>
@@ -95,11 +100,54 @@ const EditElectionList: React.FC = () => {
     )
 }
 
+interface IElectionSubmit {
+    description: string
+    name: string
+}
+
+interface IElectionEventSubmit {
+    name: string
+    description: string
+    elections: Array<IElectionSubmit>
+    encryption_protocol: string
+    id: string
+    tenant_id: string
+}
+
 const CreateElectionList: React.FC = () => {
+    const [insertElectionEvent] = useMutation<CreateElectionEventMutation>(INSERT_ELECTION_EVENT)
+    const postDefaultValues = () => ({id: v4()})
+
+    const handleSubmit = async (values: any) => {
+        const {elections, ...electionSubmit} = values as IElectionEventSubmit
+        await insertElectionEvent({
+            variables: {
+                elections: elections.map(e => ({election_event_id: electionSubmit.id, tenant_id: electionSubmit.tenant_id, ...e})),
+                electionEvent: electionSubmit,
+            },
+        })
+        console.log(values)
+    }
     return (
-        <Create>
-            <ElectionListForm />
-        </Create>
+        <SimpleForm defaultValues={postDefaultValues} onSubmit={handleSubmit}>
+            <TextInput source="description" />
+            <TextInput source="name" />
+            <SelectInput
+                source="encryption_protocol"
+                choices={[
+                    {id: "RSA256", name: "RSA256"}
+                ]}
+            />
+            <ReferenceInput source="tenant_id" reference="sequent_backend_tenant">
+                <SelectInput optionText="username" />
+            </ReferenceInput>
+            <ArrayInput source="elections">
+                <SimpleFormIterator inline>
+                    <TextInput source="name" />
+                    <TextInput source="description" />
+                </SimpleFormIterator>
+            </ArrayInput>
+        </SimpleForm>
     )
 }
 
