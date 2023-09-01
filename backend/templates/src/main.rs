94a7bad8451417f9bb8599a4a126ac955c895e40
Félix Robles <felix@sequentech.io>
2023-09-01 20:18:46 +0000

    wip

--- backend/templates/src/main.rs
+++ backend/templates/src/main.rs
@@ -22,6 +22,7 @@ fn rocket() -> _ {
         routes![
             routes::fetch_document::fetch_document,
             routes::scheduled_event::create_scheduled_event,
+            routes::create_keys::create_keys,
         ],
     )
 }
---
+++ backend/templates/src/routes/create_keys.rs
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2023 Felix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use anyhow::Result;
+use rocket::response::Debug;
+use rocket::serde::json::Json;
+use rocket::serde::json::Value;
+use rocket::serde::{Deserialize, Serialize};
+use serde_json::json;
+
+use crate::connection;
+
+#[derive(Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct CreateKeysBody {
+    tenant_id: String,
+    election_event_id: String,
+}
+
+#[post("/create-keys", format = "json", data = "<body>")]
+pub async fn create_keys(
+    body: Json<CreateKeysBody>,
+    auth_headers: connection::AuthHeaders,
+) -> Result<(), Debug<anyhow::Error>> {
+    Ok(())
+}
