cdcabc4acde2fa55e215f4ff026e3a50ea86539a
Félix Robles <felix@sequentech.io>
2023-09-02 11:59:42 +0000

    wip

--- backend/braid/scripts/protocol_manager.sh
+++ backend/braid/scripts/protocol_manager.sh
@@ -5,7 +5,7 @@ cd /opt/braid
 bb_helper --cache-dir /tmp/cache -s $IMMUDB_URL -i $IMMUDB_INDEX_DB -b defaultboard  -u $IMMUDB_USER -p $IMMUDB_PASSWORD upsert-init-db -l debug
 
 # generate pm config, if it doesn't exist
-PM_CONFIG=./pm.toml
+PM_CONFIG=/opt/braid/pm.toml
 if [ ! -f $PM_CONFIG ]
 then
     gen_trustee_config protocol-manager > $PM_CONFIG
