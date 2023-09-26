217719b66c7b34c38d36f571b31fdff7465d0557
Eduardo Robles <edu@sequentech.io>
2023-09-26 15:00:41 +0000

    Improving immudb support
    Parent issue: https://github.com/sequentech/meta/issues/188

--- packages/admin-portal/src/queries/ListPgAudit.ts
+++ packages/admin-portal/src/queries/ListPgAudit.ts
@@ -3,12 +3,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 import {gql} from "@apollo/client"
 
-//query listPgaudit($limit: Int, $offset: Int) {
-//  listPgaudit(limit: $limit, offset: $offset) {
 
 export const getList = (fields: any) => gql`
-query listPgaudit {
-  listPgaudit {
+query listPgaudit($limit: Int, $offset: Int) {
+  listPgaudit(limit: $limit, offset: $offset) {
     items {
       id
       audit_type
