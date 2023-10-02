692baac9c2d2ec3f9e011a079e79e0b68b0624e3
Eduardo Robles <edu@sequentech.io>
2023-10-02 12:15:13 +0000

    🐞 DevContainers ports are not being forwarded
    Parent issue: https://github.com/sequentech/meta/issues/266

---
+++ .devcontainer/initializeCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -i
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Create .devcontainer/.env if it does not already exists
+[ -e .devcontainer/.env ] || touch .devcontainer/.env
+# Load .devcontainer/.env environment variables
+source .devcontainer/.env
+# Set LOCAL_WORKSPACE_FOLDER environment variable if not already set
+[ ! -z "${localWorkspaceFolder}" ] || printf "\nLOCAL_WORKSPACE_FOLDER=${SCRIPT_DIR}/..\n" > .devcontainer/.env
