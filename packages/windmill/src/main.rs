96db4d2122919222b7b18a62cd6555e37e39a546
Félix Robles <felix@sequentech.io>
2023-10-17 20:39:57 +0000

    wip

---
+++ packages/windmill/src/main.rs
@@ -0,0 +1,75 @@
+#![allow(non_upper_case_globals)]
+
+use anyhow::Result;
+use async_trait::async_trait;
+use celery::prelude::*;
+use structopt::StructOpt;
+use tokio::time::{self, Duration};
+
+// This generates the task struct and impl with the name set to the function name "add"
+#[celery::task]
+fn add(x: i32, y: i32) -> TaskResult<i32> {
+    Ok(x + y)
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "celery_app",
+    about = "Run a Rust Celery producer or consumer.",
+    setting = structopt::clap::AppSettings::ColoredHelp,
+)]
+enum CeleryOpt {
+    Consume,
+    Produce {
+        #[structopt(possible_values = &["add"])]
+        tasks: Vec<String>,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opt = CeleryOpt::from_args();
+
+    let my_app = celery::app!(
+        // broker = RedisBroker { std::env::var("REDIS_ADDR").unwrap_or_else(|_| "redis://127.0.0.1:6379/".into()) },
+        broker = AMQPBroker { std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://rabbitmq:5672".into()) },
+        tasks = [
+            add,
+        ],
+        // This just shows how we can route certain tasks to certain queues based
+        // on glob matching.
+        task_routes = [
+            "add" => "test_task",
+        ],
+        prefetch_count = 2,
+        heartbeat = Some(10),
+    ).await?;
+
+    match opt {
+        CeleryOpt::Consume => {
+            my_app.display_pretty().await;
+            my_app.consume_from(&["test_task"]).await?;
+        }
+        CeleryOpt::Produce { tasks } => {
+            if tasks.is_empty() {
+                // Basic task sending.
+                my_app.send_task(add::new(1, 2)).await?;
+
+                // Sending a task with additional options like `countdown`.
+                my_app
+                    .send_task(add::new(1, 3).with_countdown(3).with_time_limit(20))
+                    .await?;
+            } else {
+                for task in tasks {
+                    match task.as_str() {
+                        "add" => my_app.send_task(add::new(1, 2)).await?,
+                        _ => panic!("unknown task"),
+                    };
+                }
+            }
+        }
+    };
+
+    my_app.close().await?;
+    Ok(())
+}
