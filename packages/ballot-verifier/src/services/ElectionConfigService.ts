8f87c498a5980456f1c3247765440c22dcbde69b
Félix Robles <felix@sequentech.io>
2023-10-24 11:37:18 +0000

    wip

--- packages/ballot-verifier/src/services/ElectionConfigService.ts
+++ packages/ballot-verifier/src/services/ElectionConfigService.ts
@@ -1,10 +1,16 @@
 // SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-import {IAnswer, IQuestion} from "sequent-core"
+import {ICandidate, IContest} from "sequent-core"
 
-export const findUrlByTitle = (answer: IAnswer, urlTitle: string): string | undefined =>
-    answer.urls.find((url) => urlTitle === url.title)?.url
+export const findUrlByTitle = (answer: ICandidate, urlTitle: string): string | undefined =>
+    answer.presentation?.urls?.find((url) => urlTitle === url.title)?.url
 
-export const checkIsWriteIn = (answer: IAnswer): boolean =>
-    "true" === findUrlByTitle(answer, "isWriteIn")
+export const getImageUrl = (answer: ICandidate): string | undefined =>
+    answer.presentation?.urls?.find((url) => url.is_image)?.url
+
+export const checkIsWriteIn = (answer: ICandidate): boolean =>
+    answer.presentation?.is_write_in || false
+
+export const checkIsInvalidVote = (answer: ICandidate): boolean =>
+    answer.presentation?.is_explicit_invalid || false
