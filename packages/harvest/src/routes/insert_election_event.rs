d05e5b039e5b15d0ed2d5a2fb395dae0d64102af
Eduardo Robles <edu@sequentech.io>
2023-10-26 16:31:22 +0000

    adding insert_election_event api endpoint to hasura

---
+++ packages/harvest/src/routes/insert_election_event.rs
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2023 Eduardo Robles <edu@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use anyhow::{Result, anyhow};
+use rocket::response::Debug;
+use rocket::serde::json::Json;
+use serde_json::Value;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use tracing::{instrument, event, Level};
+use windmill::connection;
+use windmill::hasura;
+use windmill::hasura::election_event::insert_election_event::{
+    sequent_backend_election_event_insert_input as InsertElectionEventInput
+};
+use windmill::services::celery_app::get_celery_app;
+use windmill::tasks::insert_election_event;
+
+use crate::services;
+
+#[instrument(skip(auth_headers))]
+#[post("/insert-election-event", format = "json", data = "<body>")]
+pub async fn insert_election_event_f(
+    body: Json<InsertElectionEventInput>,
+    auth_headers: connection::AuthHeaders,
+) -> Result<Json<()>, Debug<anyhow::Error>> {
+    let celery_app = get_celery_app().await;
+    let task = celery_app
+        .send_task(insert_election_event::insert_election_event_t::new(
+            auth_headers.clone(),
+            body.into_inner().clone(),
+        ))
+        .await
+        .map_err(|e| anyhow::Error::from(e))?;
+    event!(
+        Level::INFO,
+        "Sent INSERT_ELECTION_EVENT task {}",
+        task.task_id
+    );
+
+    Ok(Json(()))
+}
