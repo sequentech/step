3db561528cf0b459f341c439da42a7b7251259f5
Félix Robles <felix@sequentech.io>
2023-11-08 00:22:14 +0000

    wip

---
+++ packages/ui-essentials/src/components/Tree/__stories__/Tree.stories.tsx
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import React from "react"
+import {Meta, StoryObj} from "@storybook/react"
+import SelectElection, {SelectElectionProps} from "../SelectElection"
+import {INITIAL_VIEWPORTS} from "@storybook/addon-viewport"
+import {Box} from "@mui/material"
