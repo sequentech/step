bcf7a279d860b5302eea33feb265b82db15fd2f0
Kevin Nguyen <knth@hey.com>
2023-11-09 15:11:08 +0100

    feat: wip, fetch plaintext message

--- packages/windmill/src/tasks/process_board.rs
+++ packages/windmill/src/tasks/process_board.rs
@@ -1,9 +1,14 @@
 // SPDX-FileCopyrightText: 2023 Felix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
+
+use braid_messages::artifact::Plaintexts;
+use braid_messages::message::Message;
+use braid_messages::statement::Statement;
 use celery::error::TaskError;
 use sequent_core::services::keycloak;
 use strand::backend::ristretto::RistrettoCtx;
+use strand::serialization::StrandDeserialize;
 use tracing::instrument;
 
 use crate::hasura;
@@ -38,15 +43,54 @@ pub async fn process_board(election_event_id: String, tenant_id: String) -> Resu
     )
     .await?;
 
-    let toto = &hasura_response.data.expect("expected data");
-
-    dbg!(&toto);
+    let res = &hasura_response.data.expect("expected data");
+    dbg!(&res);
 
     let bulletin_board_opt =
         get_election_event_board(election_event.bulletin_board_reference.clone());
 
     if let Some(bulletin_board) = bulletin_board_opt {
         let pm = protocol_manager::gen_protocol_manager::<RistrettoCtx>();
+
+        let mut board = protocol_manager::get_board().await?;
+        
+        // let messages: Vec<Message> =
+        //     protocol_manager::get_board_messages(&mut board, &bulletin_board).await?;
+        //
+        // let res = messages
+        //     .iter()
+        //     .filter(|m| matches!(m.statement, Statement::Plaintexts(..)))
+        //     .map(|m| {
+        //         dbg!(&m);
+        //         Message::strand_deserialize(&m.artifact.as_ref().unwrap().clone())
+        //     })
+        //     .collect::<Vec<_>>();
+
+        let messages = board.get_messages(&bulletin_board, -1).await?;
+
+        let messages = messages
+            .into_iter()
+            .map(|bm| Message::strand_deserialize(&bm.message))
+            .filter(|bm| {
+                if let Ok(m) = bm {
+                    matches!(m.statement, Statement::Plaintexts(..))
+                    // match m.statement.get_kind() {
+                    //     StatementType::PublicKey => true,
+                    //     _ => false,
+                    // }
+                } else {
+                    false
+                }
+            })
+            .map(|bm| {
+                let mes = bm.unwrap();
+                let artifact = mes.artifact.unwrap();
+                let plaintexts = Plaintexts::<RistrettoCtx>::strand_deserialize(&artifact);
+                plaintexts
+            })
+            .collect::<Vec<_>>();
+
+        dbg!(&messages);
     }
 
     Ok(())
