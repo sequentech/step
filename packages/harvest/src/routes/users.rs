2f86472ca8d9ebf5d2a60a5d5a23e51b9edbbefe
Félix Robles <felix@sequentech.io>
2023-11-21 01:37:58 +0000

    wip

--- packages/harvest/src/routes/users.rs
+++ packages/harvest/src/routes/users.rs
@@ -1,8 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Eduardo Robles <edu@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
+use crate::services::authorization::authorize;
 use anyhow::{anyhow, Result};
 use immu_board::util::get_tenant_name;
+use rocket::http::Status;
 use rocket::response::Debug;
 use rocket::serde::json::Json;
 use sequent_core::services::jwt;
@@ -10,21 +12,28 @@ use sequent_core::services::keycloak::KeycloakAdminClient;
 use sequent_core::types::keycloak::User;
 use tracing::instrument;
 
-#[instrument]
-#[get("/users?<search>&<email>&<max>")]
+#[instrument(skip(claims))]
+#[get("/<tenant_id>/users?<search>&<email>&<max>")]
 pub async fn get_users(
-    jwt: jwt::JwtClaims,
+    claims: jwt::JwtClaims,
+    tenant_id: &str,
     search: Option<String>,
     email: Option<String>,
     max: Option<i32>,
-) -> Result<Json<Vec<User>>, Debug<anyhow::Error>> {
-    let board = get_tenant_name(&jwt.hasura_claims.tenant_id);
+) -> Result<Json<Vec<User>>, (Status, String)> {
+    authorize(
+        &claims,
+        true,
+        Some(tenant_id.into()),
+        vec!["read-users".into()],
+    )?;
+    let board = get_tenant_name(tenant_id);
     let client = KeycloakAdminClient::new()
         .await
-        .map_err(|e| anyhow::Error::from(e))?;
+        .map_err(|e| (Status::InternalServerError, format!("{:?}", e)))?;
     let users = client
         .list_users(&board, search, email, max)
         .await
-        .map_err(|e| anyhow::Error::from(e))?;
+        .map_err(|e| (Status::InternalServerError, format!("{:?}", e)))?;
     Ok(Json(users))
 }
