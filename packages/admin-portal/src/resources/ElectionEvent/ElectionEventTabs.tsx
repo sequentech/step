498437f3ac0e64a60f34a2dd72092ea2756400f5
Kevin Nguyen <knth@hey.com>
2023-11-22 22:18:56 +0100

    feat: add DashboardElectionEvent cmpt

--- packages/admin-portal/src/resources/ElectionEvent/ElectionEventTabs.tsx
+++ packages/admin-portal/src/resources/ElectionEvent/ElectionEventTabs.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import {TabbedShowLayout, TextField, useRecordContext} from "react-admin"
+import {TabbedShowLayout, useRecordContext} from "react-admin"
 import {Sequent_Backend_Election_Event} from "../../gql/graphql"
 import ElectionHeader from "../../components/ElectionHeader"
-import {Box} from "@mui/material"
 import {EditElectionEventData} from "./EditElectionEventData"
-import { BarChart, ChartsContainer, ElectionStats, PieChart } from './EditElectionEventDashboard'
-import { ReportDialog } from '../../components/ReportDialog'
-import { EditElectionEventAreas } from './EditElectionEventAreas'
+import {EditElectionEventAreas} from "./EditElectionEventAreas"
+import DashboardElectionEvent from "../../components/election-event/dashboard"
 
 export const ElectionEventTabs: React.FC = () => {
     const record = useRecordContext<Sequent_Backend_Election_Event>()
@@ -16,15 +14,7 @@ export const ElectionEventTabs: React.FC = () => {
             <ElectionHeader title={record?.name} subtitle="electionEventScreen.common.subtitle" />
             <TabbedShowLayout>
                 <TabbedShowLayout.Tab label="Dashboard">
-                    <Box sx={{padding: "16px"}}>
-                        <TextField source="name" fontSize="24px" fontWeight="bold" />
-                        <ElectionStats />
-                        <ChartsContainer>
-                            <BarChart />
-                            <PieChart />
-                        </ChartsContainer>
-                        <ReportDialog />
-                    </Box>
+                    <DashboardElectionEvent />
                 </TabbedShowLayout.Tab>
                 <TabbedShowLayout.Tab label="Data">
                     <EditElectionEventData />
