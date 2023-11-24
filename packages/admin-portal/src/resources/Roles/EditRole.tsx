24a7dabab3d110228760efd3357e687db4442ed2
Félix Robles <felix@sequentech.io>
2023-11-24 12:17:29 +0000

    wip

--- packages/admin-portal/src/resources/Roles/EditRole.tsx
+++ packages/admin-portal/src/resources/Roles/EditRole.tsx
@@ -6,7 +6,9 @@ import {Edit, Identifier, useListContext} from "react-admin"
 import {PageHeaderStyles} from "../../components/styles/PageHeaderStyles"
 import ElectionHeader from "../../components/ElectionHeader"
 import {useTranslation} from "react-i18next"
-import {IRole} from "sequent-core"
+import {IPermission, IRole} from "sequent-core"
+import {DataGrid, GridColDef, GridRenderCellParams, GridValueGetterParams} from "@mui/x-data-grid"
+import Checkbox from "@mui/material/Checkbox"
 
 interface EditRoleProps {
     id?: Identifier | undefined
@@ -21,18 +23,53 @@ export const EditRole: React.FC<EditRoleProps> = ({id, close}) => {
     }
     let role: IRole | undefined = data?.find((element) => element.id === id)
 
+    let permissions: Array<string> = role?.permissions || []
+
+    let rows: Array<IPermission & {active: boolean}> = permissions.map((permission) => ({
+        id: permission,
+        name: permission,
+        attributes: {},
+        active: true,
+    }))
+
+    const columns: GridColDef[] = [
+        {
+            field: "name",
+            headerName: "Permission",
+            width: 200,
+            editable: false,
+        },
+        {
+            field: "active",
+            headerName: "Active",
+            width: 70,
+            editable: false,
+            renderCell: (props: GridRenderCellParams<any, boolean>) => (
+                <Checkbox checked={props.value} />
+            ),
+        },
+    ]
+
     return (
         <PageHeaderStyles.Wrapper>
             <ElectionHeader
                 title={t("usersAndRolesScreen.roles.edit.title")}
                 subtitle="usersAndRolesScreen.roles.edit.subtitle"
             />
-            {role?.id}
-            {
-                role?.permissions?.map(permission => <div key={permission}>
-                    {permission}
-                </div>)
-            }
+            {role?.name}
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 20,
+                        },
+                    },
+                }}
+                pageSizeOptions={[20, 50, 100]}
+                disableRowSelectionOnClick
+            />
         </PageHeaderStyles.Wrapper>
     )
 }
