b0c87d08d10f1bfac3b3291dcf6a091e21974b7e
Félix Robles <felix@sequentech.io>
2023-11-24 23:03:19 +0000

    wip

--- packages/admin-portal/src/resources/Roles/EditRole.tsx
+++ packages/admin-portal/src/resources/Roles/EditRole.tsx
@@ -10,6 +10,7 @@ import {IPermission, IRole} from "sequent-core"
 import {DataGrid, GridColDef, GridRenderCellParams} from "@mui/x-data-grid"
 import Checkbox from "@mui/material/Checkbox"
 import {IPermissions} from "../../types/keycloak"
+import { TextField } from "@mui/material"
 
 type EnumObject = {[key: string]: number | string}
 type EnumObjectEnum<E extends EnumObject> = E extends {[key: string]: infer ET | string}
@@ -41,14 +42,15 @@ export const EditRole: React.FC<EditRoleProps> = ({id, close, permissions}) => {
     let validPermissions = getEnumValues(IPermissions)
 
     let rows: Array<IPermission & {id: string; active: boolean}> = (permissions || [])
-        .map((permission) => ({
-            ...permission,
-            id: permission.id || "",
-            active: (!!permission.name && rolePermissions.includes(permission.name)) || false,
-        }))
         .filter(
             (permission) => permission.name && validPermissions.includes(permission.name as any)
         )
+        .map((permission) => ({
+            ...permission,
+            id: permission.id || "",
+            name: permission.name && t(`usersAndRolesScreen.permissions.${permission.name}`),
+            active: (!!permission.name && rolePermissions.includes(permission.name)) || false,
+        }))
 
     const columns: GridColDef[] = [
         {
@@ -73,6 +75,14 @@ export const EditRole: React.FC<EditRoleProps> = ({id, close, permissions}) => {
             <ElectionHeader
                 title={t("usersAndRolesScreen.roles.edit.title")}
                 subtitle="usersAndRolesScreen.roles.edit.subtitle"
+            />
+
+            <TextField
+                label="Name"
+                value={role?.name}
+                InputProps={{
+                    readOnly: true,
+                }}
             />
             {role?.name}
             <DataGrid
