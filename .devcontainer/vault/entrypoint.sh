abdb7a33cf3dcac759dd5bc183129bcf46eb4882
Félix Robles <felix@sequentech.io>
2023-11-27 20:50:39 +0000

    wip

--- .devcontainer/vault/entrypoint.sh
+++ .devcontainer/vault/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
+set -x
 # Check if the data directory is empty
-if [ -z "$(ls -A /vault)" ]; then
+if [ ! -d /vault/sys ]; then
   # If it's empty, untar the pre-copied volume.tar.gz into it
   tar -xzf /opt/vault/volume.tar.gz -C /vault
 fi
 
 # Then call the original entrypoint command
-exec vault server -config /opt/vault/config.hcl
+vault server -config /opt/vault/config.hcl &
+sleep 5
+vault operator unseal $VAULT_KEY
+
+# Now bring the vault server back into the foreground
+# so that it becomes the main process of the container
+wait
