5bba28383498d982a3e3a7635434fa1fd132fd2c
Félix Robles <felix@sequentech.io>
2023-11-27 12:35:45 +0000

    wip

---
+++ packages/admin-portal/src/resources/ElectionEvent/EditElectionEventPublish.tsx
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { Box } from "@mui/material"
+import React from "react"
+
+export const EditElectionEventPublish: React.FC = () => {
+    return <Box>
+
+    </Box>
+}
