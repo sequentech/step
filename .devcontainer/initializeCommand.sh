89ecfa14e9c4bd88c9d48de77eb9a3c8fafde25a
Félix Robles <felix@sequentech.io>
2023-11-28 11:23:59 +0000

    wip

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -3,8 +3,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Create .devcontainer/.env if it does not already exists
-[ -e .devcontainer/.env ] || cp .devcontainer/windmill.env .devcontainer/.env
+[ -e .devcontainer/.env ] || touch .devcontainer/.env
+cp .devcontainer/windmill.env .devcontainer/.env
 # Load .devcontainer/.env environment variables
 source .devcontainer/.env
 # Set LOCAL_WORKSPACE_FOLDER environment variable if not already set
-[ ! -z "${localWorkspaceFolder}" ] || printf "\nLOCAL_WORKSPACE_FOLDER=${SCRIPT_DIR}/..\n" > .devcontainer/.env
+[ ! -z "${localWorkspaceFolder}" ] || printf "\nLOCAL_WORKSPACE_FOLDER=${SCRIPT_DIR}/..\n" >> .devcontainer/.env
