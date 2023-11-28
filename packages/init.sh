8e742601dd47e5cce3296f1620ba24725e7cd042
Barel Elbaz <barel.elbaz@harmonicinc.com>
2023-11-28 17:49:52 +0200

    Improve frontend Dockerfile

--- packages/init.sh
+++ packages/init.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-yarn --pure-lockfile --non-interactive
-yarn build:ui-essentials
-yarn start
+# First argument is the environment
+ENVIRONMENT=${1-:dev}
+
+if [ "$ENVIRONMENT" = "prod" ];then
+    yarn start
+else
+    yarn --pure-lockfile --non-interactive
+    yarn build:ui-essentials
+    yarn start
+fi
