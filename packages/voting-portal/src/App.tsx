c11655233b361e7fa102dc5813bf7679f550ea34
Félix Robles <felix@sequentech.io>
2023-11-29 17:23:54 +0000

    wip

--- packages/voting-portal/src/App.tsx
+++ packages/voting-portal/src/App.tsx
@@ -1,26 +1,22 @@
 // SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-import React, { useEffect, useContext } from "react"
-import { Routes, Route, useLocation, Navigate } from "react-router-dom"
-import { styled } from "@mui/material/styles"
-import { Footer, Header, PageBanner } from "@sequentech/ui-essentials"
+import React, {useEffect, useContext} from "react"
+import {Routes, Route, useLocation, Navigate} from "react-router-dom"
+import {styled} from "@mui/material/styles"
+import {Footer, Header, PageBanner} from "@sequentech/ui-essentials"
 import Stack from "@mui/material/Stack"
-import { StartScreen } from "./screens/StartScreen"
-import { VotingScreen } from "./screens/VotingScreen"
-import { ReviewScreen } from "./screens/ReviewScreen"
-import { ConfirmationScreen } from "./screens/ConfirmationScreen"
-import { AuditScreen } from "./screens/AuditScreen"
-import { ElectionSelectionScreen } from "./screens/ElectionSelectionScreen"
-import { LoginScreen } from "./screens/LoginScreen"
-import { useNavigate } from "react-router-dom"
-import { AuthContext } from "./providers/AuthContextProvider"
-import { RouteParameterProvider } from "."
-import {
-    DISABLE_AUTH,
-    DEFAULT_TENANT_ID,
-    DEFAULT_EVENT_ID,
-} from "./Config"
+import {StartScreen} from "./screens/StartScreen"
+import {VotingScreen} from "./screens/VotingScreen"
+import {ReviewScreen} from "./screens/ReviewScreen"
+import {ConfirmationScreen} from "./screens/ConfirmationScreen"
+import {AuditScreen} from "./screens/AuditScreen"
+import {ElectionSelectionScreen} from "./screens/ElectionSelectionScreen"
+import {LoginScreen} from "./screens/LoginScreen"
+import {useNavigate} from "react-router-dom"
+import {AuthContext} from "./providers/AuthContextProvider"
+import {RouteParameterProvider} from "."
+import {DISABLE_AUTH, DEFAULT_TENANT_ID, DEFAULT_EVENT_ID} from "./Config"
 
 const StyledApp = styled(Stack)`
     min-height: 100vh;
@@ -29,9 +25,7 @@ const StyledApp = styled(Stack)`
 const HeaderWithContext: React.FC = () => {
     const authContext = useContext(AuthContext)
 
-    return <Header
-        logoutFn={authContext.isAuthenticated ? authContext.logout : undefined} 
-    />
+    return <Header logoutFn={authContext.isAuthenticated ? authContext.logout : undefined} />
 }
 
 const App = () => {
@@ -39,9 +33,7 @@ const App = () => {
 
     useEffect(() => {
         if (DISABLE_AUTH) {
-            navigate(
-                `/tenant/${DEFAULT_TENANT_ID}/event/${DEFAULT_EVENT_ID}/election-chooser`
-            )
+            navigate(`/tenant/${DEFAULT_TENANT_ID}/event/${DEFAULT_EVENT_ID}/election-chooser`)
         }
     }, [navigate])
 
@@ -50,11 +42,14 @@ const App = () => {
             {DISABLE_AUTH ? <Header /> : <HeaderWithContext />}
             <PageBanner marginBottom="auto">
                 <Routes>
-                    <Route path="/"
-                        element={<Navigate
-                            replace
-                            to={`/tenant/${DEFAULT_TENANT_ID}/event/${DEFAULT_EVENT_ID}/login`}
-                        />}
+                    <Route
+                        path="/"
+                        element={
+                            <Navigate
+                                replace
+                                to={`/tenant/${DEFAULT_TENANT_ID}/event/${DEFAULT_EVENT_ID}/login`}
+                            />
+                        }
                     />
                     <Route
                         path="/tenant/:tenantId/event/:eventId/login"
