889d2dc9d7fc01d20a7535bf9a5d4dfaf0fd3bc6
Enric Badia <enric.badia@gmail.com>
2023-12-05 18:02:17 +0000

    tenant trustees list in tally

--- packages/admin-portal/src/resources/Tally/TallyTrusteesList.tsx
+++ packages/admin-portal/src/resources/Tally/TallyTrusteesList.tsx
@@ -2,19 +2,14 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 import React, {useEffect, useState} from "react"
-import {useGetOne, useGetMany} from "react-admin"
+import {useGetOne, useGetList} from "react-admin"
 
-import {
-    Sequent_Backend_Trustee,
-    Sequent_Backend_Tally_Session,
-} from "../../gql/graphql"
+import {Sequent_Backend_Trustee, Sequent_Backend_Tally_Session} from "../../gql/graphql"
 import {useElectionEventTallyStore} from "@/providers/ElectionEventTallyProvider"
-import {
-    DataGrid,
-    GridColDef,
-    GridRenderCellParams,
-} from "@mui/x-data-grid"
-import Checkbox from "@mui/material/Checkbox"
+import {DataGrid, GridColDef, GridRenderCellParams} from "@mui/x-data-grid"
+import CachedIcon from "@mui/icons-material/Cached"
+import CheckCircleIcon from "@mui/icons-material/CheckCircle"
+import { useTenantStore } from '@/providers/TenantContextProvider'
 
 interface TallyTrusteesListProps {
     update: (elections: Array<string>) => void
@@ -24,39 +19,57 @@ export const TallyTrusteesList: React.FC<TallyTrusteesListProps> = (props) => {
     const {update} = props
 
     const [tallyId] = useElectionEventTallyStore()
+    const [tenantId] = useTenantStore()
 
     const [trusteesData, setTrusteesData] = useState<
         Array<Sequent_Backend_Trustee & {rowId: number; id: string; active: boolean}>
     >([])
 
-
     const {data} = useGetOne<Sequent_Backend_Tally_Session>(
         "sequent_backend_tally_session",
         {
             id: tallyId,
+        },
+        {
+            refetchInterval: 5000,
         }
     )
 
-    const {data: trustees} = useGetMany("sequent_backend_trustee", {
-        ids: data?.trustee_ids || [],
+    const {data: trustees} = useGetList("sequent_backend_trustee", {
+        pagination: {page: 1, perPage: 1000},
+        filter: {tenant_id: tenantId},
     })
 
     useEffect(() => {
-        if (trustees) {
-            const temp = (trustees || []).map((trustee, index) => ({
-                ...trustee,
-                rowId: index,
-                id: trustee.id || "",
-                name: trustee.name,
-                active: false,
-            }))
+        if (data && trustees) {
+            const temp = (trustees || []).map((trustee, index) => {
+                if (data && data.trustee_ids) {
+                    return {
+                        ...trustee,
+                        rowId: index,
+                        id: trustee.id,
+                        name: trustee.name,
+                        active: data?.trustee_ids?.find((x) => x === trustee.id),
+                    }
+                } else {
+                    return {
+                        ...trustee,
+                        rowId: index,
+                        id: trustee.id,
+                        name: trustee.name,
+                        active: false,
+                    }
+                }
+            })
             setTrusteesData(temp)
         }
-    }, [trustees])
+    }, [trustees, data])
 
     useEffect(() => {
         if (trusteesData) {
-            const temp = trusteesData.filter((election) => election.active).map((election) => election.id)
+            const temp = trusteesData
+                .filter((election) => election.active)
+                .map((election) => election.id)
             update(temp)
         }
     }, [trusteesData])
@@ -73,26 +86,10 @@ export const TallyTrusteesList: React.FC<TallyTrusteesListProps> = (props) => {
             headerName: "Fragment",
             flex: 1,
             editable: true,
-            renderCell: (props: GridRenderCellParams<any, boolean>) => (
-                <Checkbox checked={props.value} onChange={() => handleConfirmChange(props.row)} />
-            ),
+            renderCell: (props: GridRenderCellParams<any, boolean>) =>
+                props.value ? <CheckCircleIcon sx={{color: "#0F054C"}} /> : <CachedIcon />,
         },
     ]
-
-    function handleConfirmChange(clickedRow: any) {
-        const updatedData: Array<
-            Sequent_Backend_Trustee & {rowId: number; id: string; active: boolean}
-        > = trusteesData?.map((x) => {
-            if (x.rowId === clickedRow.rowId) {
-                return {
-                    ...x,
-                    active: !clickedRow.active,
-                }
-            }
-            return x
-        })
-        setTrusteesData(updatedData)
-    }
 
     return (
         <DataGrid
@@ -110,3 +107,5 @@ export const TallyTrusteesList: React.FC<TallyTrusteesListProps> = (props) => {
         />
     )
 }
+
+
