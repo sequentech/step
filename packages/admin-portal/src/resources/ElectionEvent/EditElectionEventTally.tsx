396f3acbb2574e266d19cc8a91c1bbeed20393e7
Enric Badia <enric.badia@gmail.com>
2023-12-09 15:14:15 +0000

    initial tally ceremony

--- packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTally.tsx
+++ packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTally.tsx
@@ -7,18 +7,17 @@ import {Sequent_Backend_Election_Event, Sequent_Backend_Tally_Session} from "@/g
 import {Box, Button} from "@mui/material"
 import React, {useState} from "react"
 import {useRecordContext} from "react-admin"
-import { ListTally } from '../Tally/ListTally'
-import { useElectionEventTallyStore } from '@/providers/ElectionEventTallyProvider'
-import { TallyCeremony } from '../Tally/TallyCeremony'
+import {ListTally} from "../Tally/ListTally"
+import {useElectionEventTallyStore} from "@/providers/ElectionEventTallyProvider"
+import {TallyCeremony} from "../Tally/TallyCeremony"
 
 export const EditElectionEventTally: React.FC = () => {
     const recordEvent = useRecordContext<Sequent_Backend_Election_Event>()
     const record = useRecordContext<Sequent_Backend_Tally_Session>()
     const [showStartTallyDialog, setShowStartTallyDialog] = useState(false)
-    const [tallyId] = useElectionEventTallyStore()
+    const [tallyId, setTallyId, isTrustee] = useElectionEventTallyStore()
 
     console.log("EditElectionEventTally :: tallyId :: ", tallyId)
-    
 
     const openStartTallyDialog = () => {
         console.log("opening...")
@@ -34,7 +33,9 @@ export const EditElectionEventTally: React.FC = () => {
             />
             {/* <Button onClick={openStartTallyDialog}>Start tally</Button> */}
             {tallyId ? (
-                <TallyCeremony completed={record.is_execution_completed} />
+                <>
+                    {!isTrustee ? <TallyCeremony completed={record.is_execution_completed} /> : null}
+                </>
             ) : (
                 <ListTally record={record} />
             )}
