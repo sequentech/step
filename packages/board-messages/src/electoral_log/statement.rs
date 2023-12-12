cc47c7f86fde8fd08df2814b76d37aee020b99e3
Eduardo Robles <edu@sequentech.io>
2023-12-12 13:39:09 +0000

    doc

--- packages/board-messages/src/electoral_log/statement.rs
+++ packages/board-messages/src/electoral_log/statement.rs
@@ -48,22 +48,47 @@ impl StatementHead {
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub enum StatementBody {
-    // "Emisión de voto (sólo como registro que el sistema almacenó correctamente el voto)"
+    // NOT IMPLEMENTED YET, but please feel free
+    // "Emisión de voto (sólo como registro que el sistema almacenó correctamente el voto)
     CastVote(ContestHash, PseudonymHash, CastVoteHash),
+    // NOT IMPLEMENTED YET, but please feel free
     // "Errores en la emisión del voto."
     CastVoteError(PseudonymHash, ContestHash),
+    // /workspaces/backend-services/packages/harvest/src/main.rs
+    //    routes::ballot_publication::publish_ballot
+    //
     // "Publicación, apertura y cierre de las elecciones"
     ElectionPublish,
+    // /workspaces/backend-services/packages/harvest/src/main.rs
+    //    routes::voting_status::update_event_status,
+    //    routes::voting_status::update_election_status,
+    //
     // "Publicación, apertura y cierre de las elecciones"
     ElectionPeriodOpen,
+    // /workspaces/backend-services/packages/harvest/src/main.rs
+    //    routes::voting_status::update_event_status,
+    //    routes::voting_status::update_election_status,
+    //
     // "Publicación, apertura y cierre de las elecciones"
     ElectionPeriodClose,
+    // /workspaces/backend-services/packages/windmill/src/celery_app.rs
+    // create_keys
+    //
     // "Creación de llave criptográfica"
     KeyGeneration,
+    // /workspaces/backend-services/packages/harvest/src/main.rs
+    // routes::tally_ceremony::restore_private_key
+    //
     // "Ingreso de los fragmentos de la llave privada"
     KeyInsertionCeremony,
+    // /workspaces/backend-services/packages/windmill/src/celery_app.rs
+    // tally_election_event
+    //
     // "Apertura y cierre de la bóveda de votos"
     BallotBoxOpen,
+    // /workspaces/backend-services/packages/windmill/src/celery_app.rs
+    // execute_tally_session: falta que Felix ponga SUCCESS cuando se termine, creo, hablar con felix
+    //
     // "Apertura y cierre de la bóveda de votos"
     BallotBoxClose,
 }
