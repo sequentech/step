e8dc191becde709ae8880520418720689239e69b
Enric Badia <enric.badia@gmail.com>
2023-12-12 19:27:53 +0000

    new tally flow to results

--- packages/admin-portal/src/resources/Tally/TallyCeremony.tsx
+++ packages/admin-portal/src/resources/Tally/TallyCeremony.tsx
@@ -2,18 +2,11 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 import React, {useEffect, useState} from "react"
-import Button from "@mui/material/Button"
-import {
-    BreadCrumbSteps,
-    BreadCrumbStepsVariant,
-    Dialog,
-    IconButton,
-} from "@sequentech/ui-essentials"
+import {BreadCrumbSteps, BreadCrumbStepsVariant, Dialog} from "@sequentech/ui-essentials"
 import ChevronRightIcon from "@mui/icons-material/ChevronRight"
 import {useTranslation} from "react-i18next"
 import ElectionHeader from "@/components/ElectionHeader"
 import {useElectionEventTallyStore} from "@/providers/ElectionEventTallyProvider"
-import styled from "@emotion/styled"
 import {Accordion, AccordionDetails, AccordionSummary} from "@mui/material"
 import {ElectionStyles} from "@/components/styles/ElectionStyles"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
@@ -32,13 +25,12 @@ import {UPDATE_TALLY_CEREMONY} from "@/queries/UpdateTallyCeremony"
 import {CREATE_TALLY_CEREMONY} from "@/queries/CreateTallyCeremony"
 import {useMutation} from "@apollo/client"
 import {ITallyExecutionStatus} from "@/types/ceremonies"
-import {faKey} from "@fortawesome/free-solid-svg-icons"
-import {Box} from "@mui/material"
 import {
     Sequent_Backend_Election_Event,
     Sequent_Backend_Keys_Ceremony,
     Sequent_Backend_Tally_Session,
 } from "@/gql/graphql"
+import {CancelButton, NextButton} from "./styles"
 
 const WizardSteps = {
     Start: 0,
@@ -47,16 +39,19 @@ const WizardSteps = {
     Results: 3,
 }
 
+interface IExpanded {
+    [key: string]: boolean
+}
+
 export const TallyCeremony: React.FC = () => {
     const record = useRecordContext<Sequent_Backend_Election_Event>()
-
-    console.log("TallyCeremony :: recordEvent", record)
 
     const {t} = useTranslation()
     const {tallyId, setTallyId, setCreatingFlag} = useElectionEventTallyStore()
     const notify = useNotify()
 
     const [openModal, setOpenModal] = useState(false)
+    const [openCeremonyModal, setOpenCeremonyModal] = useState(false)
     const [page, setPage] = useState<number>(WizardSteps.Start)
     const [tally, setTally] = useState<Sequent_Backend_Tally_Session>()
     const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true)
@@ -67,9 +62,16 @@ export const TallyCeremony: React.FC = () => {
     const [CreateTallyCeremonyMutation] = useMutation(CREATE_TALLY_CEREMONY)
     const [UpdateTallyCeremonyMutation] = useMutation(UPDATE_TALLY_CEREMONY)
 
-    interface IExpanded {
-        [key: string]: boolean
-    }
+    const [expandedData, setExpandedData] = useState<IExpanded>({
+        "tally-data-general": true,
+        "tally-data-logs": true,
+        "tally-data-results": true,
+    })
+
+    const [expandedResults, setExpandedResults] = useState<IExpanded>({
+        "tally-results-general": true,
+        "tally-results-results": true,
+    })
 
     const {data} = useGetOne<Sequent_Backend_Tally_Session>(
         "sequent_backend_tally_session",
@@ -93,11 +95,12 @@ export const TallyCeremony: React.FC = () => {
         if (data) {
             if (tally?.last_updated_at !== data.last_updated_at) {
                 setPage(
-                    data.execution_status === ITallyExecutionStatus.STARTED
+                    data.execution_status === ITallyExecutionStatus.STARTED ||
+                        data.execution_status === ITallyExecutionStatus.CONNECTED
                         ? WizardSteps.Ceremony
-                        : data.execution_status === ITallyExecutionStatus.CONNECTED
-                        ? WizardSteps.Tally
                         : data.execution_status === ITallyExecutionStatus.IN_PROGRESS
+                        ? WizardSteps.Tally
+                        : data.execution_status === ITallyExecutionStatus.SUCCESS
                         ? WizardSteps.Results
                         : WizardSteps.Start
                 )
@@ -106,10 +109,6 @@ export const TallyCeremony: React.FC = () => {
         }
     }, [data])
 
-    // useEffect(() => {
-    //     console.log("TallyCeremony selectedTrustees EFFECT:: page", page)
-    // }, [page])
-
     useEffect(() => {
         setIsButtonDisabled(
             page === WizardSteps.Start && selectedElections.length === 0 ? true : false
@@ -117,48 +116,25 @@ export const TallyCeremony: React.FC = () => {
     }, [selectedElections])
 
     useEffect(() => {
-        setIsButtonDisabled(page === WizardSteps.Ceremony && !selectedTrustees)
+        console.log("TallyCeremony :: status :: ", tally?.execution_status)
     }, [selectedTrustees])
 
-    const [expandedData, setExpandedData] = useState<IExpanded>({
-        "tally-data-general": true,
-        "tally-data-logs": true,
-        "tally-data-results": true,
-    })
+    useEffect(() => {
+        console.log("TallyCeremony :: isButtonDisabled :: ", isButtonDisabled)
+    }, [isButtonDisabled])
 
-    const [expandedResults, setExpandedResults] = useState<IExpanded>({
-        "tally-results-general": true,
-        "tally-results-results": true,
-    })
-
-    const CancelButton = styled(Button)`
-        background-color: ${({theme}) => theme.palette.white};
-        color: ${({theme}) => theme.palette.brandColor};
-        border-color: ${({theme}) => theme.palette.brandColor};
-        padding: 0 4rem;
-
-        &:hover {
-            background-color: ${({theme}) => theme.palette.brandColor};
+    useEffect(() => {
+        if (page === WizardSteps.Ceremony) {
+            console.log("cambiame el button coño")
+            setIsButtonDisabled(tally?.execution_status !== ITallyExecutionStatus.CONNECTED)
         }
-    `
-
-    const NextButton = styled(Button)`
-        background-color: ${({theme}) => theme.palette.brandColor};
-        color: ${({theme}) => theme.palette.white};
-        border-color: ${({theme}) => theme.palette.brandColor};
-        padding: 0 4rem;
-
-        &:hover {
-            background-color: ${({theme}) => theme.palette.white};
-            color: ${({theme}) => theme.palette.brandColor};
-        }
-    `
+    }, [tally])
 
     const handleNext = () => {
         if (page === WizardSteps.Start) {
             setOpenModal(true)
         } else if (page === WizardSteps.Ceremony) {
-            // setOpenModal(true)
+            setOpenCeremonyModal(true)
         } else if (page === WizardSteps.Tally) {
             setPage(page < 2 ? page + 1 : 0)
         } else {
@@ -209,50 +185,32 @@ export const TallyCeremony: React.FC = () => {
         }
     }
 
-    // const confirmNextAction = async () => {
-    //     let nextAction =
-    //         tally?.execution_status === ITallyExecutionStatus.STARTED
-    //             ? ITallyExecutionStatus.IN_PROGRESS
-    //             : ITallyExecutionStatus.STARTED
+    const confirmCeremonyAction = async () => {
+        try {
+            if (selectedTrustees) {
+                const {data: nextStatus, errors} = await UpdateTallyCeremonyMutation({
+                    variables: {
+                        election_event_id: record?.id,
+                        tally_session_id: tallyId,
+                        status: ITallyExecutionStatus.IN_PROGRESS,
+                    },
+                })
 
-    //     const {data, errors} = await UpdateTallyCeremonyMutation({
-    //         variables: {
-    //             election_event_id: tally?.election_event_id,
-    //             tally_session_id: tally?.id,
-    //             status: nextAction,
-    //         },
-    //     })
+                if (errors) {
+                    notify(t("tally.startTallyError"), {type: "error"})
+                }
 
-    //     if (errors) {
-    //         notify(t("tally.startTallyError"), {type: "error"})
-    //     }
-
-    //     if (data) {
-    //         notify(t("tally.startTallySuccess"), {type: "success"})
-    //         setShowTrustees(true)
-    //     }
-    //     setShowTrustees(true)
-    // }
-
-    // const setDisabled = (): boolean => {
-
-    //     console.log("TallyCeremony :: setDisabled", page, selectedElections.length);
-
-    //     if (!tally) {
-    //         return true
-    //     }
-    //     if (page === WizardSteps.Start && selectedElections.length === 0) {
-    //         return true
-    //     }
-    //     if (
-    //         page === WizardSteps.Ceremony &&
-    //         showTrustees &&
-    //         tally?.execution_status !== ITallyExecutionStatus.CONNECTED
-    //     ) {
-    //         return true
-    //     }
-    //     return false
-    // }
+                if (nextStatus) {
+                    notify(t("tally.startTallySuccess"), {type: "success"})
+                    setCreatingFlag(false)
+                    setPage(WizardSteps.Ceremony)
+                }
+            }
+        } catch (error) {
+            console.log("TallyCeremony :: confirmCeremonyAction :: error", error)
+            notify(t("tally.startTallyError"), {type: "error"})
+        }
+    }
 
     return (
         <>
@@ -455,7 +413,7 @@ export const TallyCeremony: React.FC = () => {
             </WizardStyles.WizardWrapper>
 
             <Dialog
-                variant="warning"
+                variant="info"
                 open={openModal}
                 ok={t("tally.common.dialog.ok")}
                 cancel={t("tally.common.dialog.cancel")}
@@ -468,6 +426,22 @@ export const TallyCeremony: React.FC = () => {
                 }}
             >
                 {t("tally.common.dialog.message")}
+            </Dialog>
+
+            <Dialog
+                variant="info"
+                open={openCeremonyModal}
+                ok={t("tally.common.dialog.ok")}
+                cancel={t("tally.common.dialog.cancel")}
+                title={t("tally.common.dialog.title")}
+                handleClose={(result: boolean) => {
+                    if (result) {
+                        confirmCeremonyAction()
+                    }
+                    setOpenCeremonyModal(false)
+                }}
+            >
+                {t("tally.common.dialog.ceremony")}
             </Dialog>
         </>
     )
