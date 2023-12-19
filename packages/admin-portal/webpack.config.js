57be5ceeb67d2f8fa0f92cb6ecdf0bf7d2730ec6
Félix Robles <felix@sequentech.io>
2023-12-19 08:14:47 +0000

    wip

--- packages/admin-portal/webpack.config.js
+++ packages/admin-portal/webpack.config.js
@@ -1,40 +1,40 @@
 const path = require("path")
 // webpack.config.js
-const webpack = require('webpack')
-const dotenv = require('dotenv')
+const webpack = require("webpack")
+const dotenv = require("dotenv")
 
 // this will update the process.env with environment variables in .env file
-dotenv.config();
+dotenv.config()
 
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 const ESLintPlugin = require("eslint-webpack-plugin")
 const {ProgressPlugin} = require("webpack")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 class InterpolateHtmlPlugin {
     // Replaces %VARIABLE% with the corresponding variable from the replacements object
     constructor(replacements) {
-      this.replacements = replacements;
+        this.replacements = replacements
     }
-  
+
     apply(compiler) {
-      compiler.hooks.compilation.tap('InterpolateHtmlPlugin', (compilation) => {
-        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
-          'InterpolateHtmlPlugin', // The name of this plugin
-          (data, cb) => {
-            // Use the variable values to replace the placeholders
-            Object.keys(this.replacements).forEach(key => {
-              const value = this.replacements[key];
-              // Create a global regular expression to find all instances
-              data.html = data.html.replace(new RegExp(`%${key}%`, 'g'), value);
-            });
-            cb(null, data);
-          }
-        );
-      });
+        compiler.hooks.compilation.tap("InterpolateHtmlPlugin", (compilation) => {
+            HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+                "InterpolateHtmlPlugin", // The name of this plugin
+                (data, cb) => {
+                    // Use the variable values to replace the placeholders
+                    Object.keys(this.replacements).forEach((key) => {
+                        const value = this.replacements[key]
+                        // Create a global regular expression to find all instances
+                        data.html = data.html.replace(new RegExp(`%${key}%`, "g"), value)
+                    })
+                    cb(null, data)
+                }
+            )
+        })
     }
-  }
+}
 
 module.exports = function (env, argv) {
     return {
@@ -43,7 +43,7 @@ module.exports = function (env, argv) {
         output: {
             filename: "index.js",
             path: path.resolve(__dirname, "dist"),
-            publicPath: '/', // Set to empty string to ensure correct base path
+            publicPath: "/", // Set to empty string to ensure correct base path
         },
         devtool: "source-map",
         module: {
@@ -64,8 +64,7 @@ module.exports = function (env, argv) {
                 },
             ],
         },
-        externals: {
-        },
+        externals: {},
         resolve: {
             alias: {
                 "@root": path.resolve(__dirname, "src"),
@@ -75,30 +74,30 @@ module.exports = function (env, argv) {
         },
         plugins: [
             new InterpolateHtmlPlugin({
-                'PUBLIC_URL': '' // Provide replacements for variables
+                PUBLIC_URL: "", // Provide replacements for variables
             }),
             new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public/index.html'),
-                favicon: path.resolve(__dirname, 'public/favicon.ico'),
-                filename: './index.html',
-                favicon: './public/favicon.ico',
+                template: path.resolve(__dirname, "public/index.html"),
+                favicon: path.resolve(__dirname, "public/favicon.ico"),
+                filename: "./index.html",
+                favicon: "./public/favicon.ico",
                 // pass variables to the template
                 templateParameters: {
-                    'PUBLIC_URL': '' // Replace %PUBLIC_URL% with an empty string
-                }
+                    PUBLIC_URL: "", // Replace %PUBLIC_URL% with an empty string
+                },
             }),
 
             // Configure CopyWebpackPlugin to include a list of files from 'public/' into 'dist/'
             new CopyWebpackPlugin({
                 patterns: [
                     {
-                        from: path.resolve(__dirname, 'public'), // Source folder
-                        to: path.resolve(__dirname, 'dist'), // Destination folder
+                        from: path.resolve(__dirname, "public"), // Source folder
+                        to: path.resolve(__dirname, "dist"), // Destination folder
                         globOptions: {
                             ignore: [
                                 // Ignore all .html and .ico files (as examples, you can modify as needed)
-                                '**/index.html',
-                                '**/favicon.ico',
+                                "**/index.html",
+                                "**/favicon.ico",
                             ],
                         },
                     },
@@ -110,16 +109,16 @@ module.exports = function (env, argv) {
             }),
             new CleanWebpackPlugin(),
             new webpack.DefinePlugin({
-                'process.env': JSON.stringify(process.env)
+                "process.env": JSON.stringify(process.env),
             }),
         ],
         devServer: {
             static: {
                 directory: path.resolve(__dirname, "dist"),
             },
-            compress: true,  // Enable gzip compression
-            port: 3002,      // Run on port 3002
-            open: true,      // Automatically open the browser
+            compress: true, // Enable gzip compression
+            port: 3002, // Run on port 3002
+            open: true, // Automatically open the browser
             historyApiFallback: true,
         },
     }
--- packages/voting-portal/webpack.config.js
+++ packages/voting-portal/webpack.config.js
@@ -3,32 +3,32 @@ const path = require("path")
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 const ESLintPlugin = require("eslint-webpack-plugin")
 const {ProgressPlugin} = require("webpack")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 class InterpolateHtmlPlugin {
     // Replaces %VARIABLE% with the corresponding variable from the replacements object
     constructor(replacements) {
-      this.replacements = replacements;
+        this.replacements = replacements
     }
-  
+
     apply(compiler) {
-      compiler.hooks.compilation.tap('InterpolateHtmlPlugin', (compilation) => {
-        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
-          'InterpolateHtmlPlugin', // The name of this plugin
-          (data, cb) => {
-            // Use the variable values to replace the placeholders
-            Object.keys(this.replacements).forEach(key => {
-              const value = this.replacements[key];
-              // Create a global regular expression to find all instances
-              data.html = data.html.replace(new RegExp(`%${key}%`, 'g'), value);
-            });
-            cb(null, data);
-          }
-        );
-      });
+        compiler.hooks.compilation.tap("InterpolateHtmlPlugin", (compilation) => {
+            HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+                "InterpolateHtmlPlugin", // The name of this plugin
+                (data, cb) => {
+                    // Use the variable values to replace the placeholders
+                    Object.keys(this.replacements).forEach((key) => {
+                        const value = this.replacements[key]
+                        // Create a global regular expression to find all instances
+                        data.html = data.html.replace(new RegExp(`%${key}%`, "g"), value)
+                    })
+                    cb(null, data)
+                }
+            )
+        })
     }
-  }
+}
 
 module.exports = function (env, argv) {
     return {
@@ -37,7 +37,7 @@ module.exports = function (env, argv) {
         output: {
             filename: "index.js",
             path: path.resolve(__dirname, "dist"),
-            publicPath: '/', // Set to empty string to ensure correct base path
+            publicPath: "/", // Set to empty string to ensure correct base path
         },
         devtool: "source-map",
         module: {
@@ -58,8 +58,7 @@ module.exports = function (env, argv) {
                 },
             ],
         },
-        externals: {
-        },
+        externals: {},
         resolve: {
             alias: {
                 "@root": path.resolve(__dirname, "src"),
@@ -69,30 +68,30 @@ module.exports = function (env, argv) {
         },
         plugins: [
             new InterpolateHtmlPlugin({
-                'PUBLIC_URL': '' // Provide replacements for variables
+                PUBLIC_URL: "", // Provide replacements for variables
             }),
             new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, 'public/index.html'),
-                favicon: path.resolve(__dirname, 'public/favicon.ico'),
-                filename: './index.html',
-                favicon: './public/favicon.ico',
+                template: path.resolve(__dirname, "public/index.html"),
+                favicon: path.resolve(__dirname, "public/favicon.ico"),
+                filename: "./index.html",
+                favicon: "./public/favicon.ico",
                 // pass variables to the template
                 templateParameters: {
-                    'PUBLIC_URL': '' // Replace %PUBLIC_URL% with an empty string
-                }
+                    PUBLIC_URL: "", // Replace %PUBLIC_URL% with an empty string
+                },
             }),
 
             // Configure CopyWebpackPlugin to include a list of files from 'public/' into 'dist/'
             new CopyWebpackPlugin({
                 patterns: [
                     {
-                        from: path.resolve(__dirname, 'public'), // Source folder
-                        to: path.resolve(__dirname, 'dist'), // Destination folder
+                        from: path.resolve(__dirname, "public"), // Source folder
+                        to: path.resolve(__dirname, "dist"), // Destination folder
                         globOptions: {
                             ignore: [
                                 // Ignore all .html and .ico files (as examples, you can modify as needed)
-                                '**/index.html',
-                                '**/favicon.ico',
+                                "**/index.html",
+                                "**/favicon.ico",
                             ],
                         },
                     },
@@ -108,9 +107,9 @@ module.exports = function (env, argv) {
             static: {
                 directory: path.resolve(__dirname, "dist"),
             },
-            compress: true,  // Enable gzip compression
-            port: 3000,      // Run on port 3000
-            open: true,      // Automatically open the browser
+            compress: true, // Enable gzip compression
+            port: 3000, // Run on port 3000
+            open: true, // Automatically open the browser
             historyApiFallback: true,
         },
     }
