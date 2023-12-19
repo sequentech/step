ccafbb6a442405143ea2eff726d70af798ca931d
Félix Robles <felix@sequentech.io>
2023-12-19 07:38:18 +0000

    wip

--- packages/ballot-verifier/webpack.config.js
+++ packages/ballot-verifier/webpack.config.js
@@ -3,6 +3,32 @@ const path = require("path")
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 const ESLintPlugin = require("eslint-webpack-plugin")
 const {ProgressPlugin} = require("webpack")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+class InterpolateHtmlPlugin {
+    // Replaces %VARIABLE% with the corresponding variable from the replacements object
+    constructor(replacements) {
+      this.replacements = replacements;
+    }
+  
+    apply(compiler) {
+      compiler.hooks.compilation.tap('InterpolateHtmlPlugin', (compilation) => {
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+          'InterpolateHtmlPlugin', // The name of this plugin
+          (data, cb) => {
+            // Use the variable values to replace the placeholders
+            Object.keys(this.replacements).forEach(key => {
+              const value = this.replacements[key];
+              // Create a global regular expression to find all instances
+              data.html = data.html.replace(new RegExp(`%${key}%`, 'g'), value);
+            });
+            cb(null, data);
+          }
+        );
+      });
+    }
+  }
 
 module.exports = function (env, argv) {
     return {
@@ -11,13 +37,15 @@ module.exports = function (env, argv) {
         output: {
             filename: "index.js",
             path: path.resolve(__dirname, "dist"),
+            publicPath: '/', // Set to empty string to ensure correct base path
         },
         devtool: "source-map",
         module: {
             rules: [
                 {
-                    test: /\.css$/,
-                    use: ["style-loader", "css-loader"],
+                    test: /\.css$/i,
+                    include: path.resolve(__dirname, "src"),
+                    use: ["style-loader", "css-loader", "postcss-loader"],
                 },
                 {
                     test: /\.(js|ts)x?$/,
@@ -31,14 +59,6 @@ module.exports = function (env, argv) {
             ],
         },
         externals: {
-            /*"react": "react",
-            "react-dom": "react-dom",
-            "@mui/material": "@mui/material",
-            "@emotion/react": "@emotion/react",
-            "@emotion/styled": "@emotion/styled",
-            "mui-image": "mui-image",
-            "react-i18next": "react-i18next",
-            "react-router-dom": "react-router-dom",*/
         },
         resolve: {
             alias: {
@@ -48,11 +68,50 @@ module.exports = function (env, argv) {
             extensions: [".js", ".jsx", ".ts", ".tsx"],
         },
         plugins: [
+            new InterpolateHtmlPlugin({
+                'PUBLIC_URL': '' // Provide replacements for variables
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public/index.html'),
+                favicon: path.resolve(__dirname, 'public/favicon.ico'),
+                filename: './index.html',
+                favicon: './public/favicon.ico',
+                // pass variables to the template
+                templateParameters: {
+                    'PUBLIC_URL': '' // Replace %PUBLIC_URL% with an empty string
+                }
+            }),
+
+            // Configure CopyWebpackPlugin to include a list of files from 'public/' into 'dist/'
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'public'), // Source folder
+                        to: path.resolve(__dirname, 'dist'), // Destination folder
+                        globOptions: {
+                            ignore: [
+                                // Ignore all .html and .ico files (as examples, you can modify as needed)
+                                '**/index.html',
+                                '**/favicon.ico',
+                            ],
+                        },
+                    },
+                ],
+            }),
             new ProgressPlugin(),
             new ESLintPlugin({
                 extensions: [".js", ".jsx", ".ts", ".tsx"],
             }),
             new CleanWebpackPlugin(),
         ],
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, "dist"),
+            },
+            compress: true,  // Enable gzip compression
+            port: 3001,      // Run on port 3001
+            open: true,      // Automatically open the browser
+            historyApiFallback: true,
+        },
     }
 }
