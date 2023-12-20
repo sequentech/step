17df406213f191c64fdda6b23e9a9a5576658825
David Ruescas <david@sequentech.io>
2023-12-20 05:59:43 +0100

    ✨ Use proper timestamps in messages/braid and immu-board (#235)
    - [x]  https://github.com/sequentech/meta/issues/389
    - [x]  https://github.com/sequentech/meta/issues/411
    
    ---------
    
    Co-authored-by: Eduardo Robles <edu@sequentech.io>

--- packages/braid/src/protocol2/session.rs
+++ packages/braid/src/protocol2/session.rs
@@ -1,17 +1,18 @@
 use crate::protocol2::board::immudb::ImmudbBoard;
 use crate::protocol2::trustee::Trustee;
 use anyhow::Result;
+use std::path::PathBuf;
 use strand::context::Ctx;
 use tracing::{info, warn};
 
 pub struct Session<C: Ctx + 'static> {
     trustee: Trustee<C>,
-    board: ImmudbBoard,
+    board: BoardParams,
     dry_run: bool,
     last_message_id: i64,
 }
 impl<C: Ctx> Session<C> {
-    pub fn new(trustee: Trustee<C>, board: ImmudbBoard) -> Session<C> {
+    pub fn new(trustee: Trustee<C>, board: BoardParams) -> Session<C> {
         Session {
             trustee,
             board,
@@ -20,7 +21,7 @@ impl<C: Ctx> Session<C> {
         }
     }
 
-    pub fn new_dry(trustee: Trustee<C>, board: ImmudbBoard) -> Session<C> {
+    pub fn new_dry(trustee: Trustee<C>, board: BoardParams) -> Session<C> {
         Session {
             trustee,
             board,
@@ -33,7 +34,8 @@ impl<C: Ctx> Session<C> {
     // See https://stackoverflow.com/questions/63434977/how-can-i-spawn-asynchronous-methods-in-a-loop
     // See also protocol_test_immudb::run_protocol_test_immudb
     pub async fn step(mut self) -> Result<Self> {
-        let messages = self.board.get_messages(self.last_message_id).await?;
+        let mut board = self.board.get_board().await?;
+        let messages = board.get_messages(self.last_message_id).await?;
 
         if 0 == messages.len() {
             info!("No messages in board, no action taken");
@@ -42,7 +44,7 @@ impl<C: Ctx> Session<C> {
 
         let (send_messages, _actions) = self.trustee.step(messages)?;
         if !self.dry_run {
-            let result = self.board.insert_messages(send_messages).await;
+            let result = board.insert_messages(send_messages).await;
             match result {
                 Ok(_) => (),
                 Err(err) => {
@@ -52,5 +54,41 @@ impl<C: Ctx> Session<C> {
         }
 
         Ok(self)
+    }
+}
+
+pub struct BoardParams {
+    server_url: String,
+    user: String,
+    password: String,
+    board_name: String,
+    store_root: Option<PathBuf>,
+}
+impl BoardParams {
+    pub fn new(
+        server_url: &str,
+        user: &str,
+        password: &str,
+        board_dbname: &str,
+        store_root: Option<PathBuf>,
+    ) -> BoardParams {
+        BoardParams {
+            server_url: server_url.to_string(),
+            user: user.to_string(),
+            password: password.to_string(),
+            board_name: board_dbname.to_string(),
+            store_root: store_root,
+        }
+    }
+
+    pub async fn get_board(&self) -> Result<ImmudbBoard> {
+        ImmudbBoard::new(
+            &self.server_url,
+            &self.user,
+            &self.password,
+            self.board_name.to_string(),
+            self.store_root.clone(),
+        )
+        .await
     }
 }
