c1e12ac4a56d0bd7744e7114e327e189d5354cda
Félix Robles <felix@sequentech.io>
2024-01-01 12:44:28 +0000

    add some types

---
+++ packages/ui-essentials/src/types/ElectionEventPresentation.ts
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+export interface IElectionEventMaterials {
+    activated?: boolean
+}
+
+export interface IElectionEventLanguageConf {
+    enabled_language_codes?: Array<string>
+}
+
+export interface IElectionEventPresentation {
+    i18n?: Record<string, string>
+    materials?: IElectionEventMaterials
+    language_conf: IElectionEventLanguageConf
+}
