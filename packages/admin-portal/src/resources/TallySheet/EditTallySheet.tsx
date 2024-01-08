46610b04d44f8c65b6d7cb85c72b7780f5b38861
Enric Badia <enric.badia@gmail.com>
2024-01-08 10:23:19 +0000

    lint and pretty

--- packages/admin-portal/src/resources/TallySheet/EditTallySheet.tsx
+++ packages/admin-portal/src/resources/TallySheet/EditTallySheet.tsx
@@ -222,7 +222,7 @@ export const EditTallySheet: React.FC<EditTallySheetProps> = (props) => {
             tallySheetData.id = tallySheet.id
         }
 
-        if (doCreatedTallgit ySheet) {
+        if (doCreatedTallySheet) {
             localStorage.setItem("tallySheetData", JSON.stringify(tallySheetData))
             doCreatedTallySheet(tallySheetData)
         }
