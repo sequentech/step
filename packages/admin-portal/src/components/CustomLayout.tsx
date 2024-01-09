721d4db862eee5e672f3658c081b19ed41e80ead
Enric Badia <enric.badia@gmail.com>
2024-01-09 18:58:31 +0000

    added custom sidebar overide to avoid menu hide

--- packages/admin-portal/src/components/CustomLayout.tsx
+++ packages/admin-portal/src/components/CustomLayout.tsx
@@ -2,17 +2,29 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 import React from "react"
-import {Layout, LayoutProps} from "react-admin"
+import {Layout, LayoutProps, SidebarClasses, useSidebarState} from "react-admin"
 import {CustomAppBar} from "./CustomAppBar"
 import {CustomMenu} from "./CustomMenu"
 import {adminTheme} from "@sequentech/ui-essentials"
+import {Drawer} from "@mui/material"
+import { CustomSidebar } from './menu/CustomSidebar'
+
+const SequentSidebar = (props: any) => {
+    return (
+        <CustomSidebar
+            {...props}
+        >
+                <CustomMenu {...props} classes={SidebarClasses} />
+        </CustomSidebar>
+    )
+}
 
 export const CustomLayout: React.FC<LayoutProps> = (props) => (
     <Layout
         {...props}
         sx={{
             "& .MuiPaper-root": {
-                top: "0px",
+                top: "0",
                 position: "sticky",
                 zIndex: 100,
             },
@@ -24,6 +36,7 @@ export const CustomLayout: React.FC<LayoutProps> = (props) => (
             },
         }}
         appBar={CustomAppBar}
-        menu={CustomMenu}
+        // menu={CustomMenu}
+        sidebar={SequentSidebar}
     />
 )
