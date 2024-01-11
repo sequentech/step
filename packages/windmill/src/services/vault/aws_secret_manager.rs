742a8356a67d6bfc1fa2721ac52a787d1cd8ed54
Kevin Nguyen <knth@hey.com>
2024-01-11 13:48:40 +0100

    feat: implement aws_secret_manager

--- packages/windmill/src/services/vault/aws_secret_manager.rs
+++ packages/windmill/src/services/vault/aws_secret_manager.rs
@@ -1,7 +1,26 @@
-pub(crate) fn save_secret(key: _, value: _) -> _ {
-    todo!()
+use crate::util::aws::get_from_env_aws_config;
+use anyhow::Result;
+use aws_sdk_secretsmanager::Client;
+
+pub async fn save_secret(key: String, value: String) -> Result<()> {
+    let shared_config = get_from_env_aws_config().await?;
+    let client = Client::new(&shared_config);
+
+    client
+        .create_secret()
+        .name(key)
+        .secret_string(value)
+        .send()
+        .await?;
+
+    Ok(())
 }
 
-pub(crate) async fn read_secret(key: _) -> _ {
-    todo!()
+pub async fn read_secret(key: String) -> Result<Option<String>> {
+    let shared_config = get_from_env_aws_config().await?;
+    let client = Client::new(&shared_config);
+
+    let resp = client.get_secret_value().secret_id(key).send().await?;
+
+    Ok(resp.secret_string().map(|s| s.to_string()))
 }
