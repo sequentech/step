3a85882be3134b8a7ff14157b18cc6c8d389a9d4
Félix Robles <felix@sequentech.io>
2024-01-11 09:17:57 +0000

    fix lint

--- packages/admin-portal/src/queries/FetchDocument.ts
+++ packages/admin-portal/src/queries/FetchDocument.ts
@@ -5,10 +5,7 @@ import {gql} from "@apollo/client"
 
 export const FETCH_DOCUMENT = gql`
     query FetchDocument($electionEventId: String!, $documentId: String!) {
-        fetchDocument(
-            election_event_id: $electionEventId
-            document_id: $documentId
-        ) {
+        fetchDocument(election_event_id: $electionEventId, document_id: $documentId) {
             url
         }
     }
