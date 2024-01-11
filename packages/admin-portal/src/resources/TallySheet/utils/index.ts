0d8e45586c06918367e587d2d5d8761f4cf24fd8
Félix Robles <felix@sequentech.io>
2024-01-11 15:48:32 +0000

    wip

--- packages/admin-portal/src/resources/TallySheet/utils/index.ts
+++ packages/admin-portal/src/resources/TallySheet/utils/index.ts
@@ -1,6 +1,6 @@
 import {isString} from "@sequentech/ui-essentials"
 
-export const sortFunction = (a: {name?: string}, b: {name?: string}) => {
+export const sortFunction = (a: {name?: string | null}, b: {name?: string | null}) => {
     if (isString(a?.name) && isString(b?.name)) {
         return a.name.localeCompare(b.name)
     }
