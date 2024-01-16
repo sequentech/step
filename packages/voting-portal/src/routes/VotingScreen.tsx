d69cc89b8aebed857435516de85b8363ca36abb0
Kevin Nguyen <knth@hey.com>
2024-01-16 16:29:10 +0100

    fix: maintain ballot selection

--- packages/voting-portal/src/routes/VotingScreen.tsx
+++ packages/voting-portal/src/routes/VotingScreen.tsx
@@ -157,13 +157,6 @@ const VotingScreen: React.FC = () => {
                         ballotSelection: decodedSelectionState,
                     })
                 )
-
-                dispatch(
-                    resetBallotSelection({
-                        ballotStyle,
-                        force: true,
-                    })
-                )
             }
 
             submit(null, {method: "post"})
