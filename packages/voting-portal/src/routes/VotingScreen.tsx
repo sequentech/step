aeaec073abe6a0be01c4f282c0577e4b70da7df5
Kevin Nguyen <knth@hey.com>
2024-01-25 17:19:53 +0100

    feat: add isUniqChecked for questions

--- packages/voting-portal/src/routes/VotingScreen.tsx
+++ packages/voting-portal/src/routes/VotingScreen.tsx
@@ -255,6 +255,7 @@ const VotingScreen: React.FC = () => {
                     key={index}
                     isReview={false}
                     setDisableNext={onSetDisableNext(contest.id)}
+                    isUniqChecked={true} // TODO: make it configurable
                 />
             ))}
             <ActionButtons handleNext={encryptAndReview} disableNext={skipNextButton} />
