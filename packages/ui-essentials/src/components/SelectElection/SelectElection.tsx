9cddfbc8130c6715fe7c9ddc0b4924155f902f69
Kevin Nguyen <knth@hey.com>
2024-01-25 13:08:52 +0100

    fix: click to vote disable

--- packages/ui-essentials/src/components/SelectElection/SelectElection.tsx
+++ packages/ui-essentials/src/components/SelectElection/SelectElection.tsx
@@ -264,7 +264,7 @@ const SelectElection: React.FC<SelectElectionProps> = ({
                 {isOpen ? (
                     <StyledButton
                         className="click-to-vote-button"
-                        disabled={!handleClickToVote}
+                        disabled={!onClickToVote}
                         onClick={handleClickToVote}
                     >
                         {t("selectElection.voteButton")}
