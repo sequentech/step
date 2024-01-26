f654de1de0a14a0be8e90a0f378ae88228554c49
Félix Robles <felix@sequentech.io>
2024-01-26 00:28:16 +0000

    wip

--- packages/ui-essentials/src/components/Header/Header.tsx
+++ packages/ui-essentials/src/components/Header/Header.tsx
@@ -39,7 +39,7 @@ const StyledLink = styled.a`
 
 const StyledImage = styled(Image)`
     height: 47px !important;
-    @media (min-width: ${theme.breakpoints.values.md}px) {
+    @media (max-width: ${theme.breakpoints.values.md}px) {
         height: 37px !important;
     }
 `
