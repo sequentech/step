21d79c5f96caf5842fa731b72d1ac115f8ff1639
Kevin Nguyen <knth@hey.com>
2024-01-31 11:10:33 +0100

    chore: remove comments

--- packages/voting-portal/src/lib/utils.ts
+++ packages/voting-portal/src/lib/utils.ts
@@ -5,24 +5,11 @@ export interface IContestWithIndex extends IContest {
 }
 
 export function sortContestByCreationDate(contests: IContest[]): IContestWithIndex[] {
-    // contests = cloneDeep(contests)
-    //
-    // contests.sort((a, b) => {
-    //     const dateA = a.created_at ? new Date(a.created_at) : new Date(0)
-    //     const dateB = b.created_at ? new Date(b.created_at) : new Date(0)
-    //
-    //     return dateA.getTime() - dateB.getTime()
-    // })
-    //
-    // return contests
-
-    // Add a new field 'originalIndex' to each contest
     const contestsWithIndex = contests.map((contest, index) => ({
         ...contest,
         originalIndex: index,
     }))
 
-    // Sort the array with the added 'originalIndex' field
     contestsWithIndex.sort((a, b) => {
         const dateA = a.created_at ? new Date(a.created_at) : new Date(0)
         const dateB = b.created_at ? new Date(b.created_at) : new Date(0)
@@ -30,6 +17,5 @@ export function sortContestByCreationDate(contests: IContest[]): IContestWithInd
         return dateA.getTime() - dateB.getTime()
     })
 
-    // Return the sorted array that now includes the 'originalIndex'
     return contestsWithIndex
 }
