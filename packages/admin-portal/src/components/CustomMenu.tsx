8fcccd131cb8bb953b845197fea5d02c50f59a8e
Kevin Nguyen <knth@hey.com>
2024-02-05 12:43:10 +0100

    feat: set height to 100%

--- packages/admin-portal/src/components/CustomMenu.tsx
+++ packages/admin-portal/src/components/CustomMenu.tsx
@@ -37,7 +37,7 @@ const StyledMenu = styled(Menu)`
     box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
         0px 1px 3px 0px rgba(0, 0, 0, 0.12);
     border-radius: 4px;
-    overflow-y: auto;
+    height: 100%;
 `
 
 const DrawerContainer = styled(Box)`
