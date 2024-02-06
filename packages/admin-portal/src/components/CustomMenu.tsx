67aa529c688bb6b30fbca36b472246e9f6593501
Enric Badia <enric.badia@gmail.com>
2024-02-06 17:42:50 +0000

    sidebar scroll fixed

--- packages/admin-portal/src/components/CustomMenu.tsx
+++ packages/admin-portal/src/components/CustomMenu.tsx
@@ -29,72 +29,77 @@ const StyledItem = styled(Menu.Item)`
     }
 `
 
-const StyledMenu = styled(Menu)`
+const StyledMenu = styled(Menu)<{open: boolean}>`
+    position: fixed;
     background-color: ${adminTheme.palette.white};
     color: ${adminTheme.palette.brandColor};
     margin-top: 0px;
     margin-right: 4px;
     box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
         0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-    border-radius: 4px;
-    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    top: 80px;
+    bottom: 36px;
+    left: 0;
+    scrollbar-width: ${({open}) => (open ? "thin" : "none")};
 `
-
-const DrawerContainer = styled(Box)`
-    position: sticky;
+    
+    const DrawerContainer = styled(Box)<{open: boolean}>`
+    position: fixed;
     bottom: 0;
+    left: 0;
     background-color: ${adminTheme.palette.white};
     padding: 8px 16px;
     justify-content: right;
     border-top: 2px solid ${adminTheme.palette.customGrey.light};
     display: flex;
     margin-top: auto;
-`
-
-const MenuWrapper = styled(Box)`
-    border-bottom: 2px solid ${adminTheme.palette.customGrey.light};
-`
+    width: ${({open}) => open ? "300px" : "50px"};
+    `
+    
+    const MenuWrapper = styled(Box)`
+        border-bottom: 2px solid ${adminTheme.palette.customGrey.light};
+    `
 
 export const CustomMenu = () => {
     const [open, setOpen] = useSidebarState()
     const {t} = useTranslation()
 
     return (
-        <StyledMenu>
-            <MenuWrapper>
-                <SelectTenants />
+        <>
+            <StyledMenu open={open}>
+                <MenuWrapper>
+                    <SelectTenants />
 
-                <ElectionEvents />
+                    <ElectionEvents />
 
-                <StyledItem
-                    to="/user-roles"
-                    primaryText={open ? t("sideMenu.usersAndRoles") : null}
-                    leftIcon={<GroupIcon sx={{color: adminTheme.palette.brandColor}} />}
-                />
-                {/*<StyledItem
-                    to="/logs"
-                    primaryText={open ? t("sideMenu.logs") : null}
-                    leftIcon={<ChecklistIcon sx={{color: adminTheme.palette.brandColor}} />}
-                />*/}
-                <StyledItem
-                    to="/settings"
-                    primaryText={open ? t("sideMenu.settings") : null}
-                    leftIcon={<SettingsIcon sx={{color: adminTheme.palette.brandColor}} />}
-                />
-                <StyledItem
-                    to="/sequent_backend_communication_template"
-                    primaryText={open && t("sideMenu.communicationTemplates")}
-                    leftIcon={<MailIcon sx={{color: adminTheme.palette.brandColor}} />}
-                />
-            </MenuWrapper>
+                    <StyledItem
+                        to="/user-roles"
+                        primaryText={open ? t("sideMenu.usersAndRoles") : null}
+                        leftIcon={<GroupIcon sx={{color: adminTheme.palette.brandColor}} />}
+                    />
+                    <StyledItem
+                        to="/settings"
+                        primaryText={open ? t("sideMenu.settings") : null}
+                        leftIcon={<SettingsIcon sx={{color: adminTheme.palette.brandColor}} />}
+                    />
+                    <StyledItem
+                        to="/sequent_backend_communication_template"
+                        primaryText={open && t("sideMenu.communicationTemplates")}
+                        leftIcon={<MailIcon sx={{color: adminTheme.palette.brandColor}} />}
+                    />
+                </MenuWrapper>
 
-            <DrawerContainer>
-                <IconButton
-                    icon={open ? faAngleDoubleLeft : faAngleDoubleRight}
-                    fontSize="24px"
-                    onClick={() => setOpen(!open)}
-                />
-            </DrawerContainer>
-        </StyledMenu>
+                <DrawerContainer open={open}>
+                    <IconButton
+                        icon={open ? faAngleDoubleLeft : faAngleDoubleRight}
+                        fontSize="24px"
+                        onClick={() => setOpen(!open)}
+                    />
+                </DrawerContainer>
+
+            </StyledMenu>
+        </>
     )
 }
