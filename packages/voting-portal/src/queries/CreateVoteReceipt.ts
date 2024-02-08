4b51818f84cabc75660d88c5968e3d9316b0173d
Kevin Nguyen <knth@hey.com>
2024-02-08 18:50:21 +0100

    Revert "feat: update schema"
    This reverts commit 1a632df53d35f0a11d8d49fc890772dc90bb6d6a.

--- packages/voting-portal/src/queries/CreateVoteReceipt.ts
+++ packages/voting-portal/src/queries/CreateVoteReceipt.ts
@@ -6,16 +6,14 @@ import {gql} from "@apollo/client"
 
 export const CREATE_VOTE_RECEIPT = gql`
     mutation CreateVoteReceipt(
-        $ballotId: String!
-        $ballotTrackerUrl: String!
         $election_event_id: String!
         $tenant_id: String!
+        $ballotId: String!
     ) {
         create_vote_receipt(
-            ballot_id: $ballotId
-            $ballot_tracker_url: $ballotTrackerUrl
             election_event_id: $election_event_id
             tenant_id: $tenant_id
+            ballot_id: $ballotId
         ) {
             id
             ballot_id
