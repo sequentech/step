38a38f81ca540cd5ab5cde06583d493f64e5b46f
Kevin Nguyen <knth@hey.com>
2024-02-14 18:58:04 +0100

    feat: revert test

--- packages/harvest/src/routes/fetch_document.rs
+++ packages/harvest/src/routes/fetch_document.rs
@@ -32,12 +32,12 @@ pub async fn fetch_document(
     body: Json<GetDocumentUrlBody>,
     claims: JwtClaims,
 ) -> Result<Json<GetDocumentUrlResponse>, (Status, String)> {
-    // authorize(
-    //     &claims,
-    //     true,
-    //     Some(claims.hasura_claims.tenant_id.clone()),
-    //     vec![Permissions::DOCUMENT_DOWNLOAD],
-    // )?;
+    authorize(
+        &claims,
+        true,
+        Some(claims.hasura_claims.tenant_id.clone()),
+        vec![Permissions::DOCUMENT_DOWNLOAD],
+    )?;
     let input = body.into_inner();
     let url = documents::fetch_document(
         claims.hasura_claims.tenant_id.clone(),
