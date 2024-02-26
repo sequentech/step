3e9a4b5b4e71bb1d27f72ff2713fe565503670c7
Kevin Nguyen <knth@hey.com>
2024-02-26 13:15:34 +0100

    feat: update test

--- packages/windmill/src/test.rs
+++ packages/windmill/src/test.rs
@@ -1,10 +1,12 @@
 use crate::services::database::get_hasura_pool;
 use crate::services::s3;
+use crate::services::temp_path::write_into_named_temp_file;
 use crate::services::vote_receipt;
 use crate::services::vote_receipt::VoteReceiptData;
 use crate::services::vote_receipt::VoteReceiptRoot;
 use anyhow::{anyhow, Context, Result};
 use celery::error::TaskError;
+use sequent_core::services::pdf;
 use sequent_core::services::reports;
 use std::env;
 use tracing::instrument;
@@ -76,21 +78,59 @@ pub async fn testing() -> Result<()> {
         ballot_tracker_url: "https://localhost/".to_string(),
         qrcode: QR_CODE_TEMPLATE.to_string(),
         logo: LOGO_TEMPLATE.to_string(),
-        file_logo: format!("{}/{}", minio_endpoint_base, file_logo),
-        file_qrcode_lib: format!("{}/{}", minio_endpoint_base, file_qrcode_lib),
+        file_logo: format!(
+            "{}/{}/{}",
+            minio_endpoint_base, public_asset_path, file_logo
+        ),
+        file_qrcode_lib: format!(
+            "{}/{}/{}",
+            minio_endpoint_base, public_asset_path, file_qrcode_lib
+        ),
         title: vote_receipt_title.to_string(),
         template: None, // TODO
     };
 
     let sub_map = VoteReceiptRoot { data: data.clone() }.to_map()?;
     let computed_template = reports::render_template_text(&template, sub_map)?;
-    dbg!(&computed_template);
 
     data.template = Some(computed_template.clone());
 
     let map = VoteReceiptRoot { data: data.clone() }.to_map()?;
     let computed_template_final = reports::render_template_text(&template_hbs, map)?;
     dbg!(&computed_template_final);
+
+    let file_path = "output.html";
+    std::fs::write(file_path, computed_template_final.clone())
+        .map_err(|err| anyhow!("Failed to write PDF to file: {}", err))?;
+
+
+    // Gen pdf
+    let bytes_pdf = pdf::html_to_pdf(computed_template_final).map_err(|err| anyhow!("{}", err))?;
+
+    let file_path = "output.pdf";
+    std::fs::write(file_path, bytes_pdf)
+        .map_err(|err| anyhow!("Failed to write PDF to file: {}", err))?;
+
+    // let (temp_path, temp_path_string, file_size) =
+    //     write_into_named_temp_file(&bytes_pdf, "vote-receipt-", ".html")
+    //         .with_context(|| "Error writing to file")?;
+    //
+    // dbg!(&temp_path_string);
+    // dbg!(&temp_path);
+
+    // let auth_headers = keycloak::get_client_credentials().await?;
+    // let _document = upload_and_return_document(
+    //     temp_path_string,
+    //     file_size,
+    //     "application/pdf".to_string(),
+    //     auth_headers.clone(),
+    //     tenant_id.to_string(),
+    //     election_event_id.to_string(),
+    //     format!("vote-receipt-{ballot_id}.pdf"),
+    //     Some(element_id.to_string()),
+    //     true,
+    // )
+    // .await?;
 
     Ok(())
 }
