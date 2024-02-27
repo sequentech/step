200a1f9a152253453272372a5618eb0c8fdffa9d
Kevin Nguyen <knth@hey.com>
2024-02-27 13:00:23 +0100

    feat: update entrypoint.sh

--- .devcontainer/minio/entrypoint.sh
+++ .devcontainer/minio/entrypoint.sh
@@ -5,19 +5,22 @@ mc mb -p myminio/$MINIO_PUBLIC_BUCKET;
 mc mb -p myminio/$MINIO_BUCKET;
 mc anonymous set download myminio/$MINIO_PUBLIC_BUCKET;
 mc admin user svcacct add --access-key $MINIO_ACCESS_KEY --secret-key $MINIO_ACCESS_SECRET myminio $MINIO_ROOT_USER;
-mc stat myminio/public/certs.json
 
+mc stat myminio/public/certs.json
 if [ $? -eq 1 ]; then
-    echo uploading file
+    echo "Uploading certs.json..."    
     mc cp /scripts/certs.json myminio/public/certs.json
 else
-    echo file already exists
+    echo "certs.json already exists."
 fi
 
-
-FOLDER_PATH="./public-assets" 
-DESTINATION="myminio/$MINIO_PUBLIC_BUCKET/public-assets" 
-echo "Uploading folder $FOLDER_PATH to $DESTINATION"
-mc cp -r $FOLDER_PATH $DESTINATION
+mc ls myminio/public/public-assets
+if [ $? -eq 1 ]; then
+    echo "Uploading public-assets folder..."
+    mc cp --recursive /scripts/public-assets myminio/public/public-assets
+else
+    echo "public-assets folder already exists. Uploading/updating its contents..."
+    mc cp --recursive /scripts/public-assets myminio/public/public-assets
+fi
 
 exit 0;
