fac930de62b8bc123b39f7cbbaa9dca891d0404b
Kevin Nguyen <1632391+KNTH01@users.noreply.github.com>
2024-02-27 12:56:02 +0000

    feat: update entrypoint.sh

--- .devcontainer/minio/entrypoint.sh
+++ .devcontainer/minio/entrypoint.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-mc config host add myminio $MINIO_PRIVATE_URI $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD;
-mc mb -p myminio/$MINIO_PUBLIC_BUCKET;
-mc mb -p myminio/$MINIO_BUCKET;
-mc anonymous set download myminio/$MINIO_PUBLIC_BUCKET;
-mc admin user svcacct add --access-key $MINIO_ACCESS_KEY --secret-key $MINIO_ACCESS_SECRET myminio $MINIO_ROOT_USER;
+mc config host add myminio $MINIO_PRIVATE_URI $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc mb -p myminio/$MINIO_PUBLIC_BUCKET
+mc mb -p myminio/$MINIO_BUCKET
+mc anonymous set download myminio/$MINIO_PUBLIC_BUCKET
+mc admin user svcacct add --access-key $MINIO_ACCESS_KEY --secret-key $MINIO_ACCESS_SECRET myminio $MINIO_ROOT_USER
 
 mc stat myminio/public/certs.json
 if [ $? -eq 1 ]; then
-    echo "Uploading certs.json..."    
-    mc cp /scripts/certs.json myminio/public/certs.json
+  echo "Uploading certs.json..."
+  mc cp /scripts/certs.json myminio/public/certs.json
 else
-    echo "certs.json already exists."
+  echo "certs.json already exists."
 fi
 
-mc ls myminio/public/public-assets
-if [ $? -eq 1 ]; then
-    echo "Uploading public-assets folder..."
-    mc cp --recursive /scripts/public-assets myminio/public/public-assets
-else
-    echo "public-assets folder already exists. Uploading/updating its contents..."
-    mc cp --recursive /scripts/public-assets myminio/public/public-assets
-fi
+echo "Uploading public-assets folder..."
+mc cp --recursive /scripts/public-assets/ myminio/public/public-assets/
 
-exit 0;
+exit 0
