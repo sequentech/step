14770fb83b9be6b7b08b1035973a9eecd7538612
Eduardo Robles <edu@sequentech.io>
2024-04-08 14:34:08 +0000

    adding redirect uri support

--- packages/keycloak-extensions/conditional-authenticators/src/main/java/sequent/keycloak/conditional_authenticators/ManualVerificationToken.java
+++ packages/keycloak-extensions/conditional-authenticators/src/main/java/sequent/keycloak/conditional_authenticators/ManualVerificationToken.java
@@ -2,6 +2,8 @@ package sequent.keycloak.conditional_authenticators;
 
 import org.keycloak.authentication.actiontoken.DefaultActionToken;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.extern.jbosslog.JBossLog;
 
 /*
@@ -11,25 +13,40 @@ import lombok.extern.jbosslog.JBossLog;
 public class ManualVerificationToken extends DefaultActionToken {
 
     public static final String TOKEN_TYPE = "manual-verification-token";
+    private static final String JSON_FIELD_REDIRECT_URI = "reduri";
+
+    @JsonProperty(JSON_FIELD_REDIRECT_URI)
+    private String redirectUri;
 
     public ManualVerificationToken(
         String userId,
         int absoluteExpirationInSecs,
-        String compoundAuthenticationSessionId
+        String redirectUri
     ) {
         super(
             userId,
             TOKEN_TYPE,
             absoluteExpirationInSecs,
             null,
-            compoundAuthenticationSessionId
+            null
         );
         log.info("ManualVerificationToken");
+        setRedirectUri(redirectUri);
     }
 
     private ManualVerificationToken() {
         // Required to deserialize from JWT
         super();
         log.info("ManualVerificationToken private");
+    }
+
+    public String getRedirectUri() {
+        log.info("getRedirectUri(): " + redirectUri);
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        log.info("setRedirectUri() = " + redirectUri);
+        this.redirectUri = redirectUri;
     }
 }
--- packages/keycloak-extensions/conditional-authenticators/src/main/java/sequent/keycloak/conditional_authenticators/ManualVerificationTokenHandler.java
+++ packages/keycloak-extensions/conditional-authenticators/src/main/java/sequent/keycloak/conditional_authenticators/ManualVerificationTokenHandler.java
@@ -22,6 +22,7 @@ import org.keycloak.events.EventType;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
+import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.protocol.oidc.utils.RedirectUtils;
 
 import jakarta.ws.rs.core.Response;
@@ -57,6 +58,18 @@ public class ManualVerificationTokenHandler
         ActionTokenContext<ManualVerificationToken> tokenContext
     ) {
         return TokenUtils.predicates(
+            //TokenUtils.checkThat(
+            //    // either redirect URI is not specified or must be valid for the client
+            //    t -> 
+            //        t.getRedirectUri() == null ||
+            //            RedirectUtils.verifyRedirectUri(
+            //            tokenContext.getSession(),
+            //            t.getRedirectUri(),
+            //            tokenContext.getAuthenticationSession().getClient()
+            //        ) != null,
+            //    Errors.INVALID_REDIRECT_URI,
+            //    Messages.INVALID_REDIRECT_URI
+            //),
             verifyEmail(tokenContext)
         );
     }
@@ -73,6 +86,30 @@ public class ManualVerificationTokenHandler
         log.info("handleToken(): user = " + user.getUsername());
 
         KeycloakSession session = tokenContext.getSession();
+
+        //String redirectUri = RedirectUtils.verifyRedirectUri(
+        //    tokenContext.getSession(),
+        //    token.getRedirectUri(),
+        //    authSession.getClient()
+        //);
+        String redirectUri = token.getRedirectUri();
+
+        if (redirectUri != null) {
+            log.info("handleToken(): setting redirectUri=" + redirectUri);
+            authSession.setAuthNote(
+                AuthenticationManager.SET_REDIRECT_URI_AFTER_REQUIRED_ACTIONS,
+                "true"
+            );
+
+            authSession.setRedirectUri(redirectUri);
+            authSession.setClientNote(
+                OIDCLoginProtocol.REDIRECT_URI_PARAM,
+                redirectUri
+            );
+        } else {
+            log.info("handleToken(): NOT setting redirectUri, it's null");
+        }
+
         user.setEmailVerified(true);
         user.setAttribute(VERIFIED_ATTRIBUTE, List.of(VERIFIED_VALUE));
         log.info("handleToken(): user.VERIFIED_ATTRIBUTE = " + user.getFirstAttribute(VERIFIED_ATTRIBUTE));
