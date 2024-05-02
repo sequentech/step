4ee4680d8f3d02e28339c8818f5c64b315c22bfc
Eduardo Robles <edu@sequentech.io>
2024-05-02 14:39:41 +0000

    WIP

--- packages/admin-portal/nightwatch/ecosia.ts
+++ packages/admin-portal/nightwatch/ecosia.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023-2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
 describe("Ecosia.org Demo", function () {
     this.tags = ["demo"]
 
