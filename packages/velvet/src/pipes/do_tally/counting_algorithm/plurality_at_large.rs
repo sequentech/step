1482aa9bc5a7e8d80ecbdb9a571cb6c797e70c02
Félix Robles <felix@sequentech.io>
2024-05-11 14:06:42 +0000

    wip

--- packages/velvet/src/pipes/do_tally/counting_algorithm/plurality_at_large.rs
+++ packages/velvet/src/pipes/do_tally/counting_algorithm/plurality_at_large.rs
@@ -122,13 +122,13 @@ impl CountingAlgorithm for PluralityAtLarge {
 
         let census_base = cmp::max(1, self.tally.census) as f64;
         let percentage_total_votes = (total_votes as f64) * 100.0 / census_base;
-        let percentage_total_valid_votes = (count_valid as f64 * 100.0) / census_base;
-        let percentage_total_invalid_votes = (count_invalid as f64 * 100.0) / census_base;
-        let percentage_total_blank_votes = (count_blank as f64 * 100.0) / census_base;
+        let percentage_total_valid_votes = (count_valid as f64 * 100.0) / total_votes_base;
+        let percentage_total_invalid_votes = (count_invalid as f64 * 100.0) / total_votes_base;
+        let percentage_total_blank_votes = (count_blank as f64 * 100.0) / total_votes_base;
         let percentage_invalid_votes_explicit =
-            (count_invalid_votes.explicit as f64 * 100.0) / census_base;
+            (count_invalid_votes.explicit as f64 * 100.0) / total_votes_base;
         let percentage_invalid_votes_implicit =
-            (count_invalid_votes.implicit as f64 * 100.0) / census_base;
+            (count_invalid_votes.implicit as f64 * 100.0) / total_votes_base;
 
         let contest_result = ContestResult {
             contest: self.tally.contest.clone(),
