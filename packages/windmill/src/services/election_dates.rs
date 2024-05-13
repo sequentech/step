01696be5581498240871364c765039c3f8139d87
Félix Robles <felix@sequentech.io>
2024-05-13 21:20:47 +0000

    wip

--- packages/windmill/src/services/election_dates.rs
+++ packages/windmill/src/services/election_dates.rs
@@ -9,7 +9,17 @@ use tracing::{event, instrument, Level};
 
 use super::date::ISO8601;
 
-#[instrument(err)]
+#[instrument(skip(hasura_transaction), err)]
+pub async fn find_scheduled_start_date_election(
+    hasura_transaction: &Transaction<'_>,
+    tenant_id: &str,
+    election_event_id: &str,
+    election_id: &str,
+) -> Result<Option<()>> {
+    Ok(None)
+}
+
+#[instrument(skip(hasura_transaction), err)]
 pub async fn manage_dates(
     hasura_transaction: &Transaction<'_>,
     tenant_id: &str,
@@ -36,21 +46,66 @@ pub async fn manage_dates(
         .transpose()
         .map_err(|err| anyhow!("Error parsing election presentation {:?}", err))?
         .unwrap_or(Default::default());
-    let current_dates = election_presentation.dates.unwrap_or(Default::default());
+    let current_dates = election_presentation
+        .dates
+        .clone()
+        .unwrap_or(Default::default());
+    let mut new_dates = current_dates.clone();
 
     if is_start {
-        let Some(start_date_str) = current_dates.start_date else {
-            return Err(anyhow!("Empty start date"));
-        };
-        let start_date = ISO8601::to_date(&start_date_str)?;
-        let now = ISO8601::now();
-        if start_date > now {
-            return Err(anyhow!("start date can't be before now"));
+        let scheduled_start_date_opt = find_scheduled_start_date_election(
+            hasura_transaction,
+            tenant_id,
+            election_event_id,
+            election_id,
+        )
+        .await?;
+        if is_unset {
+            new_dates.scheduled_opening = Some(false);
+            if let Some(scheduled_start_date) = scheduled_start_date_opt {
+                /*delete_scheduled_event(
+                    hasura_transaction,
+                    tenant_id,
+                    &scheduled_start_date.id,
+                ).await?;*/
+            }
+        } else {
+            let Some(start_date_str) = current_dates.start_date else {
+                return Err(anyhow!("Empty start date"));
+            };
+            let start_date = ISO8601::to_date(&start_date_str)?;
+            let now = ISO8601::now();
+            if start_date > now {
+                return Err(anyhow!("start date can't be before now"));
+            }
+
+            /*if let Some(scheduled_start_date) = scheduled_start_date_opt {
+                update_scheduled_event(
+                    hasura_transaction,
+                    tenant_id,
+                    &scheduled_start_date.id,
+                ).await?;
+            } else {
+                insert_scheduled_event(
+                    hasura_transaction,
+                    tenant_id,
+                    &scheduled_start_date.id,
+                ).await?;
+            }*/
         }
     } else {
         let Some(end_date_str) = current_dates.end_date else {
             return Err(anyhow!("Empty end date"));
         };
     }
+    let mut new_election_presentation: ElectionPresentation = election_presentation.clone();
+    new_election_presentation.dates = Some(new_dates);
+    /*update_election_presentation(
+        hasura_transaction,
+        tenant_id,
+        election_event_id,
+        election_id,
+        new_election_presentation
+    ).await?;*/
     Ok(())
 }
