ac681402c26b16d8d9defdd6961c36b0435d85bd
Félix Robles <felix@sequentech.io>
2024-05-17 12:01:17 +0000

    wip

--- packages/sequent-core/src/services/replace_uuids.rs
+++ packages/sequent-core/src/services/replace_uuids.rs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use tracing::instrument;
 use uuid::Uuid;
 
@@ -13,14 +14,19 @@ pub fn replace_uuids(input: &str, keep: Vec<String>) -> String {
             .unwrap();
 
     let mut seen_uuids = HashMap::new();
+    let keep_set: HashSet<String> = keep.into_iter().collect();
 
     uuid_regex
         .replace_all(input, |caps: &regex::Captures| {
-            let old_uuid = caps.get(0).unwrap().as_str();
-            seen_uuids
-                .entry(old_uuid.to_owned())
-                .or_insert_with(|| Uuid::new_v4().to_string())
-                .clone()
+            let old_uuid = caps.get(0).unwrap().as_str().to_string();
+            if keep_set.contains(&old_uuid) {
+                old_uuid.clone()
+            } else {
+                seen_uuids
+                    .entry(old_uuid.clone())
+                    .or_insert_with(|| Uuid::new_v4().to_string())
+                    .clone()
+            }
         })
         .into_owned()
 }
