4be734ee3d55b6765302350e7ee6e2cd7ea04a06
Omer Naveh <omern@moveo.co.il>
2024-06-03 09:23:08 +0000

    added licensing to file

--- packages/voting-portal/src/utils/queryParams.ts
+++ packages/voting-portal/src/utils/queryParams.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 export const getLanguageFromURL = () => {
     const params = new URLSearchParams(window.location.search);
     return params.get('lang') || undefined;
