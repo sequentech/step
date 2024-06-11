a89b6c87014e588354e7c2a17adcc63c032d6113
Félix Robles <felix@sequentech.io>
2024-06-11 16:30:16 +0000

    wip

--- packages/windmill/src/postgres/area.rs
+++ packages/windmill/src/postgres/area.rs
@@ -307,6 +307,50 @@ pub async fn get_area_by_id(
 }
 
 #[instrument(err, skip_all)]
+pub async fn upsert_area_parents(
+    hasura_transaction: &Transaction<'_>,
+    areas: &Vec<Area>,
+) -> Result<()> {
+    for area in areas {
+        let statement = hasura_transaction
+            .prepare(
+                r#"
+                UPDATE
+                    sequent_backend.area
+                SET
+                    parent_id = $1
+                WHERE
+                    id = $2 AND
+                    tenant_id = $3 AND
+                    election_event_id = $4;
+            "#,
+            )
+            .await?;
+
+        let parent_id: Option<Uuid> = area
+            .parent_id
+            .clone()
+            .map(|parent_id| Uuid::parse_str(&parent_id).ok())
+            .flatten();
+
+        let rows: Vec<Row> = hasura_transaction
+            .query(
+                &statement,
+                &[
+                    &parent_id,
+                    &Uuid::parse_str(&area.id)?,
+                    &Uuid::parse_str(&area.tenant_id)?,
+                    &Uuid::parse_str(&area.election_event_id)?,
+                ],
+            )
+            .await
+            .map_err(|err| anyhow!("Error running query: {err}"))?;
+    }
+
+    Ok(())
+}
+
+#[instrument(err, skip_all)]
 pub async fn insert_areas(hasura_transaction: &Transaction<'_>, areas: &Vec<Area>) -> Result<()> {
     for area in areas {
         let statement = hasura_transaction
