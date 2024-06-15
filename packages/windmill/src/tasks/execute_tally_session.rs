95dfbe9318ee39ed96827f2d376c8918c76bc864
Félix Robles <felix@sequentech.io>
2024-06-15 14:07:52 +0000

    wip

--- packages/windmill/src/tasks/execute_tally_session.rs
+++ packages/windmill/src/tasks/execute_tally_session.rs
@@ -16,6 +16,7 @@ use crate::postgres::area::get_event_areas;
 use crate::postgres::tally_sheet::get_published_tally_sheets_by_event;
 use crate::services::cast_votes::find_area_ballots;
 use crate::services::cast_votes::{count_cast_votes_election, ElectionCastVotes};
+use crate::services::ceremonies::insert_ballots::insert_ballots_messages;
 use crate::services::ceremonies::results::populate_results_tables;
 use crate::services::ceremonies::serialize_logs::generate_logs;
 use crate::services::ceremonies::serialize_logs::print_messages;
@@ -316,113 +317,6 @@ pub async fn get_eligible_voters(
     )
     .await?;
     Ok(census as u64)
-}
-
-#[instrument(skip_all, err)]
-async fn insert_ballots_messages(
-    auth_headers: &AuthHeaders,
-    hasura_transaction: &Transaction<'_>,
-    keycloak_transaction: &Transaction<'_>,
-    tenant_id: &str,
-    election_event_id: &str,
-    board_name: &str,
-    trustee_names: Vec<String>,
-    tally_session_contests: Vec<GetLastTallySessionExecutionSequentBackendTallySessionContest>,
-) -> Result<()> {
-    let trustees = get_trustees_by_name(&auth_headers, &tenant_id, &trustee_names)
-        .await?
-        .data
-        .with_context(|| "can't find trustees")?
-        .sequent_backend_trustee;
-
-    event!(Level::INFO, "trustees len: {:?}", trustees.len());
-
-    // 4. create trustees keys from input strings
-    let deserialized_trustee_pks: Vec<StrandSignaturePk> = trustees
-        .clone()
-        .into_iter()
-        .map(|trustee| deserialize_public_key(trustee.public_key.unwrap()))
-        .collect();
-
-    event!(
-        Level::INFO,
-        "deserialized_trustee_pks len: {:?}",
-        deserialized_trustee_pks.len()
-    );
-
-    for (idx,tally_session_contest) in tally_session_contests.iter().enumerate() {
-        event!(
-            Level::INFO,
-            "Num {idx} of {}",
-            tally_session_contests.len(),
-        );
-        event!(
-            Level::INFO,
-            "Inserting Ballots message for contest {}, area {} and batch num {}",
-            tally_session_contest.contest_id,
-            tally_session_contest.area_id,
-            tally_session_contest.session_id,
-        );
-        let ballots_list = find_area_ballots(
-            &hasura_transaction,
-            &tenant_id,
-            &election_event_id,
-            &tally_session_contest.area_id,
-        )
-        .await?;
-
-        event!(Level::INFO, "ballots_list len: {:?}", ballots_list.len());
-
-        let realm = get_event_realm(&tenant_id, &election_event_id);
-        let users_map = list_keycloak_enabled_users_by_area_id(
-            keycloak_transaction,
-            &realm,
-            &tally_session_contest.area_id,
-        )
-        .await?;
-
-        let insertable_ballots: Vec<Ciphertext<RistrettoCtx>> = ballots_list
-            .iter()
-            .filter(|ballot| {
-                let Some(voter_id) = ballot.voter_id_string.clone() else {
-                    return false;
-                };
-                users_map.contains(&voter_id)
-            })
-            .map(|ballot| -> Result<Option<Ciphertext<RistrettoCtx>>> {
-                Ok(ballot
-                    .content
-                    .clone()
-                    .map(|ballot_str| -> Result<Option<Ciphertext<RistrettoCtx>>> {
-                        event!(Level::INFO, "deserializing ballot: '{:?}'", ballot_str);
-
-                        let hashable_ballot: HashableBallot = serde_json::from_str(&ballot_str)?;
-                        let contests = hashable_ballot
-                            .deserialize_contests()
-                            .map_err(|err| anyhow!("{:?}", err))?;
-                        Ok(contests
-                            .iter()
-                            .find(|contest| contest.contest_id == tally_session_contest.contest_id)
-                            .map(|contest| contest.ciphertext.clone()))
-                    })
-                    .transpose()?
-                    .flatten())
-            })
-            .collect::<Result<Vec<_>>>()?
-            .iter()
-            .filter_map(|ballot_opt| ballot_opt.clone())
-            .collect();
-
-        let batch = tally_session_contest.session_id.clone() as BatchNumber;
-        add_ballots_to_board(
-            board_name,
-            insertable_ballots,
-            batch,
-            deserialized_trustee_pks.clone(),
-        )
-        .await?;
-    }
-    Ok(())
 }
 
 #[instrument(skip_all, err)]
