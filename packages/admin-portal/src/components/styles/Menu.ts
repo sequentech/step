106403c153396aa44f15194fb23e3fa4fd9e21ba
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2024-06-21 05:24:49 +0300

    🐞 Remove tailwind from admin-portal (#503)
    Parent issue: https://github.com/sequentech/meta/issues/418
    
    ---------
    
    Co-authored-by: Eduardo Robles <edu@sequentech.io>

---
+++ packages/admin-portal/src/components/styles/Menu.ts
@@ -0,0 +1,170 @@
+//SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {css} from "@emotion/react"
+import styled from "@emotion/styled"
+import AddIcon from "@mui/icons-material/Add"
+import {NavLink} from "react-router-dom"
+import HowToVoteIcon from "@mui/icons-material/HowToVote"
+import AddCircleIcon from "@mui/icons-material/AddCircle"
+import Box from "@mui/material/Box"
+import {adminTheme} from "@sequentech/ui-essentials"
+
+export const divContainer = css`
+    flex: 0 0 auto;
+    width: 1.5rem;
+    height: 1.5rem;
+`
+
+export const MenuStyles = {
+    SideMenuContainer: styled("ul")`
+        display: flex;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        background-color: white;
+        text-transform: uppercase;
+        font-size: 0.75rem;
+        line-height: 1.5rem;
+        & > *:not(:last-child) {
+            margin-right: 1rem;
+        }
+    `,
+
+    SideMenuActiveItem: styled("li")<{isArchivedElectionEvents: boolean}>`
+        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        cursor: pointer;
+        color: ${({isArchivedElectionEvents}) =>
+            !isArchivedElectionEvents
+                ? adminTheme.palette.brandColor
+                : adminTheme.palette.secondary.main};
+        border-bottom: ${({isArchivedElectionEvents}) =>
+            !isArchivedElectionEvents ? `2px solid ${adminTheme.palette.brandSuccess}` : "none"};
+    `,
+
+    SideMenuArchiveItem: styled("li")<{isArchivedElectionEvents: boolean}>`
+        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        cursor: pointer;
+        color: ${({isArchivedElectionEvents}) =>
+            isArchivedElectionEvents
+                ? adminTheme.palette.brandColor
+                : adminTheme.palette.secondary.main};
+        border-bottom: ${({isArchivedElectionEvents}) =>
+            isArchivedElectionEvents ? `2px solid ${adminTheme.palette.brandSuccess}` : "none"};
+    `,
+
+    EmptyStateContainer: styled("div")`
+        padding: 1rem;
+        background-color: white;
+    `,
+
+    TreeLeavesContainer: styled("div")`
+        display: flex;
+        flex-direction: column;
+        margin-left: 0.75rem;
+    `,
+
+    CreateElectionContainer: styled("div")`
+        display: flex;
+        align-items: center;
+        color: ${adminTheme.palette.secondary.main};
+        & > *:not(:last-child) {
+            margin-right: 0.5rem;
+        }
+    `,
+
+    StyledAddIcon: styled(AddIcon)`
+        flex: 0 0 auto;
+    `,
+
+    StyledNavLink: styled(NavLink)`
+        flex-grow: 1;
+        padding-top: 0.375rem;
+        padding-bottom: 0.375rem;
+        border-bottom-width: 2px;
+        border-bottom-color: white;
+        cursor: pointer;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        &:hover {
+            border-bottom-color: ${adminTheme.palette.secondary.main};
+        }
+    `,
+
+    StyledHiddenDiv: styled("div")`
+        ${divContainer}
+        visibility: hidden;
+    `,
+
+    TreeMenuItemContainer: styled("div")`
+        display: flex;
+        text-align: left;
+        align-items: center;
+        & > *:not(:last-child) {
+            margin-right: 0.5rem;
+        }
+    `,
+
+    ItemContainer: styled("p")`
+        display: flex;
+        align-items: center;
+        & > *:not(:last-child) {
+            margin-right: 0.5rem;
+        }
+    `,
+    HowToVoteStyledIcon: styled(HowToVoteIcon)`
+        color: ${adminTheme.palette.brandColor};
+    `,
+    TreeMenuIconContaier: styled.div`
+        ${divContainer}
+        cursor: pointer;
+        color: black;
+    `,
+    StyledSideBarNavLink: styled(NavLink)`
+        flex-grow: 1;
+        padding-top: 0.375rem;
+        padding-bottom: 0.375rem;
+        color: black;
+        border-bottom: 2px solid white;
+        cursor: pointer;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        &:hover {
+            border-bottom-color: ${adminTheme.palette.brandColor};
+        }
+
+        &.active {
+            border-bottom-color: ${adminTheme.palette.brandColor};
+        }
+    `,
+
+    MenuActionContainer: styled("div")`
+        visibility: hidden;
+        &.group-hover-visible:hover {
+            visibility: visible;
+        }
+    `,
+    StyledIconContainer: styled("p")`
+        ${divContainer}
+        cursor: pointer
+    `,
+
+    StyledAddCircleIcon: styled(AddCircleIcon)`
+        color: ${adminTheme.palette.brandColor};
+    `,
+    StyledDiv: styled(Box)<{isWidth: boolean}>`
+        flex: 0 0 auto;
+        height: 1.5rem;
+        width: ${({isWidth}) => (isWidth ? "1.5rem" : "auto")};
+    `,
+}
