43501b21cb8ff4fe6e6fb04bff0c8429e23069d2
Omer Naveh <omern@moveo.co.il>
2024-06-27 12:44:36 +0000

    legals

--- packages/admin-portal/src/components/custom-order/CustomOrderInput.tsx
+++ packages/admin-portal/src/components/custom-order/CustomOrderInput.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import {Box} from "@mui/material"
 import {isArray} from "@sequentech/ui-essentials"
 import React, {useEffect, useState} from "react"
