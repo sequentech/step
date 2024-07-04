633fc6bb3c486ed926a88dcfce40f45e6f87c369
Bimpong <bimpongamoako@gmail.com>
2024-07-04 23:10:25 +0000

    addded headers for countdownbar component

--- packages/ui-essentials/src/components/CountdownBar/CountdownBar.tsx
+++ packages/ui-essentials/src/components/CountdownBar/CountdownBar.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022-2023 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import React from "react"
 import styled from "@emotion/styled"
 
