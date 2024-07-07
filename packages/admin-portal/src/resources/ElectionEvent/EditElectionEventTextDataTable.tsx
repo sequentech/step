ebd66adefed027b5fcd836c00ee403a52c7ce1d4
Omer Naveh <omern@moveo.co.il>
2024-07-07 13:40:37 +0000

    legals added

--- packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTextDataTable.tsx
+++ packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTextDataTable.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import {AuthContext} from "@/providers/AuthContextProvider"
 import {IPermissions} from "@/types/keycloak"
 import {Dialog, isString} from "@sequentech/ui-essentials"
