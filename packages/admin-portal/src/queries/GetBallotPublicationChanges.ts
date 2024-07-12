73aa2e3085d2fee263e0ee1cdf4f568cb9345419
Félix Robles <felix@sequentech.io>
2024-07-12 01:12:09 +0000

    wip

--- packages/admin-portal/src/queries/GetBallotPublicationChanges.ts
+++ packages/admin-portal/src/queries/GetBallotPublicationChanges.ts
@@ -7,12 +7,10 @@ export const GET_BALLOT_PUBLICATION_CHANGE = gql`
     mutation GetBallotPublicationChange(
         $ballotPublicationId: uuid!
         $electionEventId: uuid!
-        $limit: Int
     ) {
         get_ballot_publication_changes(
             ballot_publication_id: $ballotPublicationId
             election_event_id: $electionEventId
-            limit: $limit
         ) {
             current {
                 ballot_publication_id
