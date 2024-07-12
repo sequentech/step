0e7164d82a5c40a7ac5d89b2eb98437e419eae83
Félix Robles <felix@sequentech.io>
2024-07-12 22:08:10 +0000

    wip

--- packages/velvet/src/pipes/mark_winners/mark_winners.rs
+++ packages/velvet/src/pipes/mark_winners/mark_winners.rs
@@ -2,13 +2,15 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use std::path::PathBuf;
 use std::{cmp::Ordering, fs};
 
 use sequent_core::ballot::Candidate;
+use sequent_core::util::path::list_subfolders;
 use serde::Serialize;
 use tracing::{event, instrument, Level};
 
-use crate::pipes::do_tally::list_tally_sheet_subfolders;
+use crate::pipes::do_tally::{list_tally_sheet_subfolders, OUTPUT_BREAKDOWNS_FOLDER};
 use crate::pipes::error::{Error, Result};
 use crate::pipes::{
     do_tally::{
@@ -56,6 +58,32 @@ impl MarkWinners {
             })
             .collect()
     }
+
+    fn create_breakdown_winners(
+        &self,
+        base_input_path: &PathBuf,
+        base_output_path: &PathBuf,
+    ) -> Result<()> {
+        let base_input_breakdown_path = base_input_path.join(OUTPUT_BREAKDOWNS_FOLDER);
+        let base_output_breakdown_path = base_output_path.join(OUTPUT_BREAKDOWNS_FOLDER);
+        let subfolders = list_subfolders(&base_input_breakdown_path);
+        for subfolder in subfolders {
+            let contest_results_file_path = subfolder.join(OUTPUT_CONTEST_RESULT_FILE);
+            let contest_results_file = fs::File::open(&contest_results_file_path)
+                .map_err(|e| Error::FileAccess(contest_results_file_path.clone(), e))?;
+            let contest_result: ContestResult = parse_file(contest_results_file)?;
+
+            let winners = self.get_winners(&contest_result);
+
+            let subfolder_name = subfolder.file_name().unwrap();
+            let output_subfolder = base_output_breakdown_path.join(subfolder_name);
+            fs::create_dir_all(&output_subfolder)?;
+            let winners_file_path = output_subfolder.join(OUTPUT_WINNERS);
+            let winners_file = fs::File::create(winners_file_path)?;
+            serde_json::to_writer(winners_file, &winners)?;
+        }
+        Ok(())
+    }
 }
 
 impl Pipe for MarkWinners {
@@ -90,6 +118,8 @@ impl Pipe for MarkWinners {
                         Some(&contest_input.id),
                         Some(&area_input.id),
                     );
+                    // do breakdown winners
+                    self.create_breakdown_winners(&base_input_path, &base_output_path)?;
                     // do aggregate winners
                     let base_input_aggregate_path =
                         base_input_path.join(OUTPUT_CONTEST_RESULT_AREA_CHILDREN_AGGREGATE_FOLDER);
