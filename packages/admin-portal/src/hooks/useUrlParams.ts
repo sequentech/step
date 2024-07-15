7c7605da48bc525eab168707b8c3d8864fb0149d
Maya_Moveo <155538108+maya-byle@users.noreply.github.com>
2024-07-15 14:50:13 +0000

    fetch election event

---
+++ packages/admin-portal/src/hooks/useUrlParams.ts
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import {useLocation} from "react-router-dom"
+
+export function useUrlParams() {
+    const [, type, id] = useLocation().pathname.split("/")
+
+    const ids = {
+        election_event_id: type === "sequent_backend_election_event" ? id : undefined,
+        election_id: type === "sequent_backend_election" ? id : undefined,
+        contest_id: type === "sequent_backend_contest" ? id : undefined,
+        candidate_id: type === "sequent_backend_candidate" ? id : undefined,
+    }
+
+    return ids
+}
