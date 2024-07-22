fe064ed35cdc927294f9477ab0c7a0fceb2513c0
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2024-07-22 11:43:35 +0000

    apply prettify on message otp authenticator

--- packages/keycloak-extensions/message-otp-authenticator/src/main/java/sequent/keycloak/authenticator/VerifyOTPEmailRequiredAction.java
+++ packages/keycloak-extensions/message-otp-authenticator/src/main/java/sequent/keycloak/authenticator/VerifyOTPEmailRequiredAction.java
@@ -7,7 +7,6 @@ package sequent.keycloak.authenticator;
 import com.google.auto.service.AutoService;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.jbosslog.JBossLog;
-
 import org.keycloak.Config;
 import org.keycloak.authentication.InitiatedActionSupport;
 import org.keycloak.authentication.RequiredActionContext;
@@ -20,173 +19,134 @@ import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.models.UserModel;
 import org.keycloak.sessions.AuthenticationSessionModel;
 
-/**
- * Required Action that requires users to verify its email using an OTP.
- */
+/** Required Action that requires users to verify its email using an OTP. */
 @AutoService(RequiredActionFactory.class)
 @JBossLog
-public class VerifyOTPEmailRequiredAction
-    implements RequiredActionFactory, RequiredActionProvider
-{
-	public static final String PROVIDER_ID = "verify-email-otp-ra";
-	private static final String TPL_CODE = "login-message-otp.ftl";
+public class VerifyOTPEmailRequiredAction implements RequiredActionFactory, RequiredActionProvider {
+  public static final String PROVIDER_ID = "verify-email-otp-ra";
+  private static final String TPL_CODE = "login-message-otp.ftl";
 
-	@Override
-	public InitiatedActionSupport initiatedActionSupport() {
-		return InitiatedActionSupport.SUPPORTED;
-	}
+  @Override
+  public InitiatedActionSupport initiatedActionSupport() {
+    return InitiatedActionSupport.SUPPORTED;
+  }
 
-	@Override
-	public void evaluateTriggers(RequiredActionContext context) {
-		log.info("evaluateTriggers()");
-		// self registering if user doesn't have already one out of the
-		// configured credential types
-		UserModel user = context.getUser();
-		AuthenticationSessionModel authSession = context
-			.getAuthenticationSession();
+  @Override
+  public void evaluateTriggers(RequiredActionContext context) {
+    log.info("evaluateTriggers()");
+    // self registering if user doesn't have already one out of the
+    // configured credential types
+    UserModel user = context.getUser();
+    AuthenticationSessionModel authSession = context.getAuthenticationSession();
 
-		if (
-			authSession
-				.getRequiredActions()
-                .stream()
-				.noneMatch(PROVIDER_ID::equals) &&
-            user
-                .getRequiredActionsStream()
-                .noneMatch(PROVIDER_ID::equals) &&
-            !user.isEmailVerified()
-        ) {
-			log.info("evaluateTriggers(): adding required action to the user");
-			user.addRequiredAction(PROVIDER_ID);
-		}
-	}
+    if (authSession.getRequiredActions().stream().noneMatch(PROVIDER_ID::equals)
+        && user.getRequiredActionsStream().noneMatch(PROVIDER_ID::equals)
+        && !user.isEmailVerified()) {
+      log.info("evaluateTriggers(): adding required action to the user");
+      user.addRequiredAction(PROVIDER_ID);
+    }
+  }
 
-	@Override
-	public void requiredActionChallenge(RequiredActionContext context) {
-		KeycloakSession session = context.getSession();
-		AuthenticationSessionModel authSession = context
-			.getAuthenticationSession();
+  @Override
+  public void requiredActionChallenge(RequiredActionContext context) {
+    KeycloakSession session = context.getSession();
+    AuthenticationSessionModel authSession = context.getAuthenticationSession();
 
-		// initial form
-		LoginFormsProvider form = context.form();
-		form.setAttribute("realm", context.getRealm());
-		form.setAttribute("user", context.getUser());
+    // initial form
+    LoginFormsProvider form = context.form();
+    form.setAttribute("realm", context.getRealm());
+    form.setAttribute("user", context.getUser());
 
-		AuthenticatorConfigModel config = Utils
-			.getConfig(authSession.getRealm())
-			.get();
+    AuthenticatorConfigModel config = Utils.getConfig(authSession.getRealm()).get();
 
-		try {
-			UserModel user = context.getUser();
-			Utils.sendCode(
-				config,
-				session,
-				user,
-				authSession,
-				Utils.MessageCourier.EMAIL,
-				/* deferred user */ false
-			);
-			context
-				.challenge(
-					context.form().setAttribute("realm", context.getRealm()
-				)
-				.createForm(TPL_CODE));
-		} catch (Exception error) {
-			log.infov("there was an error {0}", error);
-			context.failure();
-			context.challenge(
-				context
-					.form()
-					.setError("messageNotSent", error.getMessage())
-					.createErrorPage(Response.Status.INTERNAL_SERVER_ERROR)
-			);
-		}
-	}
+    try {
+      UserModel user = context.getUser();
+      Utils.sendCode(
+          config,
+          session,
+          user,
+          authSession,
+          Utils.MessageCourier.EMAIL,
+          /* deferred user */ false);
+      context.challenge(
+          context.form().setAttribute("realm", context.getRealm()).createForm(TPL_CODE));
+    } catch (Exception error) {
+      log.infov("there was an error {0}", error);
+      context.failure();
+      context.challenge(
+          context
+              .form()
+              .setError("messageNotSent", error.getMessage())
+              .createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+    }
+  }
 
-	@Override
-	public void processAction(RequiredActionContext context) {
-		log.info("action() called");
-		String enteredCode = context
-			.getHttpRequest()
-			.getDecodedFormParameters()
-			.getFirst(Utils.CODE);
-		
-		AuthenticationSessionModel authSession = context
-			.getAuthenticationSession();
-		String code = authSession.getAuthNote(Utils.CODE);
-		String ttl = authSession.getAuthNote(Utils.CODE_TTL);
-		UserModel user = context.getUser();
+  @Override
+  public void processAction(RequiredActionContext context) {
+    log.info("action() called");
+    String enteredCode = context.getHttpRequest().getDecodedFormParameters().getFirst(Utils.CODE);
 
-		if (code == null || ttl == null) {
-			context.failure();
-			context.challenge(
-				context
-					.form()
-					.createErrorPage(Response.Status.INTERNAL_SERVER_ERROR)
-			);
-			return;
-		}
+    AuthenticationSessionModel authSession = context.getAuthenticationSession();
+    String code = authSession.getAuthNote(Utils.CODE);
+    String ttl = authSession.getAuthNote(Utils.CODE_TTL);
+    UserModel user = context.getUser();
 
-		boolean isValid = Utils.constantTimeIsEqual(
-			enteredCode.getBytes(),
-			code.getBytes()
-		);
-		if (isValid) {
-			context
-				.getAuthenticationSession()
-				.removeAuthNote(Utils.CODE);
-			if (Long.parseLong(ttl) < System.currentTimeMillis()) {
-				// expired
-				context.failure();
-				context.challenge(
-					context
-						.form()
-						.setError("messageOtpAuthCodeExpired")
-						.createErrorPage(Response.Status.BAD_REQUEST)
-				);
-			} else {
-				// valid
-				user.setEmailVerified(true);
-				user.removeRequiredAction(PROVIDER_ID);
-				context.success();
+    if (code == null || ttl == null) {
+      context.failure();
+      context.challenge(context.form().createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+      return;
+    }
 
-			}
-		} else {
-			// invalid
-			context.failure();
-			context.challenge(
-				context
-					.form()
-					.setAttribute("realm", context.getRealm())
-					.setError("messageOtpAuthCodeInvalid")
-					.createForm(TPL_CODE)
-			);
-		}
-	}
+    boolean isValid = Utils.constantTimeIsEqual(enteredCode.getBytes(), code.getBytes());
+    if (isValid) {
+      context.getAuthenticationSession().removeAuthNote(Utils.CODE);
+      if (Long.parseLong(ttl) < System.currentTimeMillis()) {
+        // expired
+        context.failure();
+        context.challenge(
+            context
+                .form()
+                .setError("messageOtpAuthCodeExpired")
+                .createErrorPage(Response.Status.BAD_REQUEST));
+      } else {
+        // valid
+        user.setEmailVerified(true);
+        user.removeRequiredAction(PROVIDER_ID);
+        context.success();
+      }
+    } else {
+      // invalid
+      context.failure();
+      context.challenge(
+          context
+              .form()
+              .setAttribute("realm", context.getRealm())
+              .setError("messageOtpAuthCodeInvalid")
+              .createForm(TPL_CODE));
+    }
+  }
 
-	@Override
-	public String getDisplayText() {
-		return "Verify Email using OTP";
-	}
+  @Override
+  public String getDisplayText() {
+    return "Verify Email using OTP";
+  }
 
-	@Override
-	public RequiredActionProvider create(KeycloakSession session) {
-		return this;
-	}
+  @Override
+  public RequiredActionProvider create(KeycloakSession session) {
+    return this;
+  }
 
-	@Override
-	public void init(Config.Scope config) {
-	}
+  @Override
+  public void init(Config.Scope config) {}
 
-	@Override
-	public void postInit(KeycloakSessionFactory factory) {
-	}
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {}
 
-	@Override
-	public void close() {
-	}
+  @Override
+  public void close() {}
 
-	@Override
-	public String getId() {
-		return PROVIDER_ID;
-	}
+  @Override
+  public String getId() {
+    return PROVIDER_ID;
+  }
 }
--- packages/keycloak-extensions/message-otp-authenticator/src/main/java/sequent/keycloak/authenticator/gateway/AwsSmsSenderProvider.java
+++ packages/keycloak-extensions/message-otp-authenticator/src/main/java/sequent/keycloak/authenticator/gateway/AwsSmsSenderProvider.java
@@ -4,64 +4,38 @@
 
 package sequent.keycloak.authenticator.gateway;
 
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.jbosslog.JBossLog;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
-import java.io.IOException;
-import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Properties;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
-import org.keycloak.theme.Theme;
-import lombok.extern.jbosslog.JBossLog;
 
 @JBossLog
 public class AwsSmsSenderProvider implements SmsSenderProvider {
 
-	private static final SnsClient sns = SnsClient.create();
-	private final String senderId;
+  private static final SnsClient sns = SnsClient.create();
+  private final String senderId;
 
-	AwsSmsSenderProvider(String senderId) {
-		this.senderId = senderId;
-	}
+  AwsSmsSenderProvider(String senderId) {
+    this.senderId = senderId;
+  }
 
-	@Override
-	public void send(String phoneNumber, String message)
-	{
-        log.infov(
-            "**Sending AWS SMS**:\n\t- phoneNumber={0}\n\t- message={1}",
-            phoneNumber,
-            message
-        );
-		Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
-		messageAttributes.put(
-			"AWS.SNS.SMS.SenderID",
-			MessageAttributeValue
-				.builder()
-				.stringValue(senderId)
-				.dataType("String")
-				.build()
-		);
-		messageAttributes.put(
-			"AWS.SNS.SMS.SMSType",
-			MessageAttributeValue
-				.builder()
-				.stringValue("Transactional")
-				.dataType("String")
-				.build()
-		);
+  @Override
+  public void send(String phoneNumber, String message) {
+    log.infov("**Sending AWS SMS**:\n\t- phoneNumber={0}\n\t- message={1}", phoneNumber, message);
+    Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+    messageAttributes.put(
+        "AWS.SNS.SMS.SenderID",
+        MessageAttributeValue.builder().stringValue(senderId).dataType("String").build());
+    messageAttributes.put(
+        "AWS.SNS.SMS.SMSType",
+        MessageAttributeValue.builder().stringValue("Transactional").dataType("String").build());
 
-		sns.publish(builder -> builder
-			.message(message)
-			.phoneNumber(phoneNumber)
-			.messageAttributes(messageAttributes)
-		);
-	}
+    sns.publish(
+        builder ->
+            builder.message(message).phoneNumber(phoneNumber).messageAttributes(messageAttributes));
+  }
 
-	@Override
-	public void close() {
-	}
+  @Override
+  public void close() {}
 }
