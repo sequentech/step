254ef27cb45a3dd9ed103592327f0ada3c7d0a81
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2024-07-22 11:12:58 +0000

    apply prettify on dummy-email-sender-provider

--- packages/keycloak-extensions/dummy-email-sender-provider/src/main/java/sequent/keycloak/dummy/DummyEmailSenderProvider.java
+++ packages/keycloak-extensions/dummy-email-sender-provider/src/main/java/sequent/keycloak/dummy/DummyEmailSenderProvider.java
@@ -4,42 +4,31 @@
 
 package sequent.keycloak.dummy;
 
-import org.keycloak.email.EmailSenderProvider;
-import org.keycloak.email.EmailException;
-import lombok.extern.jbosslog.JBossLog;
 import java.util.Map;
- 
-
+import lombok.extern.jbosslog.JBossLog;
+import org.keycloak.email.EmailException;
+import org.keycloak.email.EmailSenderProvider;
 
 @JBossLog
 public class DummyEmailSenderProvider implements EmailSenderProvider {
 
-    // Temporary variable for testing purposes
-    private String temporaryValue;
+  // Temporary variable for testing purposes
+  private String temporaryValue;
 
-    public DummyEmailSenderProvider() {
-        // Initialize the temporary value to a default or null
-        this.temporaryValue = null;
-    }
+  public DummyEmailSenderProvider() {
+    // Initialize the temporary value to a default or null
+    this.temporaryValue = null;
+  }
 
-    @Override
-    public void send(
-            Map<String, String> config,
-            String address,
-            String subject,
-            String textBody,
-            String htmlBody
-    ) throws EmailException {        
-        log.infov(
-                "**Sending dummy email**:\n\t- subject={0}\n\t- address={1}\n\t- textBody={2}\n\t- htmlBody={3}",
-                subject,
-                address,
-                textBody,
-                htmlBody
-        );
-    }
+  @Override
+  public void send(
+      Map<String, String> config, String address, String subject, String textBody, String htmlBody)
+      throws EmailException {
+    log.infov(
+        "**Sending dummy email**:\n\t- subject={0}\n\t- address={1}\n\t- textBody={2}\n\t- htmlBody={3}",
+        subject, address, textBody, htmlBody);
+  }
 
-    @Override
-    public void close() {
-    }
+  @Override
+  public void close() {}
 }
