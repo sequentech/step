10b9997194f6c3bfe6b542e0d36a6579d77c02f6
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2024-07-22 11:02:01 +0000

    Add spotless for java code prettifier and apply to aws-ses-email-sender-provider

--- packages/keycloak-extensions/aws-ses-email-sender-provider/src/main/java/sequent/keycloak/aws_ses/AwsSesEmailSenderProviderFactory.java
+++ packages/keycloak-extensions/aws-ses-email-sender-provider/src/main/java/sequent/keycloak/aws_ses/AwsSesEmailSenderProviderFactory.java
@@ -4,49 +4,43 @@
 
 package sequent.keycloak.aws_ses;
 
-import sequent.keycloak.aws_ses.AwsSesEmailSenderProvider; // sending email using AWS
-import software.amazon.awssdk.services.ses.SesClient;      // sending Mock email  
-
-import org.keycloak.email.EmailSenderProviderFactory;
-import org.keycloak.email.EmailSenderProvider;
+// sending email using AWS
+import com.google.auto.service.AutoService;
 import org.keycloak.Config;
+import org.keycloak.email.EmailSenderProvider;
+import org.keycloak.email.EmailSenderProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
-import com.google.auto.service.AutoService;
- 
- /**
-  * @author <a href="mailto:edu@sequentech.io">Eduardo Robles</a>
-  */
+import software.amazon.awssdk.services.ses.SesClient; // sending Mock email
+
+/**
+ * @author <a href="mailto:edu@sequentech.io">Eduardo Robles</a>
+ */
 @AutoService(EmailSenderProviderFactory.class)
-public class AwsSesEmailSenderProviderFactory
-    implements EmailSenderProviderFactory
-{
+public class AwsSesEmailSenderProviderFactory implements EmailSenderProviderFactory {
 
-    @Override
-    public EmailSenderProvider create(KeycloakSession session) {
-        /* 
-        // Uncomment for emailing directly using AWS
-        return new AwsSesEmailSenderProvider();
-        */
-        // For Mock Ses email sending test, Comment if to send email directly using AWS
-        SesClient sesClient = SesClient.builder().build();
-        return new AwsSesEmailSenderProvider(sesClient);
-    }
+  @Override
+  public EmailSenderProvider create(KeycloakSession session) {
+    /*
+    // Uncomment for emailing directly using AWS
+    return new AwsSesEmailSenderProvider();
+    */
+    // For Mock Ses email sending test, Comment if to send email directly using AWS
+    SesClient sesClient = SesClient.builder().build();
+    return new AwsSesEmailSenderProvider(sesClient);
+  }
 
-    @Override
-    public void init(Config.Scope config) {
-    }
+  @Override
+  public void init(Config.Scope config) {}
 
-    @Override
-    public void postInit(KeycloakSessionFactory factory) {
-    }
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {}
 
-    @Override
-    public void close() {
-    }
+  @Override
+  public void close() {}
 
-    @Override
-    public String getId() {
-        return "aws-ses";
-    }
+  @Override
+  public String getId() {
+    return "aws-ses";
+  }
 }
--- packages/keycloak-extensions/aws-ses-email-sender-provider/src/test/java/sequent/keycloak/aws_ses/AwsSesEmailSenderProviderTest.java
+++ packages/keycloak-extensions/aws-ses-email-sender-provider/src/test/java/sequent/keycloak/aws_ses/AwsSesEmailSenderProviderTest.java
@@ -2,92 +2,90 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-  
 package sequent.keycloak.aws_ses;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
-import software.amazon.awssdk.services.ses.model.SendEmailResponse;
-import org.keycloak.email.EmailException;
-import java.util.HashMap;
-import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.email.EmailException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
+import software.amazon.awssdk.services.ses.model.SendEmailResponse;
+
 @ExtendWith(MockitoExtension.class)
 public class AwsSesEmailSenderProviderTest {
 
-    @Mock
-    private SesClient sesClientMock;
+  @Mock private SesClient sesClientMock;
 
-    @InjectMocks
-    private AwsSesEmailSenderProvider emailSenderProvider;
+  @InjectMocks private AwsSesEmailSenderProvider emailSenderProvider;
 
-    public AwsSesEmailSenderProviderTest(@Mock SesClient sesClientMock) {
-        this.sesClientMock = sesClientMock;
-        this.emailSenderProvider = new AwsSesEmailSenderProvider(sesClientMock);
-    }
+  public AwsSesEmailSenderProviderTest(@Mock SesClient sesClientMock) {
+    this.sesClientMock = sesClientMock;
+    this.emailSenderProvider = new AwsSesEmailSenderProvider(sesClientMock);
+  }
 
-    @BeforeEach
-    public void setUp() {
-  
-    }
+  @BeforeEach
+  public void setUp() {}
 
-    @Test
-    public void testSendEmail() throws EmailException {
-        // Mock SES response to simulate successful email sending
-        SendEmailResponse mockResponse = SendEmailResponse.builder().messageId("mockMessageId").build();
+  @Test
+  public void testSendEmail() throws EmailException {
+    // Mock SES response to simulate successful email sending
+    SendEmailResponse mockResponse = SendEmailResponse.builder().messageId("mockMessageId").build();
 
-        // Configure sesClientMock to return mockResponse when sendEmail is called
-        when(sesClientMock.sendEmail(any(SendEmailRequest.class))).thenReturn(mockResponse);
+    // Configure sesClientMock to return mockResponse when sendEmail is called
+    when(sesClientMock.sendEmail(any(SendEmailRequest.class))).thenReturn(mockResponse);
 
-        // Perform the test
-        Map<String, String> config = new HashMap<>();
-        config.put("from", "sender@example.com");
-        config.put("fromDisplayName", "Sender Name");
-        String address = "recipient@example.com";
-        String subject = "Test Subject";
-        String textBody = "Hello, this is a text email.";
-        String htmlBody = "<html><body><h1>Hello</h1><p>This is an HTML email.</p></body></html>";
+    // Perform the test
+    Map<String, String> config = new HashMap<>();
+    config.put("from", "sender@example.com");
+    config.put("fromDisplayName", "Sender Name");
+    String address = "recipient@example.com";
+    String subject = "Test Subject";
+    String textBody = "Hello, this is a text email.";
+    String htmlBody = "<html><body><h1>Hello</h1><p>This is an HTML email.</p></body></html>";
 
-        // Call the method under test
-        emailSenderProvider.send(config, address, subject, textBody, htmlBody);
+    // Call the method under test
+    emailSenderProvider.send(config, address, subject, textBody, htmlBody);
 
-        // Verify SES client interaction
-        verify(sesClientMock, times(1)).sendEmail(any(SendEmailRequest.class));
-    }
+    // Verify SES client interaction
+    verify(sesClientMock, times(1)).sendEmail(any(SendEmailRequest.class));
+  }
 
+  @Test
+  public void testSendEmailGeneralException() {
+    // Test data
+    Map<String, String> config = new HashMap<>();
+    config.put("from", "sender@example.com");
+    config.put("fromDisplayName", "Sender Name");
+    String address = "recipient@example.com";
+    String subject = "Test Subject";
+    String textBody = "Hello, this is a text email.";
+    String htmlBody = "<html><body><h1>Hello</h1><p>This is an HTML email.</p></body></html>";
 
- 
-    @Test
-    public void testSendEmailGeneralException() {
-        // Test data
-        Map<String, String> config = new HashMap<>();
-        config.put("from", "sender@example.com");
-        config.put("fromDisplayName", "Sender Name");
-        String address = "recipient@example.com";
-        String subject = "Test Subject";
-        String textBody = "Hello, this is a text email.";
-        String htmlBody = "<html><body><h1>Hello</h1><p>This is an HTML email.</p></body></html>";
+    // Configure sesClientMock to throw a general Exception
+    doThrow(new RuntimeException("General error"))
+        .when(sesClientMock)
+        .sendEmail(any(SendEmailRequest.class));
 
-        // Configure sesClientMock to throw a general Exception
-        doThrow(new RuntimeException("General error")).when(sesClientMock).sendEmail(any(SendEmailRequest.class));
+    // Perform the test and verify that EmailException is thrown
+    EmailException thrown =
+        assertThrows(
+            EmailException.class,
+            () -> {
+              emailSenderProvider.send(config, address, subject, textBody, htmlBody);
+            });
 
-        // Perform the test and verify that EmailException is thrown
-        EmailException thrown = assertThrows(EmailException.class, () -> {
-            emailSenderProvider.send(config, address, subject, textBody, htmlBody);
-        });
-
-        // Verify that the exception message is as expected
-        assertEquals("Exception: Failed to send email via AWS SES", thrown.getMessage());
-    }
+    // Verify that the exception message is as expected
+    assertEquals("Exception: Failed to send email via AWS SES", thrown.getMessage());
+  }
 }
