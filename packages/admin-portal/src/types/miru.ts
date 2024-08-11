b4aaa1153e96bd1d8f7e63280ab6bceb57ce29d4
Félix Robles <felix@sequentech.io>
2024-08-11 11:39:26 +0000

    wip

---
+++ packages/admin-portal/src/types/miru.ts
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2024 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import { ILog } from "./ceremonies"
+
+// MiruSignature interface
+export interface IMiruSignature {
+    trusteeName: string
+    pubKey: string
+    signature: string
+}
+
+// MiruDocument interface
+export interface IMiruDocument {
+    documentId: string
+    serversSentTo: Array<string>
+    createdAt: string
+    signatures: Array<IMiruSignature>
+}
+
+// MiruCcsServer interface
+export interface IMiruCcsServer {
+    name: string
+    address: string
+    publick_key_pem: string
+}
+
+// MiruTransmissionPackageData interface
+export interface IMiruTransmissionPackageData {
+    electionId: string
+    areaId: string
+    servers: Array<IMiruCcsServer>
+    documents: Array<IMiruDocument>
+    logs: Array<ILog>
+}
+
+// MiruTallySessionData type alias
+export type IMiruTallySessionData = Array<IMiruTransmissionPackageData>
