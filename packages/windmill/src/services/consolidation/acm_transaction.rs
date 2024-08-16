dc259753c5cb2ed548852a50afe721488d929e9e
Félix Robles <felix@sequentech.io>
2024-08-16 22:17:41 +0000

    wip

--- packages/windmill/src/services/consolidation/acm_transaction.rs
+++ packages/windmill/src/services/consolidation/acm_transaction.rs
@@ -11,10 +11,10 @@ const RANDOM_PART: u64 = 13212;
 // generate a 13 digit number like 1721184531864
 pub fn generate_transaction_id() -> u64 {
     let now = Utc::now();
-    let year = now.year() as u64 - 2024u64; // Get last two digits of the year
+    let year = now.year() as u64 - 2023u64; // Get last two digits of the year
     let day = now.ordinal() as u64; // Get the day of the year (1 to 366)
-    let hour = now.hour() as u64;
-    let second = now.second() as u64;
+    let hour = now.hour() as u64 + 1u64;
+    let second = now.second() as u64 + 1u64;
 
     // Calculate the first part of the number: year * day * hour * second
     let first_part = year * day * hour * second;
