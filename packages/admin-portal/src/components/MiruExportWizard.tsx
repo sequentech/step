f218732d51cde087be34145e2ab5501cce4bc294
Félix Robles <felix@sequentech.io>
2024-08-17 19:54:19 +0000

    wip

--- packages/admin-portal/src/components/MiruExportWizard.tsx
+++ packages/admin-portal/src/components/MiruExportWizard.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
 import {Accordion, AccordionSummary, CircularProgress} from "@mui/material"
 import React, {useMemo} from "react"
 import {WizardStyles} from "./styles/WizardStyles"
