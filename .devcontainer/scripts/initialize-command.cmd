0400e40ff999c799301e515516843c7369091552
Francisco Moreno <francisco.moreno@sequentech.io>
2024-08-19 13:13:38 +0200

    add license to cmd file

--- .devcontainer/scripts/initialize-command.cmd
+++ .devcontainer/scripts/initialize-command.cmd
@@ -1,1 +1,5 @@
+# SPDX-FileCopyrightText: 2023-2024 Sequent Tech <legal@sequentech.io>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
 powershell -noprofile -windowstyle hidden -executionpolicy bypass -file .devcontainer/scripts/initialize-command.ps1
