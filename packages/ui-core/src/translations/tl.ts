3544d0ddb59312191d29ecfb6d00b821b558b2ef
Yuval Komash <102144445+yuvalkom-M@users.noreply.github.com>
2024-08-21 13:41:41 +0000

    fix prettify

--- packages/ui-core/src/translations/tl.ts
+++ packages/ui-core/src/translations/tl.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-import { TranslationType } from "./en"
+import {TranslationType} from "./en"
 
 const tagalogTranslation: TranslationType = {
     translations: {
