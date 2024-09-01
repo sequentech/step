8e91abe79cdd005da3c8cc9aa3e0a5e0b061ba80
Gal Buzaglo <105719976+Galb-moveo@users.noreply.github.com>
2024-09-01 10:37:40 +0000

    not necessary file changes

--- .devcontainer/scripts/initialize.cmd
+++
@@ -1,5 +0,0 @@
-
-: SPDX-FileCopyrightText: 2023-2024 Sequent Tech <legal@sequentech.io>
-:
-: SPDX-License-Identifier: AGPL-3.0-only
-powershell -windowstyle hidden -file .devcontainer/scripts/initialize-command.ps1
