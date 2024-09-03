6cbb716a57114ae962ec773919e04dd839041001
Francisco Moreno <francisco.moreno@sequentech.io>
2024-09-03 20:04:33 +0200

    copy .env file to working_dir

--- packages/immu-board/init-immudb.sh
+++ packages/immu-board/init-immudb.sh
@@ -4,4 +4,5 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 cp -rf /workspaces/step/.devcontainer/keycloak/import /import
+cp -rf /workspaces/step/.devcontainer/.env .env
 cargo run --bin bb_helper -- --server-url ${IMMUDB_SERVER_URL} --username ${IMMUDB_USER} --password ${IMMUDB_PASSWORD} --index-dbname ${IMMUDB_INDEX_DB} --board-dbname ${IMMUDB_BOARD_DB_NAME} --cache-dir /tmp/immu-board upsert-init-db
