af11b138689600f48cda6d5a9c0e4f3cfd265b64
FMorenoEraso <francisco.moreno@sequentech.io>
2024-09-03 13:00:35 +0000

    script for running admin portal

--- packages/init-admin.sh
+++ packages/init-admin.sh
@@ -3,9 +3,10 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-cp -r /node_modules /workspaces/step/packages/
-cp -r /ui-core/node_modules /workspaces/step/packages/ui-core/
-cp -r /ui-core/dist /workspaces/step/packages/ui-core/
-cp -r /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/
-cp -r /ui-essentials/dist /workspaces/step/packages/ui-essentials/
-cp -r /admin-portal/node_modules /workspaces/step/packages/admin-portal/
+cp -rf /node_modules /workspaces/step/packages/
+cp -rf /ui-core/node_modules /workspaces/step/packages/ui-core/
+cp -rf /ui-core/dist /workspaces/step/packages/ui-core/
+cp -rf /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/
+cp -rf /ui-essentials/dist /workspaces/step/packages/ui-essentials/
+cp -rf /admin-portal/node_modules /workspaces/step/packages/admin-portal/
+yarn start:admin-portal
