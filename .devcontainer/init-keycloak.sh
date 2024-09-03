33ee05b86ded6f9d1491263ed93c9f0abdadd39f
Francisco Moreno <francisco.moreno@sequentech.io>
2024-09-03 18:49:17 +0200

    updating immudb with keycloak tenant

---
+++ .devcontainer/init-keycloak.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2023-2024 Sequent Tech <legal@sequentech.io>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+cp -rf /workspaces/step/.devcontainer/keycloak/import /opt/keycloak/data/import
+/opt/keycloak/bin/kc.sh start-dev --features=preview --health-enabled=true --spi-user-profile-declarative-user-profile-read-only-attributes=area-id,tenant-id --spi-user-profile-declarative-user-profile-admin-read-only-attributes=sequent.admin-read-only.* -Dkeycloak.profile.feature.upload_scripts=enabled --spi-email-sender-provider=dummy --spi-email-sender-dummy-enabled=true --spi-email-sender-default-enabled=false --spi-sms-sender-provider=dummy --spi-sms-sender-dummy-enabled=true --spi-sms-sender-aws-enabled=false --import-realm
