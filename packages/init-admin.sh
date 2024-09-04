597c5323fe1eb5c93e2dde41feadccd021514400
Francisco Moreno <francisco.moreno@sequentech.io>
2024-09-04 09:58:15 +0000

    removing directories

--- packages/init-admin.sh
+++ packages/init-admin.sh
@@ -3,10 +3,16 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-cp -rf /node_modules /workspaces/step/packages/node_modules
-cp -rf /ui-core/node_modules /workspaces/step/packages/ui-core/node_modules
-cp -rf /ui-core/dist /workspaces/step/packages/ui-core/dist
-cp -rf /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/node_modules
-cp -rf /ui-essentials/dist /workspaces/step/packages/ui-essentials/dist
-cp -rf /admin-portal/node_modules /workspaces/step/packages/admin-portal/node_modules
+rm -rf /workspaces/step/packages/node_modules
+cp -r /node_modules /workspaces/step/packages/node_modules
+rm -rf /workspaces/step/packages/ui-core/node_modules
+cp -r /ui-core/node_modules /workspaces/step/packages/ui-core/node_modules
+rm -rf /workspaces/step/packages/ui-core/dist
+cp -r /ui-core/dist /workspaces/step/packages/ui-core/dist
+rm -rf /workspaces/step/packages/ui-essentials/node_modules
+cp -r /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/node_modules
+rm -rf /workspaces/step/packages/ui-essentials/dist
+cp -r /ui-essentials/dist /workspaces/step/packages/ui-essentials/dist
+rm -rf /workspaces/step/packages/admin-portal/node_modules
+cp -r /admin-portal/node_modules /workspaces/step/packages/admin-portal/node_modules
 # yarn start:admin-portal
