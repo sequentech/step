9100193773118c2ee98f0c052782d77920fe5584
Francisco Moreno <francisco.moreno@sequentech.io>
2024-09-04 11:36:06 +0200

    testing without starting admin portal

--- packages/init-admin.sh
+++ packages/init-admin.sh
@@ -9,4 +9,4 @@ cp -rf /ui-core/dist /workspaces/step/packages/ui-core/
 cp -rf /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/
 cp -rf /ui-essentials/dist /workspaces/step/packages/ui-essentials/
 cp -rf /admin-portal/node_modules /workspaces/step/packages/admin-portal/
-yarn start:admin-portal
+# yarn start:admin-portal
