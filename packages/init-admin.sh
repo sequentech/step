7f027094ee12856ddd5ab18ebc54158e1edb31f7
Francisco Moreno <francisco.moreno@sequentech.io>
2024-09-05 11:32:56 +0000

    updating dependency

--- packages/init-admin.sh
+++ packages/init-admin.sh
@@ -3,10 +3,10 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 
-cp -rf /node_modules /workspaces/step/packages/node_modules
-cp -rf /ui-core/node_modules /workspaces/step/packages/ui-core/node_modules
-cp -rf /ui-core/dist /workspaces/step/packages/ui-core/dist
-cp -rf /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/node_modules
-cp -rf /ui-essentials/dist /workspaces/step/packages/ui-essentials/dist
-cp -rf /admin-portal/node_modules /workspaces/step/packages/admin-portal/node_modules
+cp -rf /node_modules /workspaces/step/packages/
+# cp -rf /ui-core/node_modules /workspaces/step/packages/ui-core/node_modules
+# cp -rf /ui-core/dist /workspaces/step/packages/ui-core/dist
+# cp -rf /ui-essentials/node_modules /workspaces/step/packages/ui-essentials/node_modules
+# cp -rf /ui-essentials/dist /workspaces/step/packages/ui-essentials/dist
+# cp -rf /admin-portal/node_modules /workspaces/step/packages/admin-portal/node_modules
 # yarn start:admin-portal
