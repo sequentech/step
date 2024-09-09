4a8416c492abddd4f59df47c6cc7d030102b8af6
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2024-09-09 12:59:54 +0000

    Revert "Apply StyledLayout with the css input."
    This reverts commit caf089363636a494611dae978bb35110e1fa9f33.

--- packages/admin-portal/src/components/CustomLayout.tsx
+++ packages/admin-portal/src/components/CustomLayout.tsx
@@ -1,16 +1,11 @@
 // SPDX-FileCopyrightText: 2023 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-// import React from "react"
-import React, {useContext, useEffect} from "react"
+import React from "react"
 import {Layout, LayoutProps, SidebarClasses} from "react-admin"
 import {CustomAppBar} from "./CustomAppBar"
 import {CustomMenu} from "./CustomMenu"
 import {CustomSidebar} from "./menu/CustomSidebar"
-import {TenantContext} from "@/providers/TenantContextProvider"
-import {Sequent_Backend_Tenant} from "@/gql/graphql"
-import { useGetOne } from 'react-admin';
-import {styled} from "@mui/material/styles"
 
 const SequentSidebar = (props: any) => {
     return (
@@ -20,36 +15,23 @@ const SequentSidebar = (props: any) => {
     )
 }
 
-const StyledLayout = styled(Layout)<{ css: string }>`
-    ${({ css }) => css}
-    & .MuiPaper-root.RaSidebar-paper, & .MuiPaper-root.MuiAppBar-root {
-        top: 0;
-        position: sticky;
-        zIndex: 100;
-    }
-    & .MuiToolbar-root {
-        minHeight: unset;
-    }
-    & .RaList-main {
-        width: 50%;
-    }
-`;
-
-export const CustomLayout: React.FC<LayoutProps> = (props) => {
-
-    const {tenantId} = useContext(TenantContext)
-    const {data: tenantData} = useGetOne<Sequent_Backend_Tenant>("sequent_backend_tenant", {
-      id: tenantId,
-    })
-    const customCss = tenantData?.annotations?.css ?? ""
-    console.log("CustomLayout: customCss :: ", customCss)
-
-    return (
-        <StyledLayout
-            {...props}
-            css={customCss}
-            appBar={CustomAppBar}
-            sidebar={SequentSidebar}
-        />
-    )
-}
+export const CustomLayout: React.FC<LayoutProps> = (props) => (
+    <Layout
+        {...props}
+        sx={{
+            "& .MuiPaper-root.RaSidebar-paper, & .MuiPaper-root.MuiAppBar-root": {
+                top: "0",
+                position: "sticky",
+                zIndex: 100,
+            },
+            "& .MuiToolbar-root": {
+                minHeight: "unset",
+            },
+            "& .RaList-main": {
+                width: "50%",
+            },
+        }}
+        appBar={CustomAppBar}
+        sidebar={SequentSidebar}
+    />
+)
