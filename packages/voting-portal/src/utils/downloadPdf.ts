8a1b700285fcaa1e8112e017a817a1adbc618342
Bimpong <bimpongamoako@gmail.com>
2024-09-26 10:21:35 +0000

    🐞 Voting Portal > Success Screen -> Print PDF file should be downloaded not opened in a new tab

---
+++ packages/voting-portal/src/utils/downloadPdf.ts
@@ -0,0 +1,17 @@
+export function downloadPDF(documentUrl: string, fileName?: string) {
+	// Fetch the file from the URL
+	fetch(documentUrl)
+		.then(response => response.blob())
+		.then(blob => {
+			// Create a blob URL and download it
+			const blobUrl = window.URL.createObjectURL(blob);
+			const link = document.createElement('a');
+			link.href = blobUrl;
+			link.setAttribute('download', fileName ?? 'your-file.pdf'); // Specify the file name
+			document.body.appendChild(link);
+			link.click();
+			link.remove(); // Clean up after the download
+			window.URL.revokeObjectURL(blobUrl); // Free up memory
+		})
+		.catch(err => console.error('Error downloading the file', err));
+}
