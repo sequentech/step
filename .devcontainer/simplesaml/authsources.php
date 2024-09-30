fe9dffe3f92623c63101a89872215ea95e0267ad
Xavier Alsina <xavier.alsina@sequentech.io>
2024-09-30 13:02:17 +0000

    Added some login information

--- .devcontainer/simplesaml/authsources.php
+++ .devcontainer/simplesaml/authsources.php
@@ -15,9 +15,23 @@ $config = array(
         'exampleauth:UserPass',
         'user1:password' => array(
             'email' => 'user1@example.com',
+            'username' => 'user1',
+            'externalId' => 'X1',
         ),
         'user2:password' => array(
             'email' => 'user2@example.com',
+            'username' => 'user2',
+            'externalId' => 'Y2',
+        ),
+        'xalsina:1234' => array(
+            'email' => 'XAVIER.ALSINA@sequentech.io',
+            'username' => 'XALSINA',
+            'externalId' => '39380103D',
+        ),
+        'eduardo:1234' => array(
+            'email' => 'edulix@nvotes.com',
+            'username' => 'edulix',
+            'externalId' => '44055908Y',
         ),
     ),
 
