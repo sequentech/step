fabe4da2742a75022b635c557b6ddf44fb896dd9
Eduardo Robles <edu@sequentech.io>
2024-10-06 15:22:06 +0200

    🐞 Admin Portal: reset table filters on tab change (#831) (#841)
    Parent issue:
    https://github.com/sequentech/meta/issues/2784
    
    Co-authored-by: Enric Badia <enric.badia@gmail.com>

--- packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTextData.tsx
+++ packages/admin-portal/src/resources/ElectionEvent/EditElectionEventTextData.tsx
@@ -2,10 +2,11 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import React from "react"
+import React, {useEffect} from "react"
 import {Sequent_Backend_Election_Event_Extended} from "./EditElectionEventDataForm"
 import {EditBase, Identifier, RaRecord} from "react-admin"
 import EditElectionEventTextDataTable from "./EditElectionEventTextDataTable"
+import {useNavigate, useLocation} from "react-router-dom"
 
 const EditElectionEventTextData = () => {
     const transform = (data: Sequent_Backend_Election_Event_Extended): RaRecord<Identifier> => {
