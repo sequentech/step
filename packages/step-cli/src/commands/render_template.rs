f551ee8435a889a4fbdb7d82157e954034334f15
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2024-10-07 07:51:41 +0000

    cagro fmt + reuse

--- packages/step-cli/src/commands/render_template.rs
+++ packages/step-cli/src/commands/render_template.rs
@@ -1,12 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 use clap::Args;
 use sequent_core::services::reports::render_template;
 use serde_json::{Map, Value};
-use std::{collections::HashMap, fs, path::{Path, PathBuf}};
+use std::{
+    collections::HashMap,
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[derive(Args)]
 #[command(about = "Render a template with variables into a report format")]
 pub struct RenderTemplate {
-
     #[arg(short, long, value_name = "TEMPLATE")]
     template: String,
 
@@ -26,11 +33,10 @@ impl RenderTemplate {
     }
 
     fn generate_report(&self) -> Result<(), Box<dyn std::error::Error>> {
-
         let vars_content = fs::read_to_string(&self.vars)
-        .map_err(|e| format!("Could not read variables file: {}", e))?;
+            .map_err(|e| format!("Could not read variables file: {}", e))?;
         let vars_json: Map<String, Value> = serde_json::from_str(&vars_content)
-        .map_err(|e| format!("Could not parse variables JSON: {}", e))?;
+            .map_err(|e| format!("Could not parse variables JSON: {}", e))?;
 
         let template_content = fs::read_to_string(&self.template)
             .map_err(|e| format!("Could not read template file: {}", e))?;
@@ -42,26 +48,28 @@ impl RenderTemplate {
 
         Ok(())
     }
-
 }
 
 fn render_template_with_vars(
-    template_content: String, 
+    template_content: String,
     vars: Map<String, Value>,
 ) -> Result<Vec<u8>, String> {
-
-
     let base_template_name = "report_base_html";
-    let base_html_template_fild_path = "/workspaces/step/packages/velvet/src/resources/report_base_html.hbs";
+    let base_html_template_fild_path =
+        "/workspaces/step/packages/velvet/src/resources/report_base_html.hbs";
 
-    let base_template_content = fs::read_to_string(&base_html_template_fild_path)
-        .map_err(|e| format!("Could not read template file {}: {}", base_html_template_fild_path, e))?;
+    let base_template_content = fs::read_to_string(&base_html_template_fild_path).map_err(|e| {
+        format!(
+            "Could not read template file {}: {}",
+            base_html_template_fild_path, e
+        )
+    })?;
 
     let mut template_map = HashMap::new();
     template_map.insert("report_content".to_string(), template_content);
     template_map.insert(base_template_name.to_string(), base_template_content);
 
     render_template(base_template_name, template_map, vars)
-        .map(|rendered| rendered.into_bytes()) 
+        .map(|rendered| rendered.into_bytes())
         .map_err(|e| format!("Template rendering error: {}", e))
 }
