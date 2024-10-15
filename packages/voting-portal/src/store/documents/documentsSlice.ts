7bd1f0f38af4653feb7879fd0322337e722f32e7
Félix Robles <felix@sequentech.io>
2024-10-15 09:55:24 -0500

    ✨ Demo/live-preview mode (#917)
    Parent issue: https://github.com/sequentech/meta/issues/591

---
+++ packages/voting-portal/src/store/documents/documentsSlice.ts
@@ -0,0 +1,33 @@
+// SPDX-FileCopyrightText: 2024 Félix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+import {createSlice, PayloadAction} from "@reduxjs/toolkit"
+import {RootState} from "../store"
+import {Sequent_Backend_Document} from "../../gql/graphql"
+
+export interface DocumentState {
+    [documentId: string]: Sequent_Backend_Document | undefined
+}
+
+const initialState: DocumentState = {}
+
+export const documentSlice = createSlice({
+    name: "documents",
+    initialState,
+    reducers: {
+        setDocument: (
+            state: DocumentState,
+            action: PayloadAction<Sequent_Backend_Document>
+        ): DocumentState => {
+            state[action.payload.id] = action.payload
+            return state
+        },
+    },
+})
+
+export const {setDocument} = documentSlice.actions
+
+export const selectDocumentById = (documentId: string) => (state: RootState) =>
+    state.documents[documentId]
+
+export default documentSlice.reducer
