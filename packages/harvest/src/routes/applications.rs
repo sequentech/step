dd9908cba064078b52aaf56d0605a785f023d570
Xavier Alsina <xavier.alsina@sequentech.io>
2024-10-31 10:09:26 +0000

    Added verify application action

---
+++ packages/harvest/src/routes/applications.rs
@@ -0,0 +1,73 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use crate::services::authorization::authorize;
+use crate::types::optional::OptionalId;
+use anyhow::Result;
+use rocket::http::Status;
+use rocket::serde::json::Json;
+use sequent_core::services::jwt;
+use sequent_core::types::permissions::Permissions;
+use serde::Deserialize;
+use serde_json::Value;
+use tracing::instrument;
+
+#[derive(Deserialize, Debug)]
+pub struct ApplicationVerifyBody {
+    tenant_id: String,
+    election_event_id: String,
+    applicant_id: String,
+    applicant_data: Value,
+    applicant_search_attributes: String,
+}
+
+#[instrument(skip(claims))]
+#[post("/verify-application", format = "json", data = "<body>")]
+pub async fn verify_application(
+    claims: jwt::JwtClaims,
+    body: Json<ApplicationVerifyBody>,
+) -> Result<Json<OptionalId>, (Status, String)> {
+    let input = body.into_inner();
+    
+    info!("Verifiying application: {input:?}");
+
+    let required_perm: Permissions = Permissions::APPLICATION_WRITE;
+    authorize(
+        &claims,
+        true,
+        Some(input.tenant_id.clone()),
+        vec![required_perm],
+    )?;
+
+    Ok(Json(Default::default()))
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ApplicationConfirmationBody {
+    tenant_id: String,
+    election_event_id: Option<String>,
+    id: String,
+    user_id: String,
+}
+
+#[instrument(skip(claims))]
+#[post("/confirm-application", format = "json", data = "<body>")]
+pub async fn confirm_application(
+    claims: jwt::JwtClaims,
+    body: Json<ApplicationConfirmationBody>,
+) -> Result<Json<OptionalId>, (Status, String)> {
+    let input = body.into_inner();
+    
+    info!("Confirming application: {input:?}");
+
+    let required_perm: Permissions = Permissions::APPLICATION_WRITE;
+    authorize(
+        &claims,
+        true,
+        Some(input.tenant_id.clone()),
+        vec![required_perm],
+    )?;
+
+    Ok(Json(Default::default()))
+}
