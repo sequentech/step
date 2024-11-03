26dc3f108d09180f591f5d5401edd4e9fbca6017
Maya_Moveo <155538108+maya-byle@users.noreply.github.com>
2024-11-03 13:24:56 +0000

    Delete the Generate option in reports tab

--- packages/admin-portal/src/providers/ElectionEventTallyProvider.tsx
+++ packages/admin-portal/src/providers/ElectionEventTallyProvider.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 import {ETallyType} from "@/types/ceremonies"
 import {IMiruTransmissionPackageData} from "@/types/miru"
 import React, {createContext, useContext, useState} from "react"
