11b8dae06a1dfa7d8aef304e3ba529e62c05895e
Gal Buzaglo <105719976+Galb-moveo@users.noreply.github.com>
2024-11-15 17:22:11 +0200

    ✨ Admin Portal > Reports: Use Background Tasks for execution of report generation (#1110)
    parent issue: https://github.com/sequentech/meta/issues/3604
    
    ---------
    
    Co-authored-by: Xavier Alsina <xavier.alsina@sequentech.io>

--- packages/windmill/src/tasks/create_ballot_receipt.rs
+++ packages/windmill/src/tasks/create_ballot_receipt.rs
@@ -83,6 +83,7 @@ pub async fn create_ballot_receipt(
                         GenerateReportMode::REAL,
                         &hasura_transaction,
                         &keycloak_transaction,
+                        None,
                     )
                     .await
                     .map_err(|err| anyhow!("Error generating ballot receipt report: {err:?}"))?;
