03ed11f17e964bffca42de0c6e17333ad2d882da
Félix Robles <felix@sequentech.io>
2024-11-27 00:25:23 +0100

    🐞 Fix permissions and release notes (#1181)
    Parent issue: https://github.com/sequentech/meta/issues/4200

--- packages/sequent-core/src/types/permissions.rs
+++ packages/sequent-core/src/types/permissions.rs
@@ -11,10 +11,6 @@ use strum_macros::{Display, EnumString};
 pub enum Permissions {
     #[strum(serialize = "admin-user")]
     ADMIN_USER,
-    #[strum(serialize = "application-read")]
-    APPLICATION_READ,
-    #[strum(serialize = "application-write")]
-    APPLICATION_WRITE,
     #[strum(serialize = "tenant-create")]
     TENANT_CREATE,
     #[strum(serialize = "tenant-read")]
@@ -233,6 +229,26 @@ pub enum Permissions {
     SETTINGS_TRUSTEES_TAB,
     #[strum(serialize = "settings-countries-tab")]
     SETTINGS_COUNTRIES_TAB,
+    #[strum(serialize = "application-read")]
+    APPLICATION_READ,
+    #[strum(serialize = "application-write")]
+    APPLICATION_WRITE,
+    #[strum(serialize = "ee-voters-columns")]
+    EE_VOTERS_COLUMNS,
+    #[strum(serialize = "ee-voters-filters")]
+    EE_VOTERS_FILTERS,
+    #[strum(serialize = "ee-voters-logs")]
+    EE_VOTERS_LOGS,
+    #[strum(serialize = "voter-import")]
+    VOTER_IMPORT,
+    #[strum(serialize = "voter-export")]
+    VOTER_EXPORT,
+    #[strum(serialize = "voter-delete")]
+    VOTER_DELETE,
+    #[strum(serialize = "voter-manually-verify")]
+    VOTER_MANUALLY_VERIFY,
+    #[strum(serialize = "voter-change-password")]
+    VOTER_CHANGE_PASSWORD,
 }
 
 #[allow(non_camel_case_types)]
