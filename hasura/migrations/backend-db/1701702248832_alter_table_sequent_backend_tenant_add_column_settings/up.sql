75e2919cfcd8dcce06675deb1aa0e77cdb34f88e
Félix Robles <felix@sequentech.io>
2024-11-27 22:23:56 +0100

    ✨ Update config for offline (#1188)
    Parent issue: https://github.com/sequentech/meta/issues/4209

--- hasura/migrations/backend-db/1701702248832_alter_table_sequent_backend_tenant_add_column_settings/up.sql
+++ hasura/migrations/backend-db/1701702248832_alter_table_sequent_backend_tenant_add_column_settings/up.sql
@@ -1,2 +1,4 @@
 alter table "sequent_backend"."tenant" add column "settings" jsonb
  null;
+
+update "sequent_backend"."tenant" set settings = E'{"i18n":{"en":{"stats.areas":"Posts","areas.common.title":"Posts","tally.trusteeTitle":"SBEI","tally.trusteeTallyTitle":"SBEI","electionEventScreen.tabs.areas":"Posts","publish.action.stopVotingPeriod":"Close Election","electionEventScreen.tally.trustees":"SBEI","keysGeneration.configureStep.trusteeList":"SBEI"}},"help_links":[{"url":"https://docs.google.com/document/d/1opTyXioucIgb6UPQ7Teq3gmAVi6U9O5-HgbZp52BZbY/edit?usp=sharing","i18n":{"en":{"title":"System Manual"},"es":{"title":"Manual del Sistema"},"fr":{"title":"System Manual"},"tl":{"title":"System Manual"},"cat":{"title":"System Manual"}},"title":"System Manual"}],"language_conf":{"default_language_code":"en","enabled_language_codes":["en","tl"]},"enroll_countries":[],"voting_countries":["PH"]}' where id='90505c8a-23a9-4cdf-a26b-4e19f6a097d5';
