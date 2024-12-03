1b6c6b12e633ad92653ded7c5a70c8a99801f6b8
Gal Buzaglo <105719976+Galb-moveo@users.noreply.github.com>
2024-12-03 15:29:46 +0000

    add license

--- packages/admin-portal/src/resources/Publish/PublishExport.tsx
+++ packages/admin-portal/src/resources/Publish/PublishExport.tsx
@@ -1,30 +1,34 @@
-import {FormStyles} from "@/components/styles/FormStyles"
-import {WidgetProps} from "@/components/Widget"
-import {ExportBallotPublicationMutation, Sequent_Backend_Election} from "@/gql/graphql"
-import {useWidgetStore} from "@/providers/WidgetsContextProvider"
-import {EXPORT_BALLOT_PUBLICATION} from "@/queries/ExportBallotPublication"
-import {IPermissions} from "@/types/keycloak"
-import {ETasksExecution} from "@/types/tasksExecution"
-import {useMutation} from "@apollo/client"
-import {Dialog} from "@sequentech/ui-essentials"
-import React, {FC, useState} from "react"
-import {useTranslation} from "react-i18next"
-import {DownloadDocument} from "../User/DownloadDocument"
-import {Identifier, useRecordContext} from "react-admin"
-import {ListActions} from "@/components/ListActions"
-import {useTenantStore} from "@/providers/TenantContextProvider"
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+import { FormStyles } from "@/components/styles/FormStyles"
+import { WidgetProps } from "@/components/Widget"
+import { ExportBallotPublicationMutation, Sequent_Backend_Election } from "@/gql/graphql"
+import { useWidgetStore } from "@/providers/WidgetsContextProvider"
+import { EXPORT_BALLOT_PUBLICATION } from "@/queries/ExportBallotPublication"
+import { IPermissions } from "@/types/keycloak"
+import { ETasksExecution } from "@/types/tasksExecution"
+import { useMutation } from "@apollo/client"
+import { Dialog } from "@sequentech/ui-essentials"
+import React, { FC, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { DownloadDocument } from "../User/DownloadDocument"
+import { Identifier, useRecordContext } from "react-admin"
+import { ListActions } from "@/components/ListActions"
+import { useTenantStore } from "@/providers/TenantContextProvider"
 
 interface PublishExportProps {
     ballotPublicationId?: Identifier | null
 }
 
-const PublishExport: FC<PublishExportProps> = ({ballotPublicationId}) => {
+const PublishExport: FC<PublishExportProps> = ({ ballotPublicationId }) => {
     const [openExport, setOpenExport] = useState(false)
     const [exporting, setExporting] = useState(false)
     const record = useRecordContext<Sequent_Backend_Election>()
     const [tenantId] = useTenantStore()
     const [exportDocumentId, setExportDocumentId] = useState<string | undefined>()
-    const {t} = useTranslation()
+    const { t } = useTranslation()
     const [addWidget, setWidgetTaskId, updateWidgetFail] = useWidgetStore()
     const [ExportBallotPublication] = useMutation<ExportBallotPublicationMutation>(
         EXPORT_BALLOT_PUBLICATION,
@@ -48,7 +52,7 @@ const PublishExport: FC<PublishExportProps> = ({ballotPublicationId}) => {
         try {
             currWidget = addWidget(ETasksExecution.EXPORT_BALLOT_PUBLICATION)
 
-            const {data: ballotResponse, errors} = await ExportBallotPublication({
+            const { data: ballotResponse, errors } = await ExportBallotPublication({
                 variables: {
                     tenantId,
                     electionEventId: record.election_event_id
