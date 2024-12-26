d633cba34ee5f2c51cd0779e5d004c99ae94abbf
Félix Robles <felix@sequentech.io>
2024-12-26 12:18:50 +0000

    wip

--- scripts/build-airgapped.sh
+++ scripts/build-airgapped.sh
@@ -113,6 +113,7 @@ add-keycloak-data-to-tarball() {
     tmpdir=$(mktemp -d)
     mkdir -p $tmpdir/keycloak
     cp -r $PROJECT_ROOT/.devcontainer/keycloak/import $tmpdir/keycloak
+    $PROJECT_ROOT/scripts/replacements.sh $PROJECT_ROOT/packages/windmill/external-bin/janitor/config/baseConfig.json $tmpdir/keycloak/tenant-90505c8a-23a9-4cdf-a26b-4e19f6a097d5.json
     tar --append -C $tmpdir --file=$DELIVERABLE_TARBALL keycloak
 }
 
