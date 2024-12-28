e1038be6055706fa536a364a90bdee76a487846c
Xavier Alsina <xavier.alsina@sequentech.io>
2024-12-28 12:26:03 +0000

    Update graphql

--- packages/ballot-verifier/src/gql/graphql.ts
+++ packages/ballot-verifier/src/gql/graphql.ts
@@ -11057,6 +11057,7 @@ export type Sequent_Backend_Report = {
     election_id?: Maybe<Scalars["uuid"]["output"]>
     encryption_policy: Scalars["String"]["output"]
     id: Scalars["uuid"]["output"]
+    permission_label?: Maybe<Scalars["String"]["output"]>
     report_type: Scalars["String"]["output"]
     template_alias?: Maybe<Scalars["String"]["output"]>
     tenant_id: Scalars["uuid"]["output"]
@@ -11104,6 +11105,7 @@ export type Sequent_Backend_Report_Bool_Exp = {
     election_id?: InputMaybe<Uuid_Comparison_Exp>
     encryption_policy?: InputMaybe<String_Comparison_Exp>
     id?: InputMaybe<Uuid_Comparison_Exp>
+    permission_label?: InputMaybe<String_Comparison_Exp>
     report_type?: InputMaybe<String_Comparison_Exp>
     template_alias?: InputMaybe<String_Comparison_Exp>
     tenant_id?: InputMaybe<Uuid_Comparison_Exp>
@@ -11138,6 +11140,7 @@ export type Sequent_Backend_Report_Insert_Input = {
     election_id?: InputMaybe<Scalars["uuid"]["input"]>
     encryption_policy?: InputMaybe<Scalars["String"]["input"]>
     id?: InputMaybe<Scalars["uuid"]["input"]>
+    permission_label?: InputMaybe<Scalars["String"]["input"]>
     report_type?: InputMaybe<Scalars["String"]["input"]>
     template_alias?: InputMaybe<Scalars["String"]["input"]>
     tenant_id?: InputMaybe<Scalars["uuid"]["input"]>
@@ -11151,6 +11154,7 @@ export type Sequent_Backend_Report_Max_Fields = {
     election_id?: Maybe<Scalars["uuid"]["output"]>
     encryption_policy?: Maybe<Scalars["String"]["output"]>
     id?: Maybe<Scalars["uuid"]["output"]>
+    permission_label?: Maybe<Scalars["String"]["output"]>
     report_type?: Maybe<Scalars["String"]["output"]>
     template_alias?: Maybe<Scalars["String"]["output"]>
     tenant_id?: Maybe<Scalars["uuid"]["output"]>
@@ -11164,6 +11168,7 @@ export type Sequent_Backend_Report_Min_Fields = {
     election_id?: Maybe<Scalars["uuid"]["output"]>
     encryption_policy?: Maybe<Scalars["String"]["output"]>
     id?: Maybe<Scalars["uuid"]["output"]>
+    permission_label?: Maybe<Scalars["String"]["output"]>
     report_type?: Maybe<Scalars["String"]["output"]>
     template_alias?: Maybe<Scalars["String"]["output"]>
     tenant_id?: Maybe<Scalars["uuid"]["output"]>
@@ -11193,6 +11198,7 @@ export type Sequent_Backend_Report_Order_By = {
     election_id?: InputMaybe<Order_By>
     encryption_policy?: InputMaybe<Order_By>
     id?: InputMaybe<Order_By>
+    permission_label?: InputMaybe<Order_By>
     report_type?: InputMaybe<Order_By>
     template_alias?: InputMaybe<Order_By>
     tenant_id?: InputMaybe<Order_By>
@@ -11223,6 +11229,8 @@ export enum Sequent_Backend_Report_Select_Column {
     /** column name */
     Id = "id",
     /** column name */
+    PermissionLabel = "permission_label",
+    /** column name */
     ReportType = "report_type",
     /** column name */
     TemplateAlias = "template_alias",
@@ -11238,6 +11246,7 @@ export type Sequent_Backend_Report_Set_Input = {
     election_id?: InputMaybe<Scalars["uuid"]["input"]>
     encryption_policy?: InputMaybe<Scalars["String"]["input"]>
     id?: InputMaybe<Scalars["uuid"]["input"]>
+    permission_label?: InputMaybe<Scalars["String"]["input"]>
     report_type?: InputMaybe<Scalars["String"]["input"]>
     template_alias?: InputMaybe<Scalars["String"]["input"]>
     tenant_id?: InputMaybe<Scalars["uuid"]["input"]>
@@ -11259,6 +11268,7 @@ export type Sequent_Backend_Report_Stream_Cursor_Value_Input = {
     election_id?: InputMaybe<Scalars["uuid"]["input"]>
     encryption_policy?: InputMaybe<Scalars["String"]["input"]>
     id?: InputMaybe<Scalars["uuid"]["input"]>
+    permission_label?: InputMaybe<Scalars["String"]["input"]>
     report_type?: InputMaybe<Scalars["String"]["input"]>
     template_alias?: InputMaybe<Scalars["String"]["input"]>
     tenant_id?: InputMaybe<Scalars["uuid"]["input"]>
@@ -11278,6 +11288,8 @@ export enum Sequent_Backend_Report_Update_Column {
     EncryptionPolicy = "encryption_policy",
     /** column name */
     Id = "id",
+    /** column name */
+    PermissionLabel = "permission_label",
     /** column name */
     ReportType = "report_type",
     /** column name */
--- packages/voting-portal/src/gql/graphql.ts
+++ packages/voting-portal/src/gql/graphql.ts
@@ -11488,6 +11488,7 @@ export type Sequent_Backend_Report = {
   election_id?: Maybe<Scalars['uuid']['output']>;
   encryption_policy: Scalars['String']['output'];
   id: Scalars['uuid']['output'];
+  permission_label?: Maybe<Scalars['String']['output']>;
   report_type: Scalars['String']['output'];
   template_alias?: Maybe<Scalars['String']['output']>;
   tenant_id: Scalars['uuid']['output'];
@@ -11537,6 +11538,7 @@ export type Sequent_Backend_Report_Bool_Exp = {
   election_id?: InputMaybe<Uuid_Comparison_Exp>;
   encryption_policy?: InputMaybe<String_Comparison_Exp>;
   id?: InputMaybe<Uuid_Comparison_Exp>;
+  permission_label?: InputMaybe<String_Comparison_Exp>;
   report_type?: InputMaybe<String_Comparison_Exp>;
   template_alias?: InputMaybe<String_Comparison_Exp>;
   tenant_id?: InputMaybe<Uuid_Comparison_Exp>;
@@ -11571,6 +11573,7 @@ export type Sequent_Backend_Report_Insert_Input = {
   election_id?: InputMaybe<Scalars['uuid']['input']>;
   encryption_policy?: InputMaybe<Scalars['String']['input']>;
   id?: InputMaybe<Scalars['uuid']['input']>;
+  permission_label?: InputMaybe<Scalars['String']['input']>;
   report_type?: InputMaybe<Scalars['String']['input']>;
   template_alias?: InputMaybe<Scalars['String']['input']>;
   tenant_id?: InputMaybe<Scalars['uuid']['input']>;
@@ -11584,6 +11587,7 @@ export type Sequent_Backend_Report_Max_Fields = {
   election_id?: Maybe<Scalars['uuid']['output']>;
   encryption_policy?: Maybe<Scalars['String']['output']>;
   id?: Maybe<Scalars['uuid']['output']>;
+  permission_label?: Maybe<Scalars['String']['output']>;
   report_type?: Maybe<Scalars['String']['output']>;
   template_alias?: Maybe<Scalars['String']['output']>;
   tenant_id?: Maybe<Scalars['uuid']['output']>;
@@ -11597,6 +11601,7 @@ export type Sequent_Backend_Report_Min_Fields = {
   election_id?: Maybe<Scalars['uuid']['output']>;
   encryption_policy?: Maybe<Scalars['String']['output']>;
   id?: Maybe<Scalars['uuid']['output']>;
+  permission_label?: Maybe<Scalars['String']['output']>;
   report_type?: Maybe<Scalars['String']['output']>;
   template_alias?: Maybe<Scalars['String']['output']>;
   tenant_id?: Maybe<Scalars['uuid']['output']>;
@@ -11626,6 +11631,7 @@ export type Sequent_Backend_Report_Order_By = {
   election_id?: InputMaybe<Order_By>;
   encryption_policy?: InputMaybe<Order_By>;
   id?: InputMaybe<Order_By>;
+  permission_label?: InputMaybe<Order_By>;
   report_type?: InputMaybe<Order_By>;
   template_alias?: InputMaybe<Order_By>;
   tenant_id?: InputMaybe<Order_By>;
@@ -11656,6 +11662,8 @@ export enum Sequent_Backend_Report_Select_Column {
   /** column name */
   Id = 'id',
   /** column name */
+  PermissionLabel = 'permission_label',
+  /** column name */
   ReportType = 'report_type',
   /** column name */
   TemplateAlias = 'template_alias',
@@ -11671,6 +11679,7 @@ export type Sequent_Backend_Report_Set_Input = {
   election_id?: InputMaybe<Scalars['uuid']['input']>;
   encryption_policy?: InputMaybe<Scalars['String']['input']>;
   id?: InputMaybe<Scalars['uuid']['input']>;
+  permission_label?: InputMaybe<Scalars['String']['input']>;
   report_type?: InputMaybe<Scalars['String']['input']>;
   template_alias?: InputMaybe<Scalars['String']['input']>;
   tenant_id?: InputMaybe<Scalars['uuid']['input']>;
@@ -11692,6 +11701,7 @@ export type Sequent_Backend_Report_Stream_Cursor_Value_Input = {
   election_id?: InputMaybe<Scalars['uuid']['input']>;
   encryption_policy?: InputMaybe<Scalars['String']['input']>;
   id?: InputMaybe<Scalars['uuid']['input']>;
+  permission_label?: InputMaybe<Scalars['String']['input']>;
   report_type?: InputMaybe<Scalars['String']['input']>;
   template_alias?: InputMaybe<Scalars['String']['input']>;
   tenant_id?: InputMaybe<Scalars['uuid']['input']>;
@@ -11711,6 +11721,8 @@ export enum Sequent_Backend_Report_Update_Column {
   EncryptionPolicy = 'encryption_policy',
   /** column name */
   Id = 'id',
+  /** column name */
+  PermissionLabel = 'permission_label',
   /** column name */
   ReportType = 'report_type',
   /** column name */
