88c115e952fc9cf7c3fa99fe34a91f5baf3e2ac1
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-31 12:09:43 +0100

    WIP

--- .devcontainer/openwhisk/deploy-doc_renderer.sh
+++ .devcontainer/openwhisk/deploy-doc_renderer.sh
@@ -6,6 +6,11 @@ wsk property set \
   --apihost 'http://127.0.0.2:3233' \
   --auth '23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP' || true
 
+until wsk list; do
+    echo "Waiting for OpenWhisk to be ready"
+    sleep 1
+done
+
 wsk package create pdf-tools || true
 
 wsk action create pdf-tools/doc_renderer \
