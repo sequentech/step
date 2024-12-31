9a61b7effb88406b778d09befe094724d035c85b
Rafael Fernández López <ereslibre@ereslibre.es>
2024-12-31 01:38:42 +0100

    WIP

---
+++ .devcontainer/openwhisk/deploy-doc_renderer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+
+wsk property set \
+  --apihost 'http://127.0.0.2:3233' \
+  --auth '23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP' || true
+
+wsk package create pdf-tools || true
+
+wsk action create pdf-tools/doc_renderer \
+  --web no \
+  --docker registry.ereslibre.net/doc_renderer:latest || true
