d2bca357edc660cb9f994b7aee69c0d1ec2a0a06
Félix Robles <felix@sequentech.io>
2025-01-03 03:01:32 +0000

    wip

--- scripts/build-airgapped.sh
+++ scripts/build-airgapped.sh
@@ -133,7 +133,7 @@ add-hasura-data-to-tarball() {
 add-minio-config-to-tarball() {
     tmpdir=$(mktemp -d)
     mkdir -p $tmpdir/minio
-    cp -r $PROJECT_ROOT/.devcontainer/minio $tmpdir/minio
+    cp -r $PROJECT_ROOT/.devcontainer/minio/nginx $tmpdir/minio/nginx
     tar --append -C $tmpdir --file=$DELIVERABLE_TARBALL minio
 }
 
@@ -156,25 +156,25 @@ clean-artifacts-root() {
 mkdir -p $DELIVERABLE_PATH $IMAGE_ARTIFACTS_PATH
 tar -cf $DELIVERABLE_TARBALL -T /dev/null
 
-build-images
-pull-images
+# build-images
+# pull-images
 
-# Archive all images
-for image in $(all-images); do
-    archive-image-artifact "$image"
-done
+# # Archive all images
+# for image in $(all-images); do
+#     archive-image-artifact "$image"
+# done
 
-add-images-to-tarball
-add-dotenv-to-tarball
+# add-images-to-tarball
+# add-dotenv-to-tarball
 add-minio-config-to-tarball
-add-docker-compose-to-tarball
-add-keycloak-data-to-tarball
-add-trustees-data-to-tarball
-add-hasura-data-to-tarball
-add-up-script-to-tarball
-add-database-init-to-tarball
-add-readme-to-tarball
-add-janitor-to-tarball
+# add-docker-compose-to-tarball
+# add-keycloak-data-to-tarball
+# add-trustees-data-to-tarball
+# add-hasura-data-to-tarball
+# add-up-script-to-tarball
+# add-database-init-to-tarball
+# add-readme-to-tarball
+# add-janitor-to-tarball
 
 clean-artifacts-root
 
