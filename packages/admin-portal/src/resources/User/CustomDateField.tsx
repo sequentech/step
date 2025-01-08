5b4d8aa8b76a2928d109c3e78765e4d6d1932600
Enric Badia <enric.badia@gmail.com>
2025-01-08 18:18:24 +0000

    changed custom date field

--- packages/admin-portal/src/resources/User/CustomDateField.tsx
+++ packages/admin-portal/src/resources/User/CustomDateField.tsx
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 import React from "react"
 import {useRecordContext} from "react-admin"
+import {parseISO, format} from "date-fns"
 
 const CustomDateField = ({
     source,
@@ -25,11 +26,20 @@ const CustomDateField = ({
     }
 
     try {
-        const date = new Date(dateValue)
-        if (isNaN(date.getTime())) {
-            throw new Error("Invalid date")
-        }
-        return <span>{date.toLocaleDateString()}</span>
+        // TODO: ask which date format is better
+        // native
+        // const [year, month, day] = dateValue[0].split("-")
+        // const date = new Date(Date.UTC(+year, +month - 1, +day))
+        // if (isNaN(date.getTime())) {
+        //     throw new Error("Invalid date")
+        // }
+
+        // const isoString = date.toISOString().split("T")[0].split("-").reverse().join("/")
+
+        // return <span>{isoString}</span>
+
+        // using date-fns
+        return <span>{format(parseISO(dateValue[0]), "PP")}</span>
     } catch {
         return <span>{emptyText}</span>
     }
