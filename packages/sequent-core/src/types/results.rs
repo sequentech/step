c0a0c7934dc37fc33ee241bf2da19c17a9f1fdf3
Eduardo Robles <edu@sequentech.io>
2025-01-12 02:48:49 +0000

    make it build

--- packages/sequent-core/src/types/results.rs
+++ packages/sequent-core/src/types/results.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::default::Default;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub enum ResultDocumentType {
     Json,
     Pdf,
