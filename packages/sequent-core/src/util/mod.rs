76d8e9c6f6b0c0f9224c5a8f0397c0b13c28a073
Rafael Fernández López <ereslibre@ereslibre.es>
2025-01-15 16:03:13 +0100

    fix sequent-core wasm build

--- packages/sequent-core/src/util/mod.rs
+++ packages/sequent-core/src/util/mod.rs
@@ -1,12 +1,14 @@
 // SPDX-FileCopyrightText: 2022 Felix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-pub mod aws;
 pub mod convert_vec;
 pub mod date;
 pub mod date_time;
 pub mod mime;
 pub mod normalize_vote;
+
+#[cfg(feature = "reports")]
+pub mod aws;
 
 #[cfg(feature = "log")]
 pub mod init_log;
