b57cfdf979247e0a5628d2468de2f54ea5af0591
Maya_Moveo <155538108+maya-byle@users.noreply.github.com>
2025-01-21 09:11:56 +0000

    import publications wip

--- packages/windmill/src/services/import/mod.rs
+++ packages/windmill/src/services/import/mod.rs
@@ -4,6 +4,7 @@
 
 pub mod import_bulletin_boards;
 pub mod import_election_event;
+pub mod import_publications;
 pub mod import_scheduled_events;
 pub mod import_tenant;
 pub mod import_tenant_config;
