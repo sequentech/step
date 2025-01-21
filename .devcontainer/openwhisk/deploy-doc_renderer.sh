1134838f407fe701f28f0f41a95d497630e72d8d
Rafael Fernández López <ereslibre@ereslibre.es>
2025-01-21 19:27:57 +0100

    WIP

--- .devcontainer/openwhisk/deploy-doc_renderer.sh
+++ .devcontainer/openwhisk/deploy-doc_renderer.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-DOC_RENDERER_IMAGE="${REGISTRY}/doc_renderer:latest"
+DOC_RENDERER_IMAGE="localhost:5000/doc_renderer:latest"
 
 wsk property set \
   --apihost 'http://127.0.0.2:3233' \
