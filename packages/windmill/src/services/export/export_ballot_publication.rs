12f37f24bebe3df4e2f55c23daa528e719c7291a
Maya_Moveo <155538108+maya-byle@users.noreply.github.com>
2025-01-22 08:16:48 +0000

    clean

--- packages/windmill/src/services/export/export_ballot_publication.rs
+++ packages/windmill/src/services/export/export_ballot_publication.rs
@@ -9,7 +9,6 @@ use crate::services::{
 use anyhow::{anyhow, Result};
 use csv::Writer;
 use deadpool_postgres::{Client as DbClient, Transaction};
-use sequent_core::serialization::deserialize_with_path;
 use sequent_core::serialization::deserialize_with_path::deserialize_str;
 use sequent_core::types::hasura::core::Document;
 use sequent_core::types::hasura::core::{BallotPublication, Template};
@@ -17,49 +16,6 @@ use serde::{Deserialize, Serialize};
 use serde_json::{json, Map, Value};
 use tempfile::TempPath;
 use tracing::{event, info, instrument, Level};
-
-// #[instrument(err, skip(transaction))]
-// pub async fn read_export_data(
-//     transaction: &Transaction<'_>,
-//     election_event_id: &str,
-//     tenant_id: &str,
-//     ballot_design: &str,
-// ) -> Result<String> {
-//     let ballot_design_json: Value = deserialize_with_path::deserialize_str(ballot_design)?;
-//     let mut csv_data = vec![];
-
-//     fn flatten_json(prefix: String, value: &Value, csv_data: &mut Vec<(String, String)>) {
-//         match value {
-//             Value::Object(map) => {
-//                 for (k, v) in map {
-//                     flatten_json(format!("{}.{}", prefix, k), v, csv_data);
-//                 }
-//             }
-//             Value::Array(arr) => {
-//                 for (i, v) in arr.iter().enumerate() {
-//                     flatten_json(format!("{}[{}]", prefix, i), v, csv_data);
-//                 }
-//             }
-//             _ => {
-//                 csv_data.push((prefix, value.to_string()));
-//             }
-//         }
-//     }
-//     flatten_json("".to_string(), &ballot_design_json, &mut csv_data);
-//     let mut writer = Writer::from_writer(vec![]);
-//     writer.write_record(&["key", "value"])?;
-
-//     for (key, value) in csv_data {
-//         writer.write_record(&[key.trim_start_matches('.'), &value])?;
-//     }
-
-//     writer.flush()?;
-
-//     let csv_data = String::from_utf8(writer.into_inner()?)
-//         .map_err(|e| anyhow!("Error converting CSV data to String: {e:?}"))?;
-
-//     Ok(csv_data)
-// }
 
 #[instrument(err, skip(transaction, data))]
 pub async fn write_export_document(
@@ -187,71 +143,3 @@ pub async fn export_ballot_publications(
 
     Ok(temp_path)
 }
-
-// #[instrument(err, skip(hasura_transaction))]
-// pub async fn write_export_document_csv(
-//     data: Vec<BallotPublication>,
-//     hasura_transaction: &Transaction<'_>,
-//     document_id: &str,
-//     tenant_id: &str,
-//     election_event_id: &str,
-// ) -> Result<(TempPath)> {
-//     let headers = if let Some(example_event) = data.get(0) {
-//         serde_json::to_value(example_event)?
-//             .as_object()
-//             .ok_or_else(|| {
-//                 anyhow!("Failed to convert Ballots Publication to JSON object for headers")
-//             })?
-//             .keys()
-//             .cloned()
-//             .collect::<Vec<String>>()
-//     } else {
-//         vec![
-//             "id".to_string(),
-//             "tenant_id".to_string(),
-//             "election_event_id".to_string(),
-//             "labels".to_string(),
-//             "annotations".to_string(),
-//             "created_at".to_string(),
-//             "deleted_at".to_string(),
-//             "created_by_user".to_string(),
-//             "is_generated".to_string(),
-//             "election_ids".to_string(),
-//             "published_at".to_string(),
-//             "election_id".to_string(),
-//         ]
-//     };
-
-//     let name = format!("ballot-publications-{}", election_event_id);
-
-//     let mut writer = Writer::from_writer(vec![]);
-//     writer.write_record(&headers)?;
-
-//     for ballot_publication in data.clone() {
-//         let values: Vec<String> = serde_json::to_value(ballot_publication)?
-//             .as_object()
-//             .ok_or_else(|| anyhow!("Failed to convert ballot publication to JSON object"))?
-//             .values()
-//             .map(|value| value.to_string())
-//             .collect();
-
-//         writer.write_record(&values)?;
-//     }
-
-//     let data_bytes = writer
-//         .into_inner()
-//         .map_err(|e| anyhow!("Error converting writer into inner: {e:?}"))?;
-
-//     let temp_path = write_export_document(
-//         &hasura_transaction,
-//         data_bytes,
-//         document_id,
-//         election_event_id,
-//         tenant_id,
-//         false,
-//     )
-//     .await
-//     .map_err(|e| anyhow!("Error writing export document: {e:?}"))?;
-
-//     Ok(temp_path)
-// }
