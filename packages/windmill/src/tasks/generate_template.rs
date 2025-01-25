8206bdfe6caa6d19a908c292e4cbc377328d6f49
Félix Robles <felix@sequentech.io>
2025-01-25 17:57:05 +0000

    wip

--- packages/windmill/src/tasks/generate_template.rs
+++ packages/windmill/src/tasks/generate_template.rs
@@ -2,14 +2,18 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
+use crate::postgres::results_event::get_results_event_by_id;
+use crate::postgres::tally_session::get_tally_session_by_id;
+use crate::postgres::tally_session_execution::get_last_tally_session_execution;
 use crate::services::database::get_hasura_pool;
 use crate::services::tasks_execution::update_fail;
 use crate::types::error::Error;
 use crate::types::error::Result;
 use anyhow::{anyhow, Context, Result as AnyhowResult};
 use celery::error::TaskError;
-use deadpool_postgres::Client as DbClient;
+use deadpool_postgres::{Client as DbClient, Transaction};
 use rocket::serde::json::Json;
+use sequent_core::types::ceremonies::TallyExecutionStatus;
 use sequent_core::types::hasura::core::TasksExecution;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
@@ -32,23 +36,69 @@ pub enum EGenerateTemplate {
 }
 
 async fn generate_ballot_images(
-    election_event_id: String,
-    election_id: String,
-    tally_session_id: String,
+    hasura_transaction: &Transaction<'_>,
+    tenant_id: &str,
+    election_event_id: &str,
+    election_id: &str,
+    tally_session_id: &str,
 ) -> AnyhowResult<()> {
+    let tally_session = get_tally_session_by_id(
+        hasura_transaction,
+        tenant_id,
+        election_event_id,
+        tally_session_id,
+    )
+    .await?;
+
+    if !tally_session.is_execution_completed
+        || tally_session.execution_status != Some(TallyExecutionStatus::SUCCESS.to_string())
+    {
+        return Err(anyhow!("Tally session is not completed"));
+    }
+
+    let last_tally_session_execution = get_last_tally_session_execution(
+        hasura_transaction,
+        tenant_id,
+        election_event_id,
+        tally_session_id,
+    )
+    .await?
+    .ok_or(anyhow!("No tally session execution found"))?;
+
+    let results_event_id = last_tally_session_execution
+        .results_event_id
+        .ok_or(anyhow!("No results event found"))?;
+
+    let results_event = get_results_event_by_id(
+        hasura_transaction,
+        tenant_id,
+        election_event_id,
+        &results_event_id,
+    )
+    .await?;
+
+    let original_tgz_document_id = results_event
+        .documents
+        .map(|document| document.tar_gz_original)
+        .flatten()
+        .ok_or(anyhow!("No tar gz document found"))?;
+
     Ok(())
 }
 
 async fn generate_vote_receipts(
-    election_event_id: String,
-    election_id: String,
-    tally_session_id: String,
+    hasura_transaction: &Transaction<'_>,
+    tenant_id: &str,
+    election_event_id: &str,
+    election_id: &str,
+    tally_session_id: &str,
 ) -> AnyhowResult<()> {
     Ok(())
 }
 
 #[instrument(err)]
 async fn generate_template_block(
+    tenant_id: String,
     document_id: String,
     input: EGenerateTemplate,
     task_execution: Option<TasksExecution>,
@@ -80,14 +130,28 @@ async fn generate_template_block(
             election_id,
             tally_session_id,
         } => {
-            generate_ballot_images(election_event_id, election_id, tally_session_id).await?;
+            generate_ballot_images(
+                &hasura_transaction,
+                &tenant_id,
+                &election_event_id,
+                &election_id,
+                &tally_session_id,
+            )
+            .await?;
         }
         EGenerateTemplate::VoteReceipts {
             election_event_id,
             election_id,
             tally_session_id,
         } => {
-            generate_vote_receipts(election_event_id, election_id, tally_session_id).await?;
+            generate_vote_receipts(
+                &hasura_transaction,
+                &tenant_id,
+                &election_event_id,
+                &election_id,
+                &tally_session_id,
+            )
+            .await?;
         }
     }
 
@@ -103,6 +167,7 @@ async fn generate_template_block(
 #[wrap_map_err::wrap_map_err(TaskError)]
 #[celery::task]
 pub async fn generate_template(
+    tenant_id: String,
     document_id: String,
     input: EGenerateTemplate,
     task_execution: Option<TasksExecution>,
@@ -112,9 +177,15 @@ pub async fn generate_template(
     let handle = tokio::task::spawn_blocking({
         move || {
             tokio::runtime::Handle::current().block_on(async move {
-                generate_template_block(document_id, input, task_execution, executer_username)
-                    .await
-                    .map_err(|err| anyhow!("generate_report error: {:?}", err))
+                generate_template_block(
+                    tenant_id,
+                    document_id,
+                    input,
+                    task_execution,
+                    executer_username,
+                )
+                .await
+                .map_err(|err| anyhow!("generate_report error: {:?}", err))
             })
         }
     });
