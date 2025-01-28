9fb3917c4ccf9131e75e5bd1e8bdffdfa84c713d
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2025-01-28 12:36:45 +0000

    wip

--- packages/sequent-core/src/services/connection.rs
+++ packages/sequent-core/src/services/connection.rs
@@ -6,7 +6,7 @@ use rocket::http::Status;
 use rocket::request::{FromRequest, Outcome, Request};
 use serde::{Deserialize, Serialize};
 use std::net::IpAddr;
-use tracing::{instrument, warn};
+use tracing::{info, instrument, warn};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct AuthHeaders {
@@ -102,25 +102,37 @@ impl<'r> FromRequest<'r> for UserLocation {
     }
 }
 
+const MOCK_ACCESS_TOKEN: &str = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJJYXRJRWFNd004REEyVGFWa3ZEQklxbjcxQ2plRjhLeEx5VU9MbnBjRXlJIn0.eyJleHAiOjE3MzgwNTgxMTMsImlhdCI6MTczODA1NzgxMywianRpIjoiYTVkOGJmZDEtNTRkNy00NGI4LWIxMTgtYzJkMWEyYTQzNGI0IiwiaXNzIjoiaHR0cDovLzEyNy4wLjAuMTo4MDkwL3JlYWxtcy90ZW5hbnQtOTA1MDVjOGEtMjNhOS00Y2RmLWEyNmItNGUxOWY2YTA5N2Q1IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjQ4N2VhNDk2LTlmMDYtNDE0OC1hZDA2LTRhMTJmOWJiNjk4MCIsInR5cCI6IkJlYXJlciIsImF6cCI6InNlcnZpY2UtYWNjb3VudCIsImFjciI6InNpbHZlciIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZXNvdXJjZV9hY2Nlc3MiOnsic2VydmljZS1hY2NvdW50Ijp7InJvbGVzIjpbInVtYV9wcm90ZWN0aW9uIiwic2VydmljZS1yb2xlIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiY2xpZW50SG9zdCI6IjE3Mi4xOC4wLjEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImh0dHBzOi8vaGFzdXJhLmlvL2p3dC9jbGFpbXMiOnsieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoic2VydmljZS1hY2NvdW50IiwieC1oYXN1cmEtdGVuYW50LWlkIjoiOTA1MDVjOGEtMjNhOS00Y2RmLWEyNmItNGUxOWY2YTA5N2Q1IiwieC1oYXN1cmEtdXNlci1pZCI6IjQ4N2VhNDk2LTlmMDYtNDE0OC1hZDA2LTRhMTJmOWJiNjk4MCIsIngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsic2VydmljZS1hY2NvdW50Il19LCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtc2VydmljZS1hY2NvdW50IiwiY2xpZW50QWRkcmVzcyI6IjE3Mi4xOC4wLjEiLCJjbGllbnRfaWQiOiJzZXJ2aWNlLWFjY291bnQifQ.g_Y1WbS0C_w-6HyahoDeNDihCIwr-5f-fJ0K2HVwbC1JHCZ50SdiWnc8Mge1DZd889fHzK7zPPdY0sWtD06dIJRKipXun6p9oW54j1lSdRt6SCZR1GZZwXH-tMumZwWff9qoniKCKHGlOXVxTY1_Tr00snRI6JbpYEKNt65ZKu7wVcXYiNsbLDbr349umF1g1UH2rYq8DGc49PuwraX4xho1RBxrkOYM3bR1cqTMOEFq2fHXhH9fTTyNrN2vmyWfQs0Znez8che2ZRxCr_UQi7drnLOkmd-jU884_LojiHbkVKreiCFgXNd8Ypu8Qn9BgpwgL8mp9WUCG_rJ81hg3w";
+
 #[derive(Debug)]
 pub struct DatafixClaims(pub JwtClaims);
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for DatafixClaims {
     type Error = ();
+    #[instrument]
     async fn from_request(
         request: &'r Request<'_>,
     ) -> Outcome<Self, Self::Error> {
         let headers = request.headers().clone();
+        // .... WIP
+        let tenant_id = headers.get_one("tenant_id");
+        let event_id = headers.get_one("event_id");
+        info!("tenant_id: {:?} event_id: {:?}", tenant_id, event_id);
+
         match headers.get_one("authorization") {
             Some(authorization) => {
-                let mut data = authorization.split(":");
-                let client_id = data.nth(0);
-                let client_secret = data.nth(1);
+                let mut auth_collection = authorization.split(":");
+                let client_id = auth_collection.nth(0);
+                let client_secret = auth_collection.nth(0);
+
+                info!("{:?}:{:?}", client_id, client_secret);
+
+                let access_token: &str = MOCK_ACCESS_TOKEN; // get access_token in similar way as get_client_credentials
+
                 // .... WIP
                 // Do the DB query to get the tenant ID if it´s not directly
                 // provided from the client.
-                let access_token: &str = "access_token"; // get access_token in similar way as get_client_credentials
                 match decode_jwt(access_token) {
                     Ok(jwt) => Outcome::Success(DatafixClaims(jwt)),
                     Err(err) => {
@@ -130,7 +142,7 @@ impl<'r> FromRequest<'r> for DatafixClaims {
                 }
             }
             None => {
-                warn!("DatafixClaims guard: headers: {headers:?}");
+                warn!("DatafixClaims guard: No authorization in headers: {headers:?}");
                 Outcome::Error((Status::Unauthorized, ()))
             }
         }
