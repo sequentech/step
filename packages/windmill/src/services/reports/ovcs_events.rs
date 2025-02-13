094ff40773e757779fb36a8d3c63ae610ede7dce
Rafael Fernández López <ereslibre@ereslibre.es>
2025-02-13 16:42:22 +0100

    ✨ Move all PDF generation to Lambda functions (#1561)
    Parent issue: https://github.com/sequentech/meta/issues/1717

--- packages/windmill/src/services/reports/ovcs_events.rs
+++ packages/windmill/src/services/reports/ovcs_events.rs
@@ -24,7 +24,7 @@ use anyhow::{anyhow, Context, Result};
 use async_trait::async_trait;
 use deadpool_postgres::Transaction;
 use sequent_core::services::pdf;
-use sequent_core::signatures::temp_path::get_public_assets_path_env_var;
+use sequent_core::util::temp_path::get_public_assets_path_env_var;
 use sequent_core::{
     ballot::StringifiedPeriodDates, services::s3::get_minio_url, types::hasura::core::Election,
 };
