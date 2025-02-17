3a8087bd1f1da8d2ee8e875ce6d1ad97c3ffb7d7
Félix Robles <felix@sequentech.io>
2025-02-17 03:41:55 +0000

    wip

--- packages/windmill/src/services/reports/audit_logs.rs
+++ packages/windmill/src/services/reports/audit_logs.rs
@@ -83,57 +83,14 @@ impl AuditLogsTemplate {
     }
 }
 
-#[async_trait]
-impl TemplateRenderer for AuditLogsTemplate {
-    type UserData = UserData;
-    type SystemData = SystemData;
-
-    fn get_report_type(&self) -> ReportType {
-        ReportType::AUDIT_LOGS
-    }
-
-    fn get_tenant_id(&self) -> String {
-        self.ids.tenant_id.clone()
-    }
-
-    fn get_election_event_id(&self) -> String {
-        self.ids.election_event_id.clone()
-    }
-
-    fn get_initial_template_alias(&self) -> Option<String> {
-        self.ids.template_alias.clone()
-    }
-
-    fn get_report_origin(&self) -> ReportOriginatedFrom {
-        self.ids.report_origin
-    }
-
-    fn get_election_id(&self) -> Option<String> {
-        self.ids.election_id.clone()
-    }
-
-    fn base_name(&self) -> String {
-        "audit_logs".to_string()
-    }
-
-    fn prefix(&self) -> String {
-        format!("audit_logs_{}", self.ids.election_event_id)
-    }
-    async fn count_items(&self) -> Option<i64> {
-        None
-    }
-
+impl AuditLogsTemplate {
     #[instrument(err, skip(self, hasura_transaction, keycloak_transaction))]
-    async fn prepare_user_data(
+    async fn prepare_user_data_common(
         &self,
         hasura_transaction: &Transaction<'_>,
         keycloak_transaction: &Transaction<'_>,
-    ) -> Result<Self::UserData> {
-        // TODO: Fix a lot clonning happening with the getters.
-
-        let event_realm_name =
-            get_event_realm(&self.get_tenant_id(), &self.get_election_event_id());
-        let tenant_realm_name = get_tenant_realm(&self.get_tenant_id());
+    ) -> Result<UserData> {
+        // TODO: Fix a lot cloning happening with the getters.
         // This is used to fill the user data.
         let election_event = get_election_event_by_id(
             &hasura_transaction,
@@ -188,6 +145,239 @@ impl TemplateRenderer for AuditLogsTemplate {
             .map_err(|e| anyhow::anyhow!("Error getting election dates {e}"))?;
 
         let datetime_printed: String = get_date_and_time();
+
+        // To filter log entries by election we´ll prepare a list with the user Ids that belong to this election.
+        // To get the voter_ids related to this election, we need the areas.
+        let election_areas = get_areas_by_election_id(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election_id,
+        )
+        .await
+        .map_err(|err| anyhow!("Error at get_areas_by_election_id: {err:?}"))?;
+
+        if election_areas.is_empty() {
+            return Err(anyhow!(
+                "No areas found for the given election id: {election_id}"
+            ));
+        }
+
+        let votes_data = generate_election_votes_data(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election.id,
+        )
+        .await
+        .map_err(|e| anyhow!(format!("Error generating election votes data {e:?}")))?;
+
+        // Fetch necessary data (dummy placeholders for now)
+        let post = election_general_data.post.clone();
+        let geographical_region = election_general_data.geographical_region.clone();
+        let voting_center = election_general_data.voting_center.clone();
+        let precinct_code = election_general_data.precinct_code.clone();
+
+        let report_hash = get_report_hash(&ReportType::AUDIT_LOGS.to_string())
+            .await
+            .unwrap_or("-".to_string());
+        let results_hash = get_results_hash(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election_id,
+        )
+        .await
+        .map_err(|err| {
+            warn!("Error getting results_hash: {err:?}");
+            anyhow!("Error getting results_hash: {err:?}")
+        })
+        .unwrap_or("-".to_string());
+
+        let app_hash = get_app_hash();
+        let app_version = get_app_version();
+        let signature_date = datetime_printed.clone();
+
+        // Fetch areas associated with the election
+        let election_areas = get_areas_by_election_id(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election_id,
+        )
+        .await
+        .map_err(|err| anyhow!("Error at get_areas_by_election_id: {err:?}"))?;
+
+        // we need at least one area to gather the inspectors of the area
+        if election_areas.is_empty() {
+            return Err(anyhow!("No areas found for the given election"));
+        }
+        let area_general_data = extract_area_data(
+            &election_areas[0],
+            election_event_annotations.sbei_users.clone(),
+        )
+        .await
+        .map_err(|err| anyhow!("Error extract area data {err}"))?;
+
+        let ballots_counted = count_ballots_by_election(
+            hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election_id,
+        )
+        .await
+        .map_err(|err| anyhow!("Error at count_ballots_by_election: {err:?}"))?;
+
+        Ok(UserData {
+            election_event_title: election_event.name.clone(),
+            election_dates,
+            geographical_region,
+            post,
+            voting_center,
+            precinct_code,
+            registered_voters: votes_data.registered_voters,
+            ballots_counted: Some(ballots_counted),
+            voters_turnout: votes_data.voters_turnout,
+            sequences: vec![],
+            signature_date,
+            inspectors: area_general_data.inspectors,
+            execution_annotations: ExecutionAnnotations {
+                date_printed: datetime_printed,
+                report_hash,
+                software_version: app_version.clone(),
+                app_version,
+                app_hash,
+                executer_username: self.ids.executer_username.clone(),
+                results_hash: Some(results_hash),
+            },
+        })
+    }
+}
+
+#[async_trait]
+impl TemplateRenderer for AuditLogsTemplate {
+    type UserData = UserData;
+    type SystemData = SystemData;
+
+    fn get_report_type(&self) -> ReportType {
+        ReportType::AUDIT_LOGS
+    }
+
+    fn get_tenant_id(&self) -> String {
+        self.ids.tenant_id.clone()
+    }
+
+    fn get_election_event_id(&self) -> String {
+        self.ids.election_event_id.clone()
+    }
+
+    fn get_initial_template_alias(&self) -> Option<String> {
+        self.ids.template_alias.clone()
+    }
+
+    fn get_report_origin(&self) -> ReportOriginatedFrom {
+        self.ids.report_origin
+    }
+
+    fn get_election_id(&self) -> Option<String> {
+        self.ids.election_id.clone()
+    }
+
+    fn base_name(&self) -> String {
+        "audit_logs".to_string()
+    }
+
+    fn prefix(&self) -> String {
+        format!("audit_logs_{}", self.ids.election_event_id)
+    }
+    async fn count_items(&self) -> Option<i64> {
+        None
+    }
+
+    #[instrument(err, skip(self, hasura_transaction, keycloak_transaction))]
+    async fn prepare_user_data_batch(
+        &self,
+        hasura_transaction: &Transaction<'_>,
+        keycloak_transaction: &Transaction<'_>,
+        offset: i64,
+        limit: i64,
+    ) -> Result<Self::UserData> {
+        let mut user_data = self
+            .prepare_user_data_common(hasura_transaction, keycloak_transaction)
+            .await?;
+
+        Ok(user_data)
+    }
+
+    #[instrument(err, skip(self, hasura_transaction, keycloak_transaction))]
+    async fn prepare_user_data(
+        &self,
+        hasura_transaction: &Transaction<'_>,
+        keycloak_transaction: &Transaction<'_>,
+    ) -> Result<Self::UserData> {
+        let mut user_data = self
+            .prepare_user_data_common(hasura_transaction, keycloak_transaction)
+            .await?;
+
+        // TODO: Fix a lot cloning happening with the getters.
+
+        let event_realm_name =
+            get_event_realm(&self.get_tenant_id(), &self.get_election_event_id());
+        let tenant_realm_name = get_tenant_realm(&self.get_tenant_id());
+        // This is used to fill the user data.
+        /*let election_event = get_election_event_by_id(
+            &hasura_transaction,
+            &self.get_tenant_id(),
+            &self.get_election_event_id(),
+        )
+        .await
+        .map_err(|e| anyhow!("Error getting scheduled event by election event_id: {e:?}"))?;*/
+
+        let Some(election_id) = self.get_election_id() else {
+            return Err(anyhow!("Empty election_id"));
+        };
+        info!("Preparing data of audit logs report for election_id: {election_id}");
+        let election: Election = match get_election_by_id(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+            &election_id,
+        )
+        .await
+        .map_err(|e| anyhow!(format!("Error getting election by id {e:?}")))?
+        {
+            Some(election) => election,
+            None => {
+                return Err(anyhow!(
+                    "No election found for the given election id: {election_id}"
+                ));
+            }
+        };
+
+        /*
+        let election_general_data = extract_election_data(&election)
+            .await
+            .map_err(|err| anyhow!("Error extract election data {err}"))?;
+
+        let election_event_annotations = extract_election_event_annotations(&election_event)
+            .await
+            .map_err(|err| anyhow!("Error extract election event annotations {err}"))?;
+
+        // Fetch election's voting periods
+        let scheduled_events = find_scheduled_event_by_election_event_id(
+            &hasura_transaction,
+            &self.ids.tenant_id,
+            &self.ids.election_event_id,
+        )
+        .await
+        .map_err(|e| {
+            anyhow::anyhow!("Error getting scheduled events by election event_id: {}", e)
+        })?;
+
+        let election_dates = get_election_dates(&election, scheduled_events)
+            .map_err(|e| anyhow::anyhow!("Error getting election dates {e}"))?;
+
+        let datetime_printed: String = get_date_and_time();*/
 
         // To filter log entries by election we´ll prepare a list with the user Ids that belong to this election.
         // To get the voter_ids related to this election, we need the areas.
@@ -371,94 +561,9 @@ impl TemplateRenderer for AuditLogsTemplate {
             sequences.push(audit_log_entry);
         }
 
-        let votes_data = generate_election_votes_data(
-            &hasura_transaction,
-            &self.ids.tenant_id,
-            &self.ids.election_event_id,
-            &election.id,
-        )
-        .await
-        .map_err(|e| anyhow!(format!("Error generating election votes data {e:?}")))?;
+        user_data.sequences = sequences;
 
-        // Fetch necessary data (dummy placeholders for now)
-        let post = election_general_data.post.clone();
-        let geographical_region = election_general_data.geographical_region.clone();
-        let voting_center = election_general_data.voting_center.clone();
-        let precinct_code = election_general_data.precinct_code.clone();
-
-        let report_hash = get_report_hash(&ReportType::AUDIT_LOGS.to_string())
-            .await
-            .unwrap_or("-".to_string());
-        let results_hash = get_results_hash(
-            &hasura_transaction,
-            &self.ids.tenant_id,
-            &self.ids.election_event_id,
-            &election_id,
-        )
-        .await
-        .map_err(|err| {
-            warn!("Error getting results_hash: {err:?}");
-            anyhow!("Error getting results_hash: {err:?}")
-        })
-        .unwrap_or("-".to_string());
-
-        let app_hash = get_app_hash();
-        let app_version = get_app_version();
-        let signature_date = datetime_printed.clone();
-
-        // Fetch areas associated with the election
-        let election_areas = get_areas_by_election_id(
-            &hasura_transaction,
-            &self.ids.tenant_id,
-            &self.ids.election_event_id,
-            &election_id,
-        )
-        .await
-        .map_err(|err| anyhow!("Error at get_areas_by_election_id: {err:?}"))?;
-
-        // we need at least one area to gather the inspectors of the area
-        if election_areas.is_empty() {
-            return Err(anyhow!("No areas found for the given election"));
-        }
-        let area_general_data = extract_area_data(
-            &election_areas[0],
-            election_event_annotations.sbei_users.clone(),
-        )
-        .await
-        .map_err(|err| anyhow!("Error extract area data {err}"))?;
-
-        let ballots_counted = count_ballots_by_election(
-            hasura_transaction,
-            &self.ids.tenant_id,
-            &self.ids.election_event_id,
-            &election_id,
-        )
-        .await
-        .map_err(|err| anyhow!("Error at count_ballots_by_election: {err:?}"))?;
-
-        Ok(UserData {
-            election_event_title: election_event.name.clone(),
-            election_dates,
-            geographical_region,
-            post,
-            voting_center,
-            precinct_code,
-            registered_voters: votes_data.registered_voters,
-            ballots_counted: Some(ballots_counted),
-            voters_turnout: votes_data.voters_turnout,
-            sequences,
-            signature_date,
-            inspectors: area_general_data.inspectors,
-            execution_annotations: ExecutionAnnotations {
-                date_printed: datetime_printed,
-                report_hash,
-                software_version: app_version.clone(),
-                app_version,
-                app_hash,
-                executer_username: self.ids.executer_username.clone(),
-                results_hash: Some(results_hash),
-            },
-        })
+        Ok(user_data)
     }
 
     #[instrument(err, skip_all)]
