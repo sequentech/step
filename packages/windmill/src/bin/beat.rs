b54a59a14efa58258ab4161fa23be242ff63de01
Eduardo Robles <edu@sequentech.io>
2025-02-17 18:02:50 +0100

    ✨ Enrollment / Voting > Performance Improvement: support inserting log messages from keycloak using rabbitmq (#1610)
    Parent issue: https://github.com/sequentech/meta/issues/5815

--- packages/windmill/src/bin/beat.rs
+++ packages/windmill/src/bin/beat.rs
@@ -2,69 +2,81 @@
 #![feature(result_flattening)]
 #![recursion_limit = "256"]
 // SPDX-FileCopyrightText: 2023 Felix Robles <felix@sequentech.io>
+// SPDX-FileCopyrightText: 2023 Eduardo Robles <edu@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
 use anyhow::Result;
 use celery::beat::DeltaSchedule;
+use celery::prelude::Task;
 use dotenv::dotenv;
 use sequent_core::util::init_log::init_log;
 use structopt::StructOpt;
 use tokio::time::Duration;
-use windmill::services::celery_app::set_is_app_active;
+use windmill::services::celery_app::{set_is_app_active, Queue};
 use windmill::services::probe::{setup_probe, AppName};
+use windmill::tasks::electoral_log::electoral_log_batch_dispatcher;
 use windmill::tasks::review_boards::review_boards;
 use windmill::tasks::scheduled_events::scheduled_events;
 use windmill::tasks::scheduled_reports::scheduled_reports;
 
 #[derive(Debug, StructOpt)]
 #[structopt(
-    name = "windmill",
-    about = "Run a Rust Celery producer or consumer.",
+    name = "beat",
+    about = "Windmill's periodic task scheduler.",
     setting = structopt::clap::AppSettings::ColoredHelp,
 )]
 struct CeleryOpt {
-    #[structopt(short, long, default_value = "15")]
-    interval: u64,
+    #[structopt(short = "r", long, default_value = "15")]
+    review_boards_interval: u64,
+    #[structopt(short = "s", long, default_value = "10")]
+    schedule_events_interval: u64,
+    #[structopt(short = "c", long, default_value = "10")]
+    schedule_reports_interval: u64,
+    #[structopt(short = "e", long, default_value = "5")]
+    electoral_log_interval: u64,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
     init_log(true);
-
     setup_probe(AppName::BEAT).await;
 
-    // Build a `Beat` with a default scheduler backend.
     let mut beat = celery::beat!(
         broker = AMQPBroker { std::env::var("AMQP_ADDR").unwrap_or_else(|_| "amqp://rabbitmq:5672".into()) },
         tasks = [
-            "review_boards" => {
+            review_boards::NAME => {
                 review_boards,
-                schedule = DeltaSchedule::new(Duration::from_secs(CeleryOpt::from_args().interval)),
+                schedule = DeltaSchedule::new(Duration::from_secs(CeleryOpt::from_args().review_boards_interval)),
                 args = (),
             },
-            "scheduled_events" => {
+            scheduled_events::NAME => {
                 scheduled_events,
-                schedule = DeltaSchedule::new(Duration::from_secs(10)),
+                schedule = DeltaSchedule::new(Duration::from_secs(CeleryOpt::from_args().schedule_events_interval)),
                 args = (),
             },
-            "scheduled_reports" => {
+            scheduled_reports::NAME => {
                 scheduled_reports,
-                schedule = DeltaSchedule::new(Duration::from_secs(10)),
+                schedule = DeltaSchedule::new(Duration::from_secs(CeleryOpt::from_args().schedule_reports_interval)),
+                args = (),
+            },
+            electoral_log_batch_dispatcher::NAME => {
+                electoral_log_batch_dispatcher,
+                schedule = DeltaSchedule::new(Duration::from_secs(CeleryOpt::from_args().electoral_log_interval)),
                 args = (),
             },
         ],
         task_routes = [
-            "review_boards" => "beat",
-            "scheduled_events" => "beat",
-            "scheduled_reports" => "beat",
+            review_boards::NAME => Queue::Beat.as_ref(),
+            scheduled_events::NAME => Queue::Beat.as_ref(),
+            scheduled_reports::NAME => Queue::Beat.as_ref(),
+            electoral_log_batch_dispatcher::NAME => Queue::ElectoralLogBeat.as_ref(),
         ],
     ).await?;
 
     set_is_app_active(true);
     beat.start().await?;
     set_is_app_active(false);
-
     Ok(())
 }
