ece37cd89eee529682e4b38a2dad306544099acc
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2025-02-25 13:17:35 +0000

    Add TRIGGER

--- hasura/migrations/backend-db/1693427784410_init/up.sql
+++ hasura/migrations/backend-db/1693427784410_init/up.sql
@@ -857,3 +857,31 @@ CREATE TABLE "sequent_backend"."trustee" ("id" uuid NOT NULL DEFAULT gen_random_
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 alter table "sequent_backend"."scheduled_event" drop column "board_id" cascade;
+
+CREATE OR REPLACE FUNCTION check_revote_limit()
+RETURNS TRIGGER AS $$
+DECLARE
+  allowed_revotes integer;
+BEGIN
+  SELECT num_allowed_revotes INTO allowed_revotes
+  FROM "sequent_backend"."election"
+  WHERE id = NEW.election_id;
+
+  IF allowed_revotes = 0 THEN
+    RETURN NEW;
+  ELSIF (
+    SELECT COUNT(*)
+    FROM "sequent_backend"."cast_vote" cv
+    WHERE cv.election_id = NEW.election_id
+    AND cv.voter_id_string = NEW.voter_id_string
+  ) >= allowed_revotes THEN
+    RAISE EXCEPTION 'Exceeds revote limit';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_revote_trigger
+BEFORE INSERT ON "sequent_backend"."cast_vote"
+FOR EACH ROW
+EXECUTE PROCEDURE check_revote_limit();
