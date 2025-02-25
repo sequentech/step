0973e6b21f893f0f41b763bc2e6a40402287c5d9
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2025-02-25 14:43:52 +0000

    Fix

--- hasura/migrations/backend-db/1693427784410_init/up.sql
+++ hasura/migrations/backend-db/1693427784410_init/up.sql
@@ -865,7 +865,9 @@ DECLARE
 BEGIN
   SELECT num_allowed_revotes INTO allowed_revotes
   FROM "sequent_backend"."election"
-  WHERE id = NEW.election_id;
+  WHERE id = NEW.election_id
+  AND tenant_id = NEW.tenant_id
+  AND election_event_id = NEW.election_event_id;
 
   IF allowed_revotes = 0 THEN
     RETURN NEW;
@@ -874,6 +876,8 @@ BEGIN
     FROM "sequent_backend"."cast_vote" cv
     WHERE cv.election_id = NEW.election_id
     AND cv.voter_id_string = NEW.voter_id_string
+    AND cv.tenant_id = NEW.tenant_id
+    AND cv.election_event_id = NEW.election_event_id
   ) >= allowed_revotes THEN
     RAISE EXCEPTION 'Exceeds revote limit';
   END IF;
