65b9941aae598202614f97f4659b7d094516d3ec
Félix Robles <felix@sequentech.io>
2025-02-26 04:59:25 +0000

    wip

--- packages/windmill/src/services/export/export_template.rs
+++ packages/windmill/src/services/export/export_template.rs
@@ -39,7 +39,7 @@ pub async fn read_export_data(
     Ok(transformed_templates)
 }
 
-#[instrument(err, skip(transaction))]
+#[instrument(err, skip(transaction, data))]
 pub async fn write_export_document(
     transaction: &Transaction<'_>,
     data: Vec<Template>,
