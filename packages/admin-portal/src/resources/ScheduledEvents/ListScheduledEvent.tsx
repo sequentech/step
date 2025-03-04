aec003e52c06b73618db0b3ed97bdd70056f8b2c
Enric Badia <enric.badia@gmail.com>
2025-03-04 12:12:56 +0000

    added filters to scheduled events

--- packages/admin-portal/src/resources/ScheduledEvents/ListScheduledEvent.tsx
+++ packages/admin-portal/src/resources/ScheduledEvents/ListScheduledEvent.tsx
@@ -8,15 +8,27 @@ import {SettingsContext} from "@/providers/SettingsContextProvider"
 import EditIcon from "@mui/icons-material/Edit"
 import DeleteIcon from "@mui/icons-material/Delete"
 import {Button, styled, Typography} from "@mui/material"
-import React, {ReactElement, useContext, useMemo, useState} from "react"
+import React, {
+    ReactElement,
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from "react"
 import moment from "moment-timezone"
 import {
     DatagridConfigurable,
     FunctionField,
     List,
+    ReferenceInput,
+    SelectInput,
     TextField,
+    TextInput,
     useGetList,
     useGetOne,
+    useListContext,
     useNotify,
     useRefresh,
     useSidebarState,
@@ -54,6 +66,32 @@ export const DataGridContainerStyle = styled(DatagridConfigurable)<{isOpenSideBa
     }
 `
 
+const FilterWatcher = ({
+    fieldName,
+    onFilterChange,
+}: {
+    fieldName: string
+    onFilterChange: () => void
+}) => {
+    const {filterValues} = useListContext()
+    const previousFilters = useRef(filterValues)
+
+    useEffect(() => {
+        // Check which filters were removed
+        Object.keys(previousFilters.current).forEach((key) => {
+            if (!(key in filterValues)) {
+                if (key === fieldName) {
+                    onFilterChange()
+                }
+            }
+        })
+
+        previousFilters.current = filterValues
+    }, [filterValues])
+
+    return null
+}
+
 interface EditEventsProps {
     electionEventId: string
 }
@@ -70,6 +108,7 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
     const [openCreateEvent, setOpenCreateEvent] = useState(false)
     const [selectedEventId, setSelectedEventId] = useState<string | undefined>()
     const aliasRenderer = useAliasRenderer()
+    const lc = useListContext()
 
     const {
         canWriteScheduledEvent,
@@ -103,7 +142,7 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
     const {data: elections} = useGetList<Sequent_Backend_Election>(
         "sequent_backend_election",
         {
-            pagination: {page: 1, perPage: 200},
+            pagination: {page: 1, perPage: 500},
             sort: {field: "created_at", order: "DESC"},
             filter: {
                 tenant_id: tenantId,
@@ -122,7 +161,15 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
         }
     )
 
-    const electionIds = useMemo(() => elections?.map((election) => election.id) ?? [], [elections])
+    const [eventScreeElectionId, setEventScreenElectionId] = useState<string | null>(null)
+
+    const electionIds = useMemo(() => {
+        let electionsList = elections?.map((election) => election.id) ?? []
+        if (eventScreeElectionId) {
+            electionsList = electionsList.filter((item) => item === eventScreeElectionId)
+        }
+        return electionsList
+    }, [elections, eventScreeElectionId])
 
     const getElectionName = (scheduledEvent: Sequent_Backend_Scheduled_Event): string => {
         let electionId = (scheduledEvent?.event_payload as IManageElectionDatePayload | undefined)
@@ -226,6 +273,33 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
             ) : null}
         </ResourceListStyles.EmptyBox>
     )
+
+    // Define the filters as an array of elements
+    const filters = [
+        <SelectInput
+            source="event_processor"
+            key="event_processor_filter"
+            label={t("eventsScreen.fields.eventProcessor")}
+            choices={Object.values(EventProcessors).map((eventType) => ({
+                id: eventType,
+                name: t(`eventsScreen.eventType.${eventType}`),
+            }))}
+        />,
+        <SelectInput
+            source="event_payload.election_id"
+            key="election_id_filter"
+            label={t("eventsScreen.fields.electionId")}
+            choices={elections?.map((election) => ({
+                id: election.id,
+                name: election.alias || election.name || "-",
+            }))}
+            onChange={(e: any) => {
+                setEventScreenElectionId(e.target.value)
+            }}
+        />,
+        <TextInput key="id_filter" source="id" label={"id"} />,
+    ]
+
     return (
         <>
             <ElectionHeader title={t("eventsScreen.title")} subtitle="eventsScreen.subtitle" />
@@ -245,7 +319,7 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
                         },
                     },
                 }}
-                filters={Filters}
+                filters={filters}
                 queryOptions={{
                     refetchInterval: globalSettings.QUERY_POLL_INTERVAL_MS,
                 }}
@@ -270,8 +344,11 @@ const ListScheduledEvents: React.FC<EditEventsProps> = ({electionEventId}) => {
                 disableSyncWithLocation
             >
                 <DatagridConfigurable bulkActionButtons={false} omit={OMIT_FIELDS}>
+                    <FilterWatcher
+                        fieldName="event_payload"
+                        onFilterChange={() => setEventScreenElectionId(null)}
+                    />
                     <TextField source="id" />
-
                     <FunctionField
                         label={t("eventsScreen.fields.electionId")}
                         source="event_payload.election_id"
