ccd9fbdf9a1ea652997a643976fb5ffb682b5769
Omri Ovadia <33923561+omri81@users.noreply.github.com>
2025-03-05 07:19:46 +0000

    .

--- packages/windmill/external-bin/janitor/duplicate_votes.py
+++ packages/windmill/external-bin/janitor/duplicate_votes.py
@@ -37,44 +37,53 @@ get_user_ids_query = """
 SELECT ue.id, ue.username, r.name AS realm_name
 FROM user_entity AS ue
 JOIN realm AS r ON ue.realm_id = r.id
-WHERE r.name = %s;
+WHERE r.name = %s
+LIMIT %s
+OFFSET 0;
 """
 
-kc_cursor.execute(get_user_ids_query, (realm_name,))
+kc_cursor.execute(get_user_ids_query, (realm_name, target_row_count))
 existing_user_ids = [row[0] for row in kc_cursor.fetchall()]
 print("number of existing user ids: ", len(existing_user_ids))
 
 hasura_cursor.execute(
     """
-    SELECT election_id, tenant_id, area_id, annotations, content, cast_ballot_signature, election_event_id, ballot_id
+    SELECT election_id, tenant_id, area_id, content, cast_ballot_signature, election_event_id, ballot_id
         FROM sequent_backend.cast_vote WHERE id = %s""", (row_id_to_clone,))
 base_row = hasura_cursor.fetchone()
 
 if not base_row:
     print("No row found to clone.")
 else:
-    election_id, tenant_id, area_id, annotations, content, cast_ballot_signature, election_event_id, ballot_id = base_row
-    annotations_json = json.dumps(annotations)
-    # 3) For each user_id you want to reference, insert a new row
- # Determine the number of iterations: use the smaller of target_row_count and available user IDs
-    num_iterations = min(target_row_count, len(existing_user_ids))
-    rows_to_insert = []
-    for i in range(num_iterations):
-        uid = existing_user_ids[i]
-        rows_to_insert.append((
-            uid, election_id, tenant_id, area_id, annotations_json, content,
-            cast_ballot_signature, election_event_id, ballot_id
-        ))
+    election_id, tenant_id, area_id, content, cast_ballot_signature, election_event_id, ballot_id = base_row
+    # annotations_json = json.dumps(annotations)
 
     insert_query = """
     INSERT INTO sequent_backend.cast_vote (
-        voter_id_string, election_id, tenant_id, area_id, annotations, content,
+        voter_id_string, election_id, tenant_id, area_id, content,
         cast_ballot_signature, election_event_id, ballot_id
     )
     VALUES %s
 """
 
-    execute_values(hasura_cursor, insert_query, rows_to_insert, template=None, page_size=100)
+    batch_size = 1000
+    rows_to_insert = []
+    for i in range(len(existing_user_ids)):
+        uid = existing_user_ids[i]
+        rows_to_insert.append((
+            uid, election_id, tenant_id, area_id, content,
+            cast_ballot_signature, election_event_id, ballot_id
+        ))
+        print("rows_to_insert", i)
+    
+        
+    # print("rows_to_insert", len(rows_to_insert))
+    # for i in range(0, len(rows_to_insert), batch_size):
+    #     batch = rows_to_insert[i:i+batch_size]
+    #     execute_values(hasura_cursor, insert_query, batch, template=None, page_size=batch_size)
+    #     hasura_conn.commit()  # Commit after each batch
+
+    execute_values(hasura_cursor, insert_query, rows_to_insert, template=None, page_size=1000)
     
     hasura_conn.commit()
 
