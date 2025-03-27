5d122bc0ea4ec490ef282101871c973c6c68cfc9
xalsina-sequent <xavier.alsina@sequentech.io>
2025-03-27 20:24:18 +0100

    wip

--- .devcontainer/scripts/initialize-command.sh
+++ .devcontainer/scripts/initialize-command.sh
@@ -1,6 +1,18 @@
-#!/bin/bash
-echo "---- Running initializeCommand as User ----"
-echo "User: $(whoami)"
-echo "ID: $(id)"
-echo "---- initializeCommand finished successfully ----"
-exit 0
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2023-2024 Sequent Tech <legal@sequentech.io>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+set -ex -o pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Create .devcontainer/.env if it does not already exists
+[ -e .devcontainer/.env ] || touch .devcontainer/.env
+cp .devcontainer/.env.development .devcontainer/.env
+# Load .devcontainer/.env environment variables
+source .devcontainer/.env
+# Set LOCAL_WORKSPACE_FOLDER environment variable if not already set
+[ ! -z "${localWorkspaceFolder}" ] || printf "\nLOCAL_WORKSPACE_FOLDER=${SCRIPT_DIR}/..\n" >> .devcontainer/.env
+
+echo "$(pwd)/.devcontainer/.env file initialized successfully"
