c07f6cf818b36a281bcffc0ef0c8d8a84c56e90c
Enric Badia <enric.badia@gmail.com>
2025-03-28 17:48:28 +0000

    added create to CustomAutocompleteArrayInputProps

--- packages/ui-essentials/src/components/CustomAutocompleteArrayInput/CustomAutocompleteArrayInput.tsx
+++ packages/ui-essentials/src/components/CustomAutocompleteArrayInput/CustomAutocompleteArrayInput.tsx
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 import React, {useState, useRef, ChangeEvent, KeyboardEvent} from "react"
 import {Autocomplete, TextField, Chip} from "@mui/material"
+import {create} from "@mui/material/styles/createTransitions"
 
 export type Choice = {
     id: string
@@ -16,6 +17,7 @@ interface CustomAutocompleteArrayInputProps {
     onCreate?: (value: string) => void
     choices?: Choice[] | undefined
     disabled?: boolean
+    prefixCreate?: string
 }
 
 const CustomAutocompleteArrayInput: React.FC<CustomAutocompleteArrayInputProps> = ({
@@ -25,6 +27,7 @@ const CustomAutocompleteArrayInput: React.FC<CustomAutocompleteArrayInputProps> 
     onCreate,
     choices,
     disabled,
+    prefixCreate = "Create",
 }) => {
     const [inputValue, setInputValue] = useState<string>("")
     const [selectedValues, setSelectedValues] = useState<string[]>(defaultValue || [])
@@ -36,22 +39,46 @@ const CustomAutocompleteArrayInput: React.FC<CustomAutocompleteArrayInputProps> 
     }
 
     const handleChange = (event: ChangeEvent<{}>, newValue: string[]) => {
-        const newLabels = newValue.flatMap((value) => value.split(/\s+/))
-        const uniqueLabels = Array.from(new Set(newLabels))
-        setSelectedValues(uniqueLabels)
-        onChange(uniqueLabels)
+        const createValue = newValue.filter(
+            (value) => value === `${prefixCreate || "Create"} ${inputValue}`
+        )
+
+        if (createValue && createValue.length > 0) {
+            handleCreateOption()
+        } else {
+            const filteredValue = newValue.filter(
+                (value) => value !== `${prefixCreate || "Create"} ${inputValue}`
+            )
+            const newLabels = filteredValue.flatMap((value) => value.split(/\s+/))
+            const uniqueLabels = Array.from(new Set(newLabels))
+            setSelectedValues(uniqueLabels)
+            onChange(uniqueLabels)
+        }
     }
 
     const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
         if (event.key === "Enter" && inputValue) {
             event.preventDefault()
+            handleCreateOption()
+        }
+    }
+
+    const handleCreateOption = () => {
+        if (inputValue.trim()) {
+            // Ensure inputValue is not blank
             const newLabels = inputValue.trim().split(/\s+/)
+
+            console.log("aa newLabels", newLabels)
 
             const updatedValues = [...selectedValues]
             const newChoices = [...(updatedChoices || [])]
 
             newLabels.forEach((newLabel) => {
-                if (newLabel && !updatedValues.includes(newLabel)) {
+                if (
+                    newLabel &&
+                    !updatedValues.includes(newLabel) &&
+                    !newChoices.some((choice) => choice.name === newLabel)
+                ) {
                     updatedValues.push(newLabel)
                     newChoices.push({id: newLabel, name: newLabel})
                 }
@@ -59,53 +86,76 @@ const CustomAutocompleteArrayInput: React.FC<CustomAutocompleteArrayInputProps> 
 
             setSelectedValues(updatedValues)
             setUpdatedChoices(newChoices)
-            setInputValue("")
+            setInputValue("") // Clear inputValue after creating labels
             inputRef?.current?.focus()
 
-            // Call onCreate for each new label after updating state
             newLabels.forEach((newLabel) => {
                 if (onCreate && newLabel && !selectedValues.includes(newLabel)) {
                     onCreate(newLabel)
                 }
             })
 
-            // Ensure all labels are saved correctly
+            onChange(updatedValues)
+        }
+    }
+
+    const handleSelectOption = (option: string) => {
+        if (option === `${prefixCreate || "Create"} ${inputValue}`) {
+            handleCreateOption()
+        } else if (option && option.trim()) {
+            const updatedValues = selectedValues.includes(option)
+                ? selectedValues.filter((value) => value !== option)
+                : [...selectedValues, option]
+            setSelectedValues(updatedValues)
             onChange(updatedValues)
         }
     }
 
     return (
-        <Autocomplete
-            multiple
-            freeSolo
-            fullWidth
-            disabled={disabled}
-            value={selectedValues}
-            onChange={handleChange}
-            inputValue={inputValue}
-            onInputChange={handleInputChange}
-            options={(updatedChoices || []).map((choice) => choice.name)}
-            renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                    <Chip
+        <>
+            <Autocomplete
+                multiple
+                freeSolo
+                fullWidth
+                disabled={disabled}
+                value={selectedValues}
+                onChange={handleChange}
+                inputValue={inputValue}
+                onInputChange={handleInputChange}
+                options={[
+                    ...(inputValue.trim() ? [`${prefixCreate || "Create"} ${inputValue}`] : []),
+                    ...(updatedChoices || []).map((choice) => choice.name),
+                ]}
+                renderTags={(value, getTagProps) =>
+                    value.map((option, index) => (
+                        <Chip
+                            variant="outlined"
+                            label={option}
+                            {...getTagProps({index})}
+                            key={option}
+                        />
+                    ))
+                }
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
                         variant="outlined"
-                        label={option}
-                        {...getTagProps({index})}
-                        key={option}
+                        label={label}
+                        onKeyDown={handleKeyDown}
+                        inputRef={inputRef}
+                        disabled={disabled}
                     />
-                ))
-            }
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    variant="outlined"
-                    label={label}
-                    onKeyDown={handleKeyDown}
-                    inputRef={inputRef}
-                    disabled={disabled}
-                />
-            )}
-        />
+                )}
+                onClose={() => setInputValue("")} // Clear input on dropdown close
+                onSelect={(event) => {
+                    const option = (event.target as HTMLElement).textContent || ""
+
+                    if (option !== "") {
+                        handleSelectOption(option)
+                    }
+                }}
+            />
+        </>
     )
 }
 
