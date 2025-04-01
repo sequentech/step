eee79b25aa66b6368d6b54ea953a06ad386f1ee4
Yuval Komash <yuval@moveoboost.com>
2025-04-01 10:54:32 +0000

    wip

--- packages/sms-sender/src/lib.rs
+++ packages/sms-sender/src/lib.rs
@@ -1,27 +1,36 @@
+// SPDX-FileCopyrightText: 2024 Felix Robles <felix@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
 use reqwest::Client;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 
+#[derive(Serialize, Deserialize)]
+pub struct SmsConfig {
+    receiver: String,
+    message: String,
+}
 
-#[derive(Serialize)]
-struct SessionlessSmsRequest {
+#[derive(Serialize, Deserialize)]
+struct SendSmsRequest {
     from_phone: String,
     phone_number: Vec<String>,
     message: Vec<String>,
 }
-
-pub struct GoogleCloudSmsSender {
+pub struct SmsSender {
     client: Client,
     access_token: String,
     from_phone: String,
 }
 
-impl GoogleCloudSmsSender {
+impl SmsSender {
     pub fn new() -> Self {
+        let _ = dotenv::dotenv();
+
         let access_token = std::env::var("GOOGLE_CLOUD_ACCESS_TOKEN")
             .expect("GOOGLE_CLOUD_ACCESS_TOKEN environment variable not set");
-        let from_phone = std::env::var("SMS_FROM_PHONE")
-        .expect("SMS_FROM_PHONE environment variable not set");
+        let from_phone =
+            std::env::var("SMS_FROM_PHONE").expect("SMS_FROM_PHONE environment variable not set");
         Self {
             client: Client::new(),
             access_token,
@@ -30,18 +39,18 @@ impl GoogleCloudSmsSender {
     }
 
     /// Sends an SMS using the Sessionless Outbound SMS API.
-    pub async fn send_sms(&self, receiver: String, message: String) -> Result<String, Box<dyn Error>> {
-
+    pub async fn send_sms(&self, sms_config: SmsConfig) -> Result<String, Box<dyn Error>> {
         let url = std::env::var("GOOGLE_OUTBOUND_SMS_API_URL")
-        .expect("GOOGLE_OUTBOUND_SMS_API_URL environment variable not set");
+            .expect("GOOGLE_OUTBOUND_SMS_API_URL environment variable not set");
 
-        let payload = SessionlessSmsRequest {
+        let payload = SendSmsRequest {
             from_phone: self.from_phone.clone(),
-            phone_number: vec![receiver],
-            message: vec![message]
+            phone_number: vec![sms_config.receiver],
+            message: vec![sms_config.message],
         };
 
-        let response = self.client
+        let response = self
+            .client
             .post(url)
             .bearer_auth(&self.access_token)
             .json(&payload)
