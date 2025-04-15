988b9890509bb6a5c5159ce37acdbfd10560a272
Félix Robles <felix@sequentech.io>
2025-04-14 19:57:15 -0500

    🐞 Ballot receipts don't create task executions (#1794)
    Parent issue: https://github.com/sequentech/meta/issues/6583

--- packages/harvest/src/routes/create_vote_receipt.rs
+++ packages/harvest/src/routes/create_vote_receipt.rs
@@ -7,6 +7,7 @@ use anyhow::Result;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use sequent_core::types::date_time::DateFormat;
+use sequent_core::types::hasura::core::TasksExecution;
 use sequent_core::types::permissions::VoterPermissions;
 use sequent_core::{services::jwt::JwtClaims, types::date_time::TimeZone};
 use serde::{Deserialize, Serialize};
@@ -15,31 +16,33 @@ use std::time::Instant;
 use tracing::{event, instrument, Level};
 use uuid::Uuid;
 use windmill::services::celery_app::get_celery_app;
+use windmill::services::tasks_execution::post;
+use windmill::types::tasks::ETasksExecution;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct CreateVoteReceiptInput {
-    ballot_id: String,
-    ballot_tracker_url: String,
-    tenant_id: String,
-    election_event_id: String,
-    election_id: String,
-    time_zone: Option<TimeZone>,
-    date_format: Option<DateFormat>,
+pub struct CreateBallotReceiptInput {
+    pub ballot_id: String,
+    pub ballot_tracker_url: String,
+    pub election_event_id: String,
+    pub election_id: String,
+    pub time_zone: Option<TimeZone>,
+    pub date_format: Option<DateFormat>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct CreateVoteReceiptOutput {
-    id: String,
-    ballot_id: String,
-    status: String,
+pub struct CreateBallotReceiptOutput {
+    pub id: String,
+    pub ballot_id: String,
+    pub status: String,
+    pub task_execution: TasksExecution,
 }
 
 #[instrument(skip_all)]
-#[post("/create-vote-receipt", format = "json", data = "<body>")]
-pub async fn create_vote_receipt(
-    body: Json<CreateVoteReceiptInput>,
+#[post("/create-ballot-receipt", format = "json", data = "<body>")]
+pub async fn create_ballot_receipt(
+    body: Json<CreateBallotReceiptInput>,
     claims: JwtClaims,
-) -> Result<Json<CreateVoteReceiptOutput>, (Status, String)> {
+) -> Result<Json<CreateBallotReceiptOutput>, (Status, String)> {
     let input = body.into_inner();
     let area_id = authorize_voter_election(
         &claims,
@@ -50,19 +53,35 @@ pub async fn create_vote_receipt(
     let element_id: String = Uuid::new_v4().to_string();
     let celery_app = get_celery_app().await;
 
-    let task = celery_app
+    // Insert the task execution record
+    let task_execution = post(
+        &tenant_id,
+        Some(&input.election_event_id),
+        ETasksExecution::CREATE_BALLOT_RECEIPT,
+        &executer_name,
+    )
+    .await
+    .map_err(|error| {
+        (
+            Status::InternalServerError,
+            format!("Failed to insert task execution record: {error:?}"),
+        )
+    })?;
+
+    let celery_task_result = celery_app
         .send_task(
             windmill::tasks::create_vote_receipt::create_vote_receipt::new(
                 element_id.clone(),
                 input.ballot_id.clone(),
                 input.ballot_tracker_url,
-                input.tenant_id,
+                tenant_id.clone(),
                 input.election_event_id,
                 input.election_id,
                 area_id,
                 voter_id,
                 input.time_zone,
                 input.date_format,
+                task_execution.clone(),
             ),
         )
         .await
@@ -74,9 +93,12 @@ pub async fn create_vote_receipt(
         })?;
     event!(Level::INFO, "Sent task {:?} successfully", task);
 
-    Ok(Json(CreateVoteReceiptOutput {
-        id: element_id,
+    info!("Sent task {:?} successfully", task);
+
+    Ok(Json(CreateBallotReceiptOutput {
+        id: document_id,
         ballot_id: input.ballot_id,
         status: "pending".to_string(),
+        task_execution: task_execution,
     }))
 }
