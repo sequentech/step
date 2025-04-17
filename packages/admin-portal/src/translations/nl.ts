4542fb6eaa97e63011daf2f537e599e5a05e0b95
xalsina-sequent <xavier.alsina@sequentech.io>
2025-04-17 15:14:47 +0200

    Added missing translations

--- packages/admin-portal/src/translations/nl.ts
+++ packages/admin-portal/src/translations/nl.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Félix Robles <felix@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
-import {TranslationType} from "./en"
+import { TranslationType } from "./en"
 
 const dutchTranslation: TranslationType = {
     translations: {
@@ -416,6 +416,10 @@ const dutchTranslation: TranslationType = {
                         "The Tally Ceremony cannot start until you create one publication in the Publish tab.",
                     participateNow:
                         "You have been invited to participate in a Tally ceremony. Please <1>click on the ceremony's Key Action</1> to participate.",
+                    startDisabled:
+                        "You cannot continue the ceremony because no elections are selected or the elections are not published.",
+                    ceremonyDisabled:
+                        "You cannot continue the ceremony because the tally session is not connected or the start of the ceremony is not allowed.",
                 },
             },
             importAreas: {
