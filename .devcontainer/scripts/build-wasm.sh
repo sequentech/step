f0bb0545fd45a7120209f7fa9b3487b576ebf308
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2025-04-17 16:23:55 +0000

    wip

--- .devcontainer/scripts/build-wasm.sh
+++ .devcontainer/scripts/build-wasm.sh
@@ -8,8 +8,11 @@ set -ex -o pipefail
 source .devcontainer/.env
 
 cd packages/sequent-core 
-# wasm-pack build --mode no-install --out-name index --release --target web --features=wasmtest
-# wasm-pack -v pack . 2>&1 | tee output.log
+# cargo --version
+# cargo install wasm-pack --version 0.12.1
+# cargo install -f wasm-bindgen-cli --version 0.2.100
+wasm-pack build --mode no-install --out-name index --release --target web --features=wasmtest
+wasm-pack -v pack . 2>&1 | tee output.log
 
 cd ..
 hash=$(grep "shasum:" sequent-core/output.log | awk '{printf $4}')
@@ -24,12 +27,13 @@ awk -v hash="${hash}" '
 
 mv yarn.lock.tmp yarn.lock
 
-# rm ./ui-core/rust/sequent-core-0.1.0.tgz ./admin-portal/rust/sequent-core-0.1.0.tgz ./voting-portal/rust/sequent-core-0.1.0.tgz ./ballot-verifier/rust/sequent-core-0.1.0.tgz
-# cp sequent-core/pkg/sequent-core-0.1.0.tgz ./ui-core/rust/sequent-core-0.1.0.tgz
-# cp sequent-core/pkg/sequent-core-0.1.0.tgz ./admin-portal/rust/sequent-core-0.1.0.tgz
-# cp sequent-core/pkg/sequent-core-0.1.0.tgz ./voting-portal/rust/sequent-core-0.1.0.tgz
-# cp sequent-core/pkg/sequent-core-0.1.0.tgz ./ballot-verifier/rust/sequent-core-0.1.0.tgz
+rm sequent-core/output.log
+rm ./ui-core/rust/sequent-core-0.1.0.tgz ./admin-portal/rust/sequent-core-0.1.0.tgz ./voting-portal/rust/sequent-core-0.1.0.tgz ./ballot-verifier/rust/sequent-core-0.1.0.tgz
+cp sequent-core/pkg/sequent-core-0.1.0.tgz ./ui-core/rust/sequent-core-0.1.0.tgz
+cp sequent-core/pkg/sequent-core-0.1.0.tgz ./admin-portal/rust/sequent-core-0.1.0.tgz
+cp sequent-core/pkg/sequent-core-0.1.0.tgz ./voting-portal/rust/sequent-core-0.1.0.tgz
+cp sequent-core/pkg/sequent-core-0.1.0.tgz ./ballot-verifier/rust/sequent-core-0.1.0.tgz
 
-# rm -rf node_modules ui-core/node_modules voting-portal/node_modules ballot-verifier/node_modules admin-portal/node_modules
+rm -rf node_modules ui-core/node_modules voting-portal/node_modules ballot-verifier/node_modules admin-portal/node_modules
 
-# yarn && yarn build:ui-core && yarn build:ui-essentials && yarn build:voting-portal && yarn build:admin-portal
+yarn && yarn build:ui-core && yarn build:ui-essentials && yarn build:voting-portal && yarn build:admin-portal
