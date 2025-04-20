e40f470c78225f9ef7865c1451d3ec2e47b939e7
Félix Robles <felix@sequentech.io>
2025-04-20 21:14:13 +0800

    🐞 version error wasm-bindgen (#1804)
    Parent issue: https://github.com/sequentech/meta/issues/5480

--- packages/sequent-core/flake.nix
+++ packages/sequent-core/flake.nix
@@ -7,7 +7,7 @@
 
   # input
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
-  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
@@ -28,7 +28,7 @@
           configureRustTargets = targets : pkgs
             .rust-bin
             .nightly
-            ."2023-08-01"
+            ."2024-07-31"
             .default
             .override {
                 extensions = [ "rust-src" ];
@@ -71,7 +71,7 @@
             ];
             buildPhase = ''
               echo 'Build: wasm-pack build'
-              wasm-pack build --mode no-install --out-name index --release --target web --features=wasmtest
+              wasm-pack build --out-name index --release --target web --features=wasmtest
             '';
             installPhase = "
               # set HOME temporarily to fix npm pack
