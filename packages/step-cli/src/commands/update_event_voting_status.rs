6694b5ae630e56592bf994a7f45c72e821760b98
omri <omri.ovadya@gmail.com>
2025-05-26 11:10:54 +0000

    fix

--- packages/step-cli/src/commands/update_event_voting_status.rs
+++ packages/step-cli/src/commands/update_event_voting_status.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 use std::str::FromStr;
 
 use crate::{types::hasura_types::*, utils::read_config::read_config};
