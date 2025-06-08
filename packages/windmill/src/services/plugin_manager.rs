e577fc1b925ea5140daa9d5135e3cf52dbeacd78
Yuval Komash <yuval@moveoboost.com>
2025-06-08 11:56:29 +0000

    wip

--- packages/windmill/src/services/plugin_manager.rs
+++ packages/windmill/src/services/plugin_manager.rs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
 use anyhow::{anyhow, Context, Result};
 use dashmap::DashMap;
 use extism::{Manifest, Plugin, Wasm};
