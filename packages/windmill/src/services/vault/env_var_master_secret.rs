e7b1ab962bb048869b63f65231552159cea3f560
Beltran Rodriguez <beltran.rodriguez@sequentech.io>
2025-07-25 13:00:47 +0000

    Impl logic when missing ENV var.

--- packages/windmill/src/services/vault/env_var_master_secret.rs
+++ packages/windmill/src/services/vault/env_var_master_secret.rs
@@ -6,9 +6,8 @@
 use super::{Vault, VaultManagerType};
 use anyhow::{Context, Result};
 use async_trait::async_trait;
-use serde::{Deserialize, Serialize};
 use std::env;
-use tracing::{info, instrument};
+use tracing::{error, info, instrument};
 
 #[derive(Debug)]
 pub struct EnvVarMasterSecret;
@@ -16,15 +15,25 @@ pub struct EnvVarMasterSecret;
 #[async_trait]
 impl Vault for EnvVarMasterSecret {
     #[instrument(err)]
-    async fn save_secret(&self, _key: String, _value: String) -> Result<()> {
-        // it is not needed
-        Ok(())
+    async fn save_secret(&self, _key: String, value: String) -> Result<()> {
+        // If initialize_master_secret failed to read, it creates the master secret value
+        // and tries to save it calling to this function.
+        // We want it to fail becasue the admin must be aware that the set up was wrong.
+        // We will then print the generated value to the console and return an error, so the admin can add it manually.
+        info!("Generated master secret automatically.");
+        info!("Please set manually MASTER_SECRET = {value} ");
+        Err(anyhow::anyhow!("MASTER_SECRET env var missing."))
     }
 
     #[instrument(err)]
     async fn read_secret(&self, _key: String) -> Result<Option<String>> {
-        let master_secret = env::var("MASTER_SECRET").context("MASTER_SECRET must be set")?;
-        Ok(Some(master_secret))
+        match env::var("MASTER_SECRET") {
+            Ok(master_secret) => Ok(Some(master_secret)),
+            Err(_) => {
+                error!("MASTER_SECRET must be set.");
+                Ok(None)
+            }
+        }
     }
 
     #[instrument]
