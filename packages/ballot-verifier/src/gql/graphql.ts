6dc4e99200676d7aaba323f613168bc551dfb423
Beltran Rodriguez <beltran.rodriguez@sequentech.io>
2025-08-04 13:43:13 +0000

    Graphql

--- packages/ballot-verifier/src/gql/graphql.ts
+++ packages/ballot-verifier/src/gql/graphql.ts
@@ -17094,6 +17094,8 @@ export type Sequent_Backend_Tally_Sheet_Bool_Exp = {
 export enum Sequent_Backend_Tally_Sheet_Constraint {
     /** unique or primary key constraint on columns "id", "tenant_id", "election_event_id" */
     TallySheetPkey = "tally_sheet_pkey",
+    /** unique or primary key constraint on columns "election_id", "contest_id", "area_id", "channel", "tenant_id", "election_event_id" */
+    TallySheetUniqChannel = "tally_sheet_uniq_channel",
 }
 
 /** delete the field or element with specified path (for JSON arrays, negative integers count from the end) */
