3dbd1eb514577759232a263cf35046c7d4988733
Findeton <felix@sequentech.io>
2025-12-08 13:14:23 +0000

    wip

---
+++ packages/keycloak-extensions/custom-event-listener/src/main/java/sequent/keycloak/custom_event_listener/Utils.java
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package sequent.keycloak.custom_event_listener;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.jbosslog.JBossLog;
+
+@UtilityClass
+@JBossLog
+public class Utils {
+  public static final String EVENT_TYPE_COMMUNICATIONS = "communications";
+}
