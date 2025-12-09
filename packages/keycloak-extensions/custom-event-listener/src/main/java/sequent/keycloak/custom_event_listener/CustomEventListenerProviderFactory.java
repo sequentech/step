3e766c937c39435becf8e76adec8a319d608786e
Félix Robles <felix@sequentech.io>
2025-12-09 10:21:06 -0500

    🐞 Keycloak's custom event listener is not working (#2236) (#2241)
    Parent issue: https://github.com/sequentech/meta/issues/9574

---
+++ packages/keycloak-extensions/custom-event-listener/src/main/java/sequent/keycloak/custom_event_listener/CustomEventListenerProviderFactory.java
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+package sequent.keycloak.custom_event_listener;
+
+import com.google.auto.service.AutoService;
+import org.keycloak.Config.Scope;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+@AutoService(EventListenerProviderFactory.class)
+public class CustomEventListenerProviderFactory implements EventListenerProviderFactory {
+
+  @Override
+  public EventListenerProvider create(KeycloakSession session) {
+    return new CustomEventListenerProvider(session);
+  }
+
+  @Override
+  public void init(Scope config) {}
+
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {}
+
+  @Override
+  public void close() {}
+
+  @Override
+  public String getId() {
+    return "custom-event-listener";
+  }
+}
