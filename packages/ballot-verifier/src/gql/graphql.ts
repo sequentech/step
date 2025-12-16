dc004da8e4bf7bd9afe6591ca9828d9db1af0e88
yuvalkom-M <yuval@moveoboost.com>
2025-12-16 13:13:50 +0000

    reverse changes and only add timezone to the format date on the hbs files

--- packages/ballot-verifier/src/gql/graphql.ts
+++ packages/ballot-verifier/src/gql/graphql.ts
@@ -1739,7 +1739,7 @@ export type Mutation_RootCreate_Ballot_ReceiptArgs = {
     ballot_tracker_url: Scalars["String"]["input"]
     election_event_id: Scalars["uuid"]["input"]
     election_id: Scalars["uuid"]["input"]
-    user_timezone: Scalars["String"]["input"]
+    tenant_id: Scalars["uuid"]["input"]
 }
 
 /** mutation root */
@@ -2291,7 +2291,6 @@ export type Mutation_RootExport_Election_EventArgs = {
 export type Mutation_RootExport_Election_Event_LogsArgs = {
     election_event_id: Scalars["String"]["input"]
     format: Scalars["String"]["input"]
-    user_timezone: Scalars["String"]["input"]
 }
 
 /** mutation root */
@@ -2357,7 +2356,6 @@ export type Mutation_RootGenerate_ReportArgs = {
     report_id: Scalars["String"]["input"]
     report_mode: Scalars["String"]["input"]
     tenant_id: Scalars["String"]["input"]
-    user_timezone: Scalars["String"]["input"]
 }
 
 /** mutation root */
