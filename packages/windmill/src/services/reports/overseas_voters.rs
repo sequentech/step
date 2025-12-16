54b41f814c8132244ffec67aa4ee29962e400ff0
yuvalkom-M <yuval@moveoboost.com>
2025-12-16 13:28:55 +0000

    remove unnecessary changes

--- packages/windmill/src/services/reports/overseas_voters.rs
+++ packages/windmill/src/services/reports/overseas_voters.rs
@@ -124,6 +124,7 @@ impl OverseasVotersReport {
     #[instrument(err, skip_all)]
     async fn generate_report_area(
         &self,
+        generate_mode: GenerateReportMode,
         hasura_transaction: &Transaction<'_>,
         keycloak_transaction: &Transaction<'_>,
         user_tpl_document: &str,
@@ -551,6 +552,7 @@ impl TemplateRenderer for OverseasVotersReport {
                                     .block_on(async {
                                         info!("processing batch {batch_index}: inside the GLOBAL_RT.block_on");
                                         self.generate_report_area(
+                                            generate_mode.clone(),
                                             &hasura_transaction,
                                             &keycloak_transaction,
                                             &user_tpl_document,
