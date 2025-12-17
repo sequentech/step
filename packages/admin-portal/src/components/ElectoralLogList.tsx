938c18f07f40ee544591b53d5d8fcc36b439af50
Beltran Rodriguez <beltran.rodriguez@sequentech.io>
2025-12-17 14:29:48 +0000

    wip

--- packages/admin-portal/src/components/ElectoralLogList.tsx
+++ packages/admin-portal/src/components/ElectoralLogList.tsx
@@ -204,23 +204,11 @@ export const ElectoralLogList: React.FC<ElectoralLogListProps> = ({
             key={"created"}
             source={"created"}
             label={String(t("logsScreen.column.created"))}
-            slotProps={{
-                htmlInput: {
-                    step: 1, // Adds seconds field to the datetime input
-                },
-            }}
-            parse={(value) => (value ? new Date(`${value}Z`).toISOString() : value)}
         />,
         <DateTimeInput
             key={"statement_timestamp"}
             source={"statement_timestamp"}
             label={String(t("logsScreen.column.statement_timestamp"))}
-            slotProps={{
-                htmlInput: {
-                    step: 1, // Adds seconds field to the datetime input
-                },
-            }}
-            parse={(value) => (value ? new Date(`${value}Z`).toISOString() : value)}
         />,
         <TextInput
             key={"statement_kind"}
