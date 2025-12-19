c55c58e5e9b2c6f955a6ed7c3dfb9527581c45ff
xalsina-sequent <xavier.alsina@sequentech.io>
2025-12-19 15:33:08 +0000

    wip fix licenses

--- packages/braid/scripts/test/gen.sh
+++ packages/braid/scripts/test/gen.sh
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 Sequent Tech <legal@sequentech.io>
+# SPDX-FileCopyrightText: 2025 Sequent Tech Inc <legal@sequentech.io>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
 cargo run --release --bin demo_tool -- gen-configs --port=5432 --password=postgrespw --num-trustees=$1 --threshold=$2
