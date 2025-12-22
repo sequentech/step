173b3168391f1adc3492bb3c71cf3d40affbd17b
xalsina-sequent <xavier.alsina@sequentech.io>
2025-12-22 09:59:56 +0000

    Fix formatting and reuse

--- packages/admin-portal/src/screens/TrusteeDashboard.tsx
+++ packages/admin-portal/src/screens/TrusteeDashboard.tsx
@@ -1,4 +1,6 @@
-// src/braid/TrusteeDashboard.tsx
+// SPDX-FileCopyrightText: 2025 Sequent Tech Inc <legal@sequentech.io>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
 import React, {useState, useEffect, useRef, useCallback} from "react"
 import {
     Card,
