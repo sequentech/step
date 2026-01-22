7e84268d9f7bfcb2ee639b16023270464ac30872
Xavier Alsina <xavier.alsina@sequentech.io>
2026-01-22 16:20:53 +0100

    ✨ Step-CLI Generate voters - Allow email_verified and authorized_election_count (#1920)
    Parent issue: https://github.com/sequentech/meta/issues/7254
    
    Co-authored-by: xalsina-sequent <“xavier.alsina@sequentech.io”>
    Co-authored-by: yuvalkom-M <yuval@moveoboost.com>
    Co-authored-by: Beltrán Rodríguez <beltran.rodriguez@sequentech.io>

--- packages/step-cli/src/types/config.rs
+++ packages/step-cli/src/types/config.rs
@@ -19,3 +19,46 @@ pub struct ConfigData {
     pub client_secret: String,
     pub username: String,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExternalConfigData {
+    pub election_event_json_file: String,
+    pub realm_name: String,
+    pub tenant_id: String,
+    pub election_event_id: String,
+    pub area_id: String,
+    pub election_id: String,
+    pub generate_voters: GenerateVoters,
+    pub duplicate_votes: DuplicateVotes,
+    pub generate_applications: GenerateApplications,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GenerateVoters {
+    pub csv_file_name: String,
+    pub fields: Vec<String>,
+    pub excluded_columns: Vec<String>,
+    pub email_prefix: String,
+    pub domain: String,
+    pub sequence_email_number: bool,
+    pub sequence_start_number: i64,
+    pub voter_password: String,
+    pub password_salt: String,
+    pub hashed_password: String,
+    pub overseas_reference: String,
+    pub min_age: i64,
+    pub max_age: i64,
+    pub authorized_elections_count: i64,
+    pub email_verified: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DuplicateVotes {
+    pub row_id_to_clone: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GenerateApplications {
+    pub applicant_data: HashMap<String, Value>,
+    pub annotations: HashMap<String, Value>,
+}
