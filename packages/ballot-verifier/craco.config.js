00f9aecfa73d640d6be5683911789118690854dd
yuvalkom-M <yuval@moveoboost.com>
2026-01-29 08:13:41 +0000

    wip

--- packages/ballot-verifier/craco.config.js
+++ packages/ballot-verifier/craco.config.js
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 module.exports = {
-    devServer: {
-        headers: {
+    devServer: (devServerConfig, {env, paths}) => {
+        devServerConfig.headers = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Headers": "*",
@@ -12,6 +12,23 @@ module.exports = {
             "Cross-Origin-Resource-Policy": "cross-origin",
             "Cross-Origin-Embedder-Policy": "credentialless",
             "Referrer-Policy": "no-referrer",
-        },
+        }
+
+        // Remove deprecated properties for webpack-dev-server v5 compatibility
+        delete devServerConfig.onAfterSetupMiddleware
+        delete devServerConfig.onBeforeSetupMiddleware
+        delete devServerConfig.https
+
+        // Use the new setupMiddlewares instead
+        devServerConfig.setupMiddlewares = (middlewares, devServer) => {
+            return middlewares
+        }
+
+        // Configure HTTPS properly for webpack-dev-server v5
+        if (process.env.HTTPS === "true") {
+            devServerConfig.server = "https"
+        }
+
+        return devServerConfig
     },
 }
