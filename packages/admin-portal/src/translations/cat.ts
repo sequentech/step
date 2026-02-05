ecff728227bb0d71479be415c13675a3f27c2d61
Findeton <felix@sequentech.io>
2026-02-05 21:30:21 +0000

    wip

--- packages/admin-portal/src/translations/cat.ts
+++ packages/admin-portal/src/translations/cat.ts
@@ -1302,6 +1302,9 @@ const catalanTranslation: TranslationType = {
             paginationPolicy: {
                 label: "Nom de la pàgina",
             },
+            allowWriteins: {
+                label: "Permetre candidatures manuals",
+            },
             error: {},
             createContestSuccess: "Pregunta creada",
             createContestError: "Error creant pregunta",
--- packages/admin-portal/src/translations/eu.ts
+++ packages/admin-portal/src/translations/eu.ts
@@ -1290,6 +1290,9 @@ const basqueTranslation: TranslationType = {
             paginationPolicy: {
                 label: "Orri Izena",
             },
+            allowWriteins: {
+                label: "Eskuzko hautagaitzak baimendu",
+            },
             error: {},
             createContestSuccess: "Lehiaketa sortua",
             createContestError: "Errorea hautagaia sortzerakoan",
