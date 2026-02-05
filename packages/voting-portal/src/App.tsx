35fa398d00d89120b3207ed0398373ad36235b79
yuvalkom-M <yuval@moveoboost.com>
2026-02-05 15:02:25 +0000

    wip

--- packages/voting-portal/src/App.tsx
+++ packages/voting-portal/src/App.tsx
@@ -19,6 +19,7 @@ import {useAppSelector} from "./store/hooks"
 import {selectElectionIds} from "./store/elections/electionsSlice"
 import {
     selectBallotStyleByElectionId,
+    selectBallotStyleElectionIds,
     selectFirstBallotStyle,
 } from "./store/ballotStyles/ballotStylesSlice"
 import WatermarkBackground from "./components/WaterMark/Watermark"
@@ -85,7 +86,10 @@ const App = () => {
     const {isAuthenticated, setTenantEvent} = useContext(AuthContext)
 
     const electionIds = useAppSelector(selectElectionIds)
-    const ballotStyle = useAppSelector(selectBallotStyleByElectionId(String(electionIds[0])))
+    const ballotStyleElectionIds = useAppSelector(selectBallotStyleElectionIds)
+
+    const electionId = electionIds.length > 0 ? electionIds[0] : ballotStyleElectionIds[0]
+    const ballotStyle = useAppSelector(selectBallotStyleByElectionId(String(electionId)))
 
     useEffect(() => {
         if (location.pathname === "/") {
@@ -119,7 +123,7 @@ const App = () => {
 
     return (
         <StyledApp
-            className="app-root"
+            className="voting-portal app-root"
             css={ballotStyle?.ballot_eml.election_event_presentation?.css ?? ""}
         >
             <ScrollRestoration />
