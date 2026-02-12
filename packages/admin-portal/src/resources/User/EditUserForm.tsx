c1d854640278ef5bfba4b31e4e47246115d60800
yuvalkom-M <yuval@moveoboost.com>
2026-02-12 15:28:54 +0000

    fix user edit form election select

--- packages/admin-portal/src/resources/User/EditUserForm.tsx
+++ packages/admin-portal/src/resources/User/EditUserForm.tsx
@@ -569,6 +569,13 @@ export const EditUserForm: React.FC<EditUserFormProps> = ({
         return {"name@_ilike,alias@_ilike": searched.current}
     }
 
+    const formattedElections = electionsList?.map((e) => {
+        return {
+            id: e.id,
+            name: aliasRenderer(e),
+        }
+    })
+
     const renderFormField = useCallback(
         (attr: UserProfileAttribute, index: number) => {
             if (attr.name) {
@@ -746,10 +753,10 @@ export const EditUserForm: React.FC<EditUserFormProps> = ({
                                 label={getTranslationLabel(attr.name, attr.display_name, t)}
                                 className="elections-selector"
                                 fullWidth
-                                choices={electionsList || []}
+                                choices={formattedElections || []}
                                 source="attributes.authorized-election-ids"
-                                defaultValue={user?.attributes?.["authorized-election-ids"] ?? []}
-                                optionText={aliasRenderer}
+                                optionValue={"name"}
+                                optionText={"name"}
                                 onChange={handleArraySelectChange}
                                 disabled={
                                     !(
