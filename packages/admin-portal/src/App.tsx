95c729022e68135e580501d60337b5d2900065a5
Yuval Komash <102144445+yuvalkom-M@users.noreply.github.com>
2026-03-03 19:29:38 +0200

    🐞 Ballot Verifier > Custom CSS not properly applie (#2391)
    parent issue: https://github.com/sequentech/meta/issues/8476
    
    ---------
    
    Co-authored-by: Beltrán Rodríguez <beltran.rodriguez@sequentech.io>

--- packages/admin-portal/src/App.tsx
+++ packages/admin-portal/src/App.tsx
@@ -65,14 +65,14 @@ import {UpsertArea} from "./resources/Area/UpsertArea"
 
 interface AppProps {}
 
-const StyledApp = styled(Box)<{css: string}>`
-    ${({css}) => css}
+const StyledApp = styled(Box)<{customCssss: string}>`
+    ${({customCss}) => customCss}
 `
 
 export const StyledAppAtom: React.FC<{children: React.ReactNode}> = ({children}) => {
     const css = useAtomValue(cssInputLookAndFeel)
     return (
-        <StyledApp className="styled-app-atom" css={css}>
+        <StyledApp className="styled-app-atom" customCss={css}>
             {children}
         </StyledApp>
     )
