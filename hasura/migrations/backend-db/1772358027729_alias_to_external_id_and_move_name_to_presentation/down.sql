ea57ac6b6da5de155587dcda6a43076e8c332042
yuvalkom-M <yuval@moveoboost.com>
2026-03-04 10:08:09 +0000

    fix down.sql

--- hasura/migrations/backend-db/1772358027729_alias_to_external_id_and_move_name_to_presentation/down.sql
+++ hasura/migrations/backend-db/1772358027729_alias_to_external_id_and_move_name_to_presentation/down.sql
@@ -1,3 +1,26 @@
+-- ===============================
+-- candidate
+-- ===============================
+do $$
+begin
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema='sequent_backend' and table_name='candidate' and column_name='name'
+  ) then
+    alter table "sequent_backend"."candidate" add column "name" text;
+  end if;
+end $$;
+
+update "sequent_backend"."candidate"
+set "name" = coalesce(
+  nullif("presentation" #>> '{i18n,en,name}', ''),
+  '-'
+)
+where "name" is null;
+
+alter table "sequent_backend"."candidate"
+alter column "name" set not null;
+
 do $$
 begin
   if exists (
@@ -11,6 +34,30 @@ begin
     rename column "external_id" to "alias";
   end if;
 end $$;
+
+
+-- ===============================
+-- contest
+-- ===============================
+do $$
+begin
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema='sequent_backend' and table_name='contest' and column_name='name'
+  ) then
+    alter table "sequent_backend"."contest" add column "name" text;
+  end if;
+end $$;
+
+update "sequent_backend"."contest"
+set "name" = coalesce(
+  nullif("presentation" #>> '{i18n,en,name}', ''),
+  '-'
+)
+where "name" is null;
+
+alter table "sequent_backend"."contest"
+alter column "name" set not null;
 
 do $$
 begin
@@ -26,6 +73,30 @@ begin
   end if;
 end $$;
 
+
+-- ===============================
+-- election
+-- ===============================
+do $$
+begin
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema='sequent_backend' and table_name='election' and column_name='name'
+  ) then
+    alter table "sequent_backend"."election" add column "name" text;
+  end if;
+end $$;
+
+update "sequent_backend"."election"
+set "name" = coalesce(
+  nullif("presentation" #>> '{i18n,en,name}', ''),
+  '-'
+)
+where "name" is null;
+
+alter table "sequent_backend"."election"
+alter column "name" set not null;
+
 do $$
 begin
   if exists (
@@ -39,6 +110,30 @@ begin
     rename column "external_id" to "alias";
   end if;
 end $$;
+
+
+-- ===============================
+-- election_event
+-- ===============================
+do $$
+begin
+  if not exists (
+    select 1 from information_schema.columns
+    where table_schema='sequent_backend' and table_name='election_event' and column_name='name'
+  ) then
+    alter table "sequent_backend"."election_event" add column "name" text;
+  end if;
+end $$;
+
+update "sequent_backend"."election_event"
+set "name" = coalesce(
+  nullif("presentation" #>> '{i18n,en,name}', ''),
+  '-'
+)
+where "name" is null;
+
+alter table "sequent_backend"."election_event"
+alter column "name" set not null;
 
 do $$
 begin
