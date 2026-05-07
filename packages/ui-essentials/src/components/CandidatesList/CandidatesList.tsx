6d5e469b692cdb6bf295a9928833ab82127a7ebb
xalsina-sequent <xavier.alsina@sequentech.io>
2026-05-07 17:49:28 +0000

    wip

--- packages/ui-essentials/src/components/CandidatesList/CandidatesList.tsx
+++ packages/ui-essentials/src/components/CandidatesList/CandidatesList.tsx
@@ -56,6 +56,7 @@ export interface CandidatesListProps extends PropsWithChildren {
     isCheckable?: boolean
     checked?: boolean
     setChecked?: (value: boolean) => void
+    shouldDisable?: boolean
 }
 
 const CandidatesList: React.FC<CandidatesListProps> = ({
@@ -65,14 +66,15 @@ const CandidatesList: React.FC<CandidatesListProps> = ({
     isCheckable,
     checked,
     setChecked,
+    shouldDisable,
 }) => {
     const onClick = () => {
-        if (isActive && isCheckable && setChecked) {
+        if (isActive && isCheckable && !shouldDisable && setChecked) {
             setChecked(!checked)
         }
     }
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-        isActive && isCheckable && setChecked && setChecked(event.target.checked)
+        isActive && isCheckable && !shouldDisable && setChecked && setChecked(event.target.checked)
 
     return (
         <ListContainer
@@ -91,7 +93,11 @@ const CandidatesList: React.FC<CandidatesListProps> = ({
                     </ListTitle>
                 </Box>
                 {isActive && isCheckable ? (
-                    <Checkbox checked={checked} onChange={handleChange} />
+                    <Checkbox
+                        checked={checked}
+                        onChange={handleChange}
+                        disabled={shouldDisable}
+                    />
                 ) : null}
             </ListHeader>
             <ListChildrenContainer className="candidates-list-children">
