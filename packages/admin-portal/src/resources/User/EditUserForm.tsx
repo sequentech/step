ffc8472d2f72dd8a15a36660e612a16283ed9b01
Beltran Rodriguez <beltran.rodriguez@sequentech.io>
2026-05-07 15:54:40 +0200

    Fix empty key

--- packages/admin-portal/src/resources/User/EditUserForm.tsx
+++ packages/admin-portal/src/resources/User/EditUserForm.tsx
@@ -459,8 +459,8 @@ export const EditUserForm: React.FC<EditUserFormProps> = ({
 
     const handleAttrChange =
         (attrName: string) => async (e: React.ChangeEvent<HTMLInputElement>) => {
-            const {name, value} = e.target
-            debouncedHandleChange(name, value)
+            const {value} = e.target
+            debouncedHandleChange(attrName, value)
         }
 
     const debouncedHandleChange = useCallback(
