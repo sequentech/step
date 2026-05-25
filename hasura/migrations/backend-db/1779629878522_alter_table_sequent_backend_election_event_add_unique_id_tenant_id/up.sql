f9f79b6f925450a49debcf7f0d59286f84e17a06
Enes Selim <eselimsen@gmail.com>
2026-05-25 19:55:37 +0300

    imp: fix pg election event constraint ordering

--- hasura/migrations/backend-db/1779629878522_alter_table_sequent_backend_election_event_add_unique_id_tenant_id/up.sql
+++ hasura/migrations/backend-db/1779629878522_alter_table_sequent_backend_election_event_add_unique_id_tenant_id/up.sql
@@ -1,1 +1,2 @@
-alter table "sequent_backend"."election_event" add constraint "election_event_id_tenant_id_key" unique ("id", "tenant_id");
+alter table "sequent_backend"."election_event"
+    add constraint "election_event_id_tenant_id_key" unique ("tenant_id", "id");
