25f4e64ff27c0c6bdd90e77dab9d182cae2de067
Enes Selim <eselimsen@gmail.com>
2026-06-11 21:15:48 +0300

    imp: add updated_at for phone_blacklist for convention

--- hasura/migrations/backend-db/1779629886309_create_table_sequent_backend_phone_blacklist/up.sql
+++ hasura/migrations/backend-db/1779629886309_create_table_sequent_backend_phone_blacklist/up.sql
@@ -7,6 +7,7 @@ CREATE TABLE "sequent_backend"."phone_blacklist"
     "reason"            text,
     "created_at"        timestamptz NOT NULL DEFAULT now(),
     "created_by"        uuid        NOT NULL,
+    "updated_at"        timestamptz NOT NULL DEFAULT now(),
     PRIMARY KEY ("id"),
     FOREIGN KEY ("tenant_id")
         REFERENCES "sequent_backend"."tenant" ("id")
