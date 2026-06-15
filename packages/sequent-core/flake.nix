217444aa5832f4f5a64bd604798048d98df0e5ae
xalsina-sequent <xavier.alsina@sequentech.io>
2026-06-15 18:38:54 +0000

    Revert "Update rust version to stable 1.96"
    This reverts commit c037ef44542661ed5b8eec9409d2502b7b829361.

--- packages/sequent-core/flake.nix
+++ packages/sequent-core/flake.nix
@@ -27,8 +27,8 @@
           };
           configureRustTargets = targets : pkgs
             .rust-bin
-            .stable
-            ."1.96.0"
+            .nightly
+            ."2025-01-29"
             .default
             .override {
                 extensions = [ "rust-src" ];
@@ -124,9 +124,6 @@
               export CXX=${pkgs.llvmPackages_19.clang-unwrapped}/bin/clang++
               export AR=${pkgs.llvmPackages_19.llvm}/bin/llvm-ar
               export CC_wasm32_unknown_unknown=${pkgs.llvmPackages_19.clang-unwrapped}/bin/clang
-              -
-              # Nix hardening flags are not supported when compiling C code for WebAssembly
-              export NIX_HARDENING_ENABLE=""
               -
               # Set up the clang resource directory properly
               CLANG_MAJOR_VERSION="19"
