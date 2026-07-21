735cf1a914a731a8d2f4a7c09ead68df356d7a35
Félix Robles <felix@sequentech.io>
2026-07-20 19:51:17 -0500

    ✨ Smartlink for second gen (#2874)
    Parent issue: https://github.com/sequentech/meta/issues/12275

--- packages/admin-portal/src/types/keycloak.ts
+++ packages/admin-portal/src/types/keycloak.ts
@@ -10,6 +10,8 @@ export enum IPermissions {
     ELECTION_EVENT_CREATE = "election-event-create",
     ELECTION_EVENT_READ = "election-event-read",
     ELECTION_EVENT_WRITE = "election-event-write",
+    KEYCLOAK_REALM_ATTRIBUTES_READ = "keycloak-realm-attributes-read",
+    KEYCLOAK_REALM_ATTRIBUTES_WRITE = "keycloak-realm-attributes-write",
     ELECTION_EVENT_DELETE = "election-event-delete",
     ELECTION_EVENT_ARCHIVE = "election-event-archive",
     VOTER_CREATE = "voter-create",
