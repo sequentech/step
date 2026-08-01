f31ec3d6d3271dfc8d8d12d69e1acb92184a111d
Félix Robles <felix@sequentech.io>
2026-08-01 02:36:59 +0000

    maybe fix tests

--- packages/ui-essentials/jest.config.cjs
+++ packages/ui-essentials/jest.config.cjs
@@ -1,15 +1,9 @@
-// SPDX-FileCopyrightText: 2025 Sequent Tech Inc <legal@sequentech.io>
+// SPDX-FileCopyrightText: 2026 Sequent Tech Inc <legal@sequentech.io>
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-/** @type {import('jest').Config} */
 module.exports = {
     testEnvironment: "node",
-    testMatch: ["<rootDir>/src/**/*.test.ts"],
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-        "^@sequentech/ui-core$": "<rootDir>/../ui-core/src/types/VotingChannel.ts",
-    },
     transform: {
         "^.+\\.[jt]sx?$": [
             "babel-jest",
