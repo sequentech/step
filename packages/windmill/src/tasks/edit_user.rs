685588ac4fa580ac648ade6dd28e1cc685b5811b
Félix Robles <felix@sequentech.io>
2026-08-03 13:22:55 +0000

    format

--- packages/windmill/src/tasks/edit_user.rs
+++ packages/windmill/src/tasks/edit_user.rs
@@ -12,6 +12,7 @@ use crate::services::datafix::types::{SoapRequest, SoapRequestResponse, SoapRequ
 use crate::services::datafix::utils::{
     datafix_voter_lock_key, post_operation_result_to_electoral_log, voted_via_internet,
 };
+use crate::services::datafix::voterview_requests::SoapSendError;
 use crate::services::electoral_log::{
     post_voter_password_change, ElectoralLogAdminContext, VoterPasswordChangeSource,
 };
@@ -23,7 +24,6 @@ use crate::services::external::utils::{
     external_voter_lock_key, post_operation_result_to_electoral_log, voted_via_internet,
     voted_via_not_internet_channel, DATAFIX_VOTER_LOCK_SECS,
 };
-use crate::services::datafix::voterview_requests::SoapSendError;
 use crate::services::pg_lock::PgLock;
 use crate::services::tasks_execution::{update_complete, update_fail};
 use crate::types::error::{Error, Result};
