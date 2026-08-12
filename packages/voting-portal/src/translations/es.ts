cb40c93313347e3879804763ff4027dabab76387
Beltrán Rodríguez <beltran.rodriguez@sequentech.io>
2026-08-12 14:10:04 +0000

    Voting portal and headers policy fixes
    (cherry picked from commit b9123193de9a6a61947c77b8ea2415afcf4c954f)

--- packages/voting-portal/src/translations/es.ts
+++ packages/voting-portal/src/translations/es.ts
@@ -334,6 +334,7 @@ const spanishTranslation: TranslationType = {
                 back: "Volver a la Lista de Votaciones",
                 close: "Cerrar",
                 preview: "Vista previa",
+                download: "Descargar",
             },
         },
         ballotLocator: {
