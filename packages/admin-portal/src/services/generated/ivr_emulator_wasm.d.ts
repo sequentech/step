37b2ce5228f57e43d72d47d766c8675a44b3704a
Félix Robles <felix@sequentech.io>
2026-08-23 21:34:47 -0500

    ✨ IVR: Ballot level blank votes- #829 (#3085)
    Parent issue: https://github.com/sequentech/meta/issues/12891

--- packages/admin-portal/src/services/generated/ivr_emulator_wasm.d.ts
+++ packages/admin-portal/src/services/generated/ivr_emulator_wasm.d.ts
@@ -56,15 +56,15 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
     readonly memory: WebAssembly.Memory;
     readonly __wbg_ivremulatordriver_free: (a: number, b: number) => void;
-    readonly init: (a: any) => void;
     readonly ivremulatordriver_attributes: (a: number) => [number, number, number];
     readonly ivremulatordriver_execute: (a: number, b: number) => any;
     readonly ivremulatordriver_new: (a: any) => [number, number, number];
     readonly ivremulatordriver_send_input: (a: number, b: number, c: number) => void;
     readonly ivremulatordriver_send_timeout: (a: number) => void;
-    readonly wasm_bindgen__closure__destroy__hd27b649bb08bb176: (a: number, b: number) => void;
-    readonly wasm_bindgen__convert__closures_____invoke__h1de2b50527c514e7: (a: number, b: number, c: any) => [number, number];
-    readonly wasm_bindgen__convert__closures_____invoke__h06edaa965244ee7f: (a: number, b: number, c: any, d: any) => void;
+    readonly init: (a: any) => void;
+    readonly wasm_bindgen__closure__destroy__h7baa5a84311a3b04: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h5fed3dcc0d3dcd7b: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h331bfc0ec3639989: (a: number, b: number, c: any, d: any) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
     readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
     readonly __wbindgen_exn_store: (a: number) => void;
